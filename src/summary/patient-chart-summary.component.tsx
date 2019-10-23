c3aaf15e4facfc3ec0a77479c01e2c309dee8cdc
Donald Kibet <chelashawdonald@yahoo.com>
2019-10-23 19:15:15 +0300

    MF:85 Create History Widget for Patient Chart : Allergy Card implemen… (#4)
    * MF:85 Create History Widget for Patient Chart : Allergy Card implementation
    
    * MF-85 allergy-card code review

--- src/summary/patient-chart-summary.component.tsx
+++ src/summary/patient-chart-summary.component.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { RouteComponentProps } from "react-router";
 import styles from "./patient-chart-summary.css";
 import ProfileSection from "./profile/profile-section.component";
+import HistorySection from "./history/history-section.component";
 
 export default function PatientChartSummary(props: PatientChartSummaryProps) {
   return (
     <main className={`omrs-main-content ${styles.patientSummary}`}>
       <ProfileSection match={props.match} />
+      <HistorySection match={props.match} />
     </main>
   );
 }
