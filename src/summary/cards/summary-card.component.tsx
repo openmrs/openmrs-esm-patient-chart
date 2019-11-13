b0e0b3f0fac9d66d39cd22ecf5078a0b8492fe58
Fatma Ali <alifatma2019@gmail.com>
2019-11-13 17:54:25 +0300

    Updated Summary card header to new designs

--- src/summary/cards/summary-card.component.tsx
+++ src/summary/cards/summary-card.component.tsx
@@ -6,10 +6,13 @@ export default function SummaryCard(props: SummaryCardProps) {
   return (
     <div className={`omrs-card ${styles.card}`} style={props.styles}>
       <div className={styles.header}>
-        <h2 className={`omrs-margin-0`}>{props.name}</h2>
-        <svg className="omrs-icon" fill="rgba(0, 0, 0, 0.54)">
-          <use xlinkHref="#omrs-icon-chevron-right" />
-        </svg>
+        <div className={styles.title}>
+          <h2 className={`omrs-margin-0`}>{props.name}</h2>
+          <svg className="omrs-icon" fill="rgba(0, 0, 0, 0.54)">
+            <use xlinkHref="#omrs-icon-chevron-right" />
+          </svg>
+        </div>
+        <button className={`omrs-unstyled ${styles.addBtn}`}>Add</button>
       </div>
       {props.children}
     </div>
