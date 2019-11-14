75a0defb19477cda383011a32a64ac0e1a8e87ec
Donald Kibet <chelashawdonald@yahoo.com>
2019-11-14 14:56:00 +0300

    MF-94 Impementing Level Two Allergy UI and Test (#17)
    * MF-94: Implementing Allergy Level Two
    
    * MF-94 Implement Level Two Allergies UI Improvement & Tests

--- src/summary/history/allergy-card-level-two.component.tsx
+++ src/summary/history/allergy-card-level-two.component.tsx
@@ -32,76 +32,113 @@ export function AllergyCardLevelTwo(props: AllergyCardLevelTwoProps) {
         match={props.match}
         styles={{ width: "100%" }}
       >
-        <div className={`omrs-type-body-regular ${styles.allergyHeader}`}>
-          <p className="omrs-type-body-regular">ALLERGEN</p>
-          <p className="omrs-type-body-regular">SEVERITY & REACTION</p>
-          <p className="omrs-type-body-regular">SINCE</p>
-          <p className="omrs-type-body-regular">UPDATED</p>
-        </div>
-        {patientAllergy &&
-          patientAllergy.total > 0 &&
-          patientAllergy.entry.map(allergy => {
-            return (
-              <>
-                <div className={styles.allergyContent}>
-                  <div
-                    className={`omrs-type-body-regular ${styles.allergyRow}`}
-                  >
-                    <p className="omrs-bold">{allergy.resource.code.text}</p>
-                    <p className={styles.allergyRowManifestation}>
-                      <span
-                        className="omrs-bold"
-                        style={{ textTransform: "uppercase" }}
-                      >
-                        {allergy.resource.criticality === "high" && (
-                          <svg
-                            className={`omrs-icon`}
-                            fontSize={"15px"}
-                            fill="var(--omrs-color-danger)"
-                          >
-                            <use xlinkHref="#omrs-icon-important-notification" />
-                          </svg>
-                        )}
-                        {allergy.resource.criticality}
-                      </span>
-                      <span className="omrs-type-body-regular">
-                        <p>
-                          {allergy.resource.reaction[0].manifestation.map(
-                            manifestation => `${manifestation.text} `
-                          )}
-                        </p>
-                      </span>
-                    </p>
-                    <p>
-                      {dayjs(
-                        allergy.resource.extension[0].valueDateTime
-                      ).format("MMM-YYYY")}
-                    </p>
-                    <p>
-                      {dayjs(patientAllergy.meta.lastUpdated).format(
-                        "DD-MMM-YYYY"
-                      )}
-                    </p>
-                    <svg
-                      className={`omrs-icon ${styles.headerIcons}`}
-                      fill="rgba(0, 0, 0, 0.54)"
-                    >
-                      <use xlinkHref="#omrs-icon-chevron-right" />
-                    </svg>
-                  </div>
-                  <div className={styles.allergyComment}>
-                    <p></p>
-                    <p className={styles.allergyCommentText}>
-                      <p className="omrs-type-body-large">
-                        {allergy.resource.note && allergy.resource.note[0].text}
-                      </p>
-                      <p>more ...</p>
-                    </p>
-                  </div>
+        <table className={styles.allergyTable}>
+          <thead>
+            <tr>
+              <td>ALLERGEN</td>
+              <td>
+                <div className={styles.centerItems}>
+                  SEVERITY & REACTION
+                  <svg className="omrs-icon" fill="rgba(0, 0, 0, 0.54)">
+                    <use xlinkHref="#omrs-icon-arrow-downward" />
+                  </svg>
                 </div>
-              </>
-            );
-          })}
+              </td>
+              <td>SINCE</td>
+              <td>UPDATED</td>
+            </tr>
+          </thead>
+          <tbody>
+            {patientAllergy &&
+              patientAllergy.entry.map(allergy => {
+                return (
+                  <React.Fragment key={allergy.resource.id}>
+                    <tr
+                      className={`${
+                        allergy.resource.criticality === "high"
+                          ? `${styles.high}`
+                          : `${styles.low}`
+                      }`}
+                    >
+                      <td className={"omrs-bold"}>
+                        {allergy.resource.code.text}
+                      </td>
+                      <td>
+                        <div
+                          className={`${styles.centerItems} ${
+                            allergy.resource.criticality === "high"
+                              ? `omrs-bold`
+                              : ``
+                          }`}
+                          style={{ textTransform: "uppercase" }}
+                        >
+                          {allergy.resource.criticality === "high" && (
+                            <svg
+                              className={`omrs-icon`}
+                              fontSize={"15px"}
+                              fill="rgba(181, 7, 6, 1)"
+                            >
+                              <use xlinkHref="#omrs-icon-important-notification" />
+                            </svg>
+                          )}
+                          {allergy.resource.criticality}
+                        </div>
+                      </td>
+                      <td>
+                        {dayjs(
+                          allergy.resource.extension[0].valueDateTime
+                        ).format("MMM-YYYY")}
+                      </td>
+                      <td>
+                        <div
+                          className={`${styles.centerItems} ${styles.alignRight}`}
+                        >
+                          <span>
+                            {dayjs(patientAllergy.meta.lastUpdated).format(
+                              "DD-MMM-YYYY"
+                            )}
+                          </span>
+                          <svg className="omrs-icon" fill="rgba(0, 0, 0, 0.54)">
+                            <use xlinkHref="#omrs-icon-chevron-right" />
+                          </svg>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td colSpan={3}>
+                        {Object.values(
+                          allergy.resource.reaction[0].manifestation.map(
+                            manifestation => manifestation.text
+                          )
+                        ).join(", ")}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td colSpan={3}>
+                        <span className={`${styles.allergyComment}`}>
+                          <span>
+                            {allergy.resource.note &&
+                              allergy.resource.note[0].text}
+                          </span>
+                          <span>more...</span>
+                        </span>
+                      </td>
+                    </tr>
+                  </React.Fragment>
+                );
+              })}
+          </tbody>
+        </table>
+        <div className={styles.allergyFooter}>
+          <p
+            style={{ color: "var(--omrs-color-ink-medium-contrast)" }}
+            className={"omrs-type-body-large"}
+          >
+            No more allergies available
+          </p>
+        </div>
       </SummaryCard>
     );
   }
@@ -141,22 +178,20 @@ export function AllergyCardLevelTwo(props: AllergyCardLevelTwoProps) {
 
   function displayNoAllergenHistory() {
     return (
-      <div>
-        <SummaryCard
-          name="Allergy"
-          match={props.match}
-          styles={{ width: "100%" }}
-        >
-          <div className={styles.allergyMargin}>
-            <p className="omrs-bold">
-              The patient's allergy history is not documented.
-            </p>
-            <p className="omrs-bold">
-              Please <a href="/">add allergy history</a>.
-            </p>
-          </div>
-        </SummaryCard>
-      </div>
+      <SummaryCard
+        name="Allergy"
+        match={props.match}
+        styles={{ width: "100%" }}
+      >
+        <div className={styles.allergyMargin}>
+          <p className="omrs-bold">
+            The patient's allergy history is not documented.
+          </p>
+          <p className="omrs-bold">
+            Please <a href="/">add allergy history</a>.
+          </p>
+        </div>
+      </SummaryCard>
     );
   }
 
