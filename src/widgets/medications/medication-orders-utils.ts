4ebd6769cd3818225383cf2b467e86fb7713fdaf
Jonathan Dick <jonathandick@Jonathans-MacBook-Pro-2.local>
2020-01-21 10:19:01 +0300

    moved dosage function to utils file

---
+++ src/widgets/medications/medication-orders-utils.ts
@@ -0,0 +1,31 @@
+export function getDosage(strength, doseNumber) {
+  const i = strength.search(/\D/);
+  const strengthQuantity = strength.substring(0, i);
+
+  const concentrationStartIndex = strength.search(/\//);
+
+  let strengthUnits = strength.substring(i);
+  let dosage;
+
+  if (concentrationStartIndex >= 0) {
+    strengthUnits = strength.substring(i, concentrationStartIndex);
+    const j = strength.substring(concentrationStartIndex + 1).search(/\D/);
+    const concentrationQuantity = strength.substr(
+      concentrationStartIndex + 1,
+      j
+    );
+    const concentrationUnits = strength.substring(
+      concentrationStartIndex + 1 + j
+    );
+    dosage =
+      doseNumber +
+      strengthUnits +
+      " (" +
+      (doseNumber / strengthQuantity) * concentrationQuantity +
+      concentrationUnits +
+      ")";
+  } else {
+    dosage = strengthQuantity * doseNumber + strengthUnits;
+  }
+  return dosage;
+}
--- src/widgets/medications/medications-summary.component.tsx
+++ src/widgets/medications/medications-summary.component.tsx
@@ -8,6 +8,7 @@ import { createErrorHandler } from "@openmrs/esm-error-handling";
 import { useCurrentPatient } from "@openmrs/esm-api";
 import SummaryCardFooter from "../cards/summary-card-footer.component";
 import { useTranslation } from "react-i18next";
+import { getDosage } from "./medication-orders-utils";
 
 export default function MedicationsSummary(props: MedicationsOverviewProps) {
   const [patientMedications, setPatientMedications] = React.useState(null);
@@ -129,38 +130,6 @@ export default function MedicationsSummary(props: MedicationsOverviewProps) {
       );
     });
   }
-}
-
-function getDosage(strength, doseNumber) {
-  const i = strength.search(/\D/);
-  const strengthQuantity = strength.substring(0, i);
-
-  const concentrationStartIndex = strength.search(/\//);
-
-  let strengthUnits = strength.substring(i);
-  let dosage;
-
-  if (concentrationStartIndex >= 0) {
-    strengthUnits = strength.substring(i, concentrationStartIndex);
-    const j = strength.substring(concentrationStartIndex + 1).search(/\D/);
-    const concentrationQuantity = strength.substr(
-      concentrationStartIndex + 1,
-      j
-    );
-    const concentrationUnits = strength.substring(
-      concentrationStartIndex + 1 + j
-    );
-    dosage =
-      doseNumber +
-      strengthUnits +
-      " (" +
-      (doseNumber / strengthQuantity) * concentrationQuantity +
-      concentrationUnits +
-      ")";
-  } else {
-    dosage = strengthQuantity * doseNumber + strengthUnits;
-  }
-  return dosage;
 }
 
 type MedicationsOverviewProps = {
