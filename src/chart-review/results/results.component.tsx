2d647bdabf76afc1a7eb730e0087c3d45c452b1f
Donald Kibet <chelashawdonald@yahoo.com>
2020-02-14 06:53:03 +0300

    Implemented Height and Weight Detailed Summary View Component

--- src/chart-review/results/results.component.tsx
+++ src/chart-review/results/results.component.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, useLocation } from "react-router-dom";
 import VitalsDetailedSummary from "../../widgets/vitals/vitals-detailed-summary.component";
-import HeightAndWeightDetailed from "../../widgets/heightandweight/heightandweight-detailed.component";
 import ChartWidget from "../../ui-components/chart-widget/chart-widget.component";
 import ResultsChartOverview from "./results-chart-overview.component";
+import HeightAndWeightSummary from "../../widgets/heightandweight/heightandweight-summary.component";
+import { HeightAndWeightDetailedSummary } from "../../widgets/heightandweight/heightandweight-detailed-summary.component";
 
 export default function Results(props: any) {
   const match = useRouteMatch();
+  let queryParams = new URLSearchParams(useLocation().search);
 
   const widgetConfig = {
     name: "results",
@@ -27,11 +29,31 @@ export default function Results(props: any) {
       {
         name: "Height and Weight",
         component: () => {
-          return <HeightAndWeightDetailed match={match} />;
+          return <HeightAndWeightSummary match={match} />;
         }
       }
     ]
   };
+
+  if (queryParams.get("tab") && queryParams.get("uuid")) {
+    let selectedTab = widgetConfig.tabs.findIndex(
+      element => element.name === queryParams.get("tab")
+    );
+    switch (queryParams.get("tab")) {
+      case "Height and Weight":
+        widgetConfig.tabs.splice(selectedTab, 1, {
+          name: "Height and Weight",
+          component: () => {
+            return (
+              <HeightAndWeightDetailedSummary
+                match={match}
+                uuid={queryParams.get("uuid")}
+              />
+            );
+          }
+        });
+    }
+  }
 
   return <ChartWidget {...props} widgetConfig={widgetConfig} />;
 }
---
+++ src/widgets/heightandweight/heightandweight-detailed-summary.test.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { BrowserRouter, match } from "react-router-dom";
+import { HeightAndWeightDetailedSummary } from "./heightandweight-detailed-summary.component";
+import { useCurrentPatient, openmrsObservableFetch } from "@openmrs/esm-api";
+import { mockDimensionResponseRESTAPI } from "../../../__mocks__/dimensions.mock";
+import { mockPatient } from "../../../__mocks__/patient.mock";
+import { render, wait, cleanup, getByText } from "@testing-library/react";
+import { of } from "rxjs";
+
+const mockUseCurrentPatient = useCurrentPatient as jest.Mock;
+const mockOpenmrsObservableFetch = openmrsObservableFetch as jest.Mock;
+
+jest.mock("@openmrs/esm-api", () => ({
+  useCurrentPatient: jest.fn(),
+  openmrsObservableFetch: jest.fn()
+}));
+
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useParams: jest.fn()
+}));
+
+describe("<HeightAndWeightDetailedSummary/>", () => {
+  let match: match = { params: {}, url: "/", isExact: false, path: "/" };
+  let patient: fhir.Patient = mockPatient;
+  beforeEach(() => {
+    mockOpenmrsObservableFetch.mockReset();
+    mockUseCurrentPatient.mockReset();
+  });
+
+  afterEach(cleanup);
+
+  it("render withour dying", () => {
+    mockOpenmrsObservableFetch.mockReturnValue(
+      of(mockDimensionResponseRESTAPI)
+    );
+    mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
+    <BrowserRouter>
+      <HeightAndWeightDetailedSummary
+        match={match}
+        uuid="bbd27a2f-442a-418a-9952-f2bb0e54df97"
+      ></HeightAndWeightDetailedSummary>
+    </BrowserRouter>;
+  });
+
+  it("should display the height, weight, bmi correctly", async () => {
+    mockOpenmrsObservableFetch.mockReturnValue(
+      of(mockDimensionResponseRESTAPI)
+    );
+    mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
+    const wrapper = render(
+      <BrowserRouter>
+        <HeightAndWeightDetailedSummary
+          match={match}
+          uuid="bbd27a2f-442a-418a-9952-f2bb0e54df97"
+        ></HeightAndWeightDetailedSummary>
+      </BrowserRouter>
+    );
+    await wait(() => {
+      expect(getByText(wrapper.container, "65").textContent).toBe("65 Kg");
+      expect(getByText(wrapper.container, "143.33").textContent).toBe(
+        "143.33 lbs"
+      );
+
+      expect(getByText(wrapper.container, "165").textContent).toBe("165 cm");
+      expect(
+        getByText(wrapper.container, content => content.startsWith("5"))
+          .textContent
+      ).toBe("5 feet 4.96 inches");
+      expect(getByText(wrapper.container, "23.9").textContent).toBe(
+        "23.9 Kg/m2"
+      );
+    });
+  });
+
+  it("should display error message when response is empty", async () => {
+    mockOpenmrsObservableFetch.mockReturnValue(
+      of(mockDimensionResponseRESTAPI)
+    );
+    mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
+    const wrapper = render(
+      <BrowserRouter>
+        <HeightAndWeightDetailedSummary
+          match={match}
+          uuid="bbd27a2f-442a-418a-9952-f2bb0e54df65"
+        ></HeightAndWeightDetailedSummary>
+      </BrowserRouter>
+    );
+
+    await wait(() => {
+      expect(
+        wrapper.getByText("The patient's Height and Weight is not documented.")
+      ).toBeTruthy();
+      expect(wrapper.getByText("add patient height and weight")).toBeTruthy();
+    });
+  });
+});
