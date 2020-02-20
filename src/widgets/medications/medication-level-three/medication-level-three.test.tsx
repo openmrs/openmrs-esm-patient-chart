c19ca7a9aa2ce366d6432a96d784c8072d5c61f1
Donald Kibet <chelashawdonald@yahoo.com>
2020-02-20 11:59:05 +0300

    Fix Medication Level Three failing test

--- src/widgets/medications/medication-level-three/medication-level-three.test.tsx
+++ src/widgets/medications/medication-level-three/medication-level-three.test.tsx
@@ -1,19 +1,38 @@
 import React from "react";
-
 import { render, cleanup, wait } from "@testing-library/react";
 import { BrowserRouter, match } from "react-router-dom";
 import MedicationCardLevelThree from "./medication-level-three.component";
+import { useRouteMatch } from "react-router";
+import { mockPatient } from "../../../../__mocks__/patient.mock";
+
+const mockUseReactRouterMatch = useRouteMatch as jest.Mock;
+
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useRouteMatch: jest.fn()
+}));
 
 describe("<MedicationCardLevelThree />", () => {
-  let match: match = { params: {}, isExact: false, path: "/", url: "/" };
+  let match: match = {
+    params: { medicationUuid: "bbd27a2f-442a-418a-9952-f2bb0e54df97" },
+    isExact: false,
+    path: "/",
+    url: "/"
+  };
+  let patient: fhir.Patient = mockPatient;
   let wrapper: any;
 
   afterEach(cleanup);
 
+  beforeEach(() => {
+    mockUseReactRouterMatch.mockReset();
+  });
+
   it("renders without dying", async () => {
+    mockUseReactRouterMatch.mockReturnValue(match);
     wrapper = render(
       <BrowserRouter>
-        <MedicationCardLevelThree match={match} />
+        <MedicationCardLevelThree />
       </BrowserRouter>
     );
 
