169d78f702d17959baaef9580a8565aeb7911141
Nicky Kibor Kimaina <nkimaina@ampath.or.ke>
2020-04-27 10:00:44 +0300

    Mf 172 (#153)
    * MF-172: Scrolling of nav bar header items
    
    * fixed styling for scrolling buttoms
    
    * Fixing broken build
    
    * fixing build
    
    * fixing build
    
    * fixed failing tests
    
    * responding to PR reviews

--- src/chart-review/chart-review.component.tsx
+++ src/chart-review/chart-review.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useReducer } from "react";
 import {
   Link,
   useParams,
@@ -27,6 +27,24 @@ export default function ChartReview(props: any) {
 
   const [selected, setSelected] = React.useState(getInitialTab());
   const [tabHistory, setTabHistory] = React.useState({});
+
+  // NAV HEADER
+  const [navHeaderOverflowed, setNavHeaderOverflowed] = React.useState(false);
+  enum PagerVisibility {
+    NONE,
+    LEFT,
+    RIGHT,
+    BOTH
+  }
+  const initialHeaderPagerVisibility = {
+    pagerVisibility: PagerVisibility.NONE
+  };
+
+  const navRef = useRef(null);
+  const navItemsRefs = navbarItems.reduce((acc, value) => {
+    acc[navbarItems.indexOf(value)] = React.createRef();
+    return acc;
+  }, {});
 
   function getInitialTab() {
     if (
@@ -65,39 +83,138 @@ export default function ChartReview(props: any) {
     setSelected(views.findIndex(element => element.path === "/" + viewPath));
   }, [views, viewPath]);
 
+  const headerPagerVisibilityReducer = (state, action) => {
+    if (!navHeaderOverflowed) {
+      return { pagerVisibility: PagerVisibility.NONE };
+    }
+    if (navRef.current.scrollLeft === 0) {
+      return { pagerVisibility: PagerVisibility.RIGHT };
+    }
+
+    let scrolledToRightEnd =
+      navRef.current.scrollLeft + navRef.current.clientWidth + 50 >=
+      navRef.current.scrollWidth;
+
+    if (scrolledToRightEnd) {
+      return { pagerVisibility: PagerVisibility.LEFT };
+    }
+    return { pagerVisibility: PagerVisibility.BOTH };
+  };
+
+  const [headerPagerState, dispatch] = useReducer(
+    headerPagerVisibilityReducer,
+    initialHeaderPagerVisibility
+  );
+
+  const onNavHeaderScrolled = () => {
+    dispatch("scrolled");
+  };
+
+  const scrollToItem = id => {
+    if (navItemsRefs[id]) {
+      navItemsRefs[id].current.scrollIntoView({
+        behavior: "smooth",
+        block: "end"
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    scrollToItem(selected);
+  }, [selected]);
+
+  const nextPage = () => {
+    navRef.current.scrollLeft =
+      navRef.current.scrollLeft + navRef.current.clientWidth;
+  };
+
+  const previousPage = () => {
+    navRef.current.scrollLeft =
+      navRef.current.scrollLeft - navRef.current.clientWidth;
+  };
+
+  const checkForOverflow = () => {
+    if (
+      navRef &&
+      navRef.current &&
+      navRef.current.scrollWidth - navRef.current.clientWidth > 0
+    ) {
+      if (!navHeaderOverflowed) {
+        setNavHeaderOverflowed(true);
+        dispatch("overflowed-changed");
+      }
+    } else {
+      if (navHeaderOverflowed) {
+        setNavHeaderOverflowed(false);
+        dispatch("overflowed-changed");
+      }
+    }
+  };
+  checkForOverflow();
   return (
     <>
-      <nav className={styles.topnav} style={{ marginTop: "0" }}>
-        <ul>
-          {navbarItems &&
-            navbarItems.map((item, index) => {
-              return (
-                <li key={index}>
-                  <div
-                    className={`${
-                      index === selected ? styles.selected : styles.unselected
-                    }`}
-                  >
-                    <Link
-                      to={
-                        tabHistory[item.path.substr(1)] ||
-                        defaultPath + item.path
-                      }
+      <div className={styles.navContainer}>
+        {(headerPagerState.pagerVisibility === PagerVisibility.LEFT ||
+          headerPagerState.pagerVisibility === PagerVisibility.BOTH) && (
+          <button
+            className={styles.leftHeaderPagination}
+            onClick={previousPage}
+          >
+            <svg
+              className="omrs-icon omrs-type-body-regular"
+              fill="var(--omrs-color-ink-medium-contrast)"
+            >
+              <use xlinkHref="#omrs-icon-arrow-back"></use>
+            </svg>
+          </button>
+        )}
+        <nav
+          onScroll={onNavHeaderScrolled}
+          ref={navRef}
+          className={styles.topnav}
+        >
+          <ul>
+            {navbarItems &&
+              navbarItems.map((item, index) => {
+                return (
+                  <li key={index} ref={navItemsRefs[index]}>
+                    <div
+                      className={`${
+                        index === selected ? styles.selected : styles.unselected
+                      }`}
                     >
-                      <button
-                        className="omrs-unstyled"
-                        onClick={() => setSelected(index)}
+                      <Link
+                        to={
+                          tabHistory[item.path.substr(1)] ||
+                          defaultPath + item.path
+                        }
                       >
-                        {item.label}
-                      </button>
-                    </Link>
-                  </div>
-                </li>
-              );
-            })}
-        </ul>
-      </nav>
-
+                        <button
+                          className="omrs-unstyled"
+                          id={"nav_item" + index}
+                          onClick={() => setSelected(index)}
+                        >
+                          {item.label}
+                        </button>
+                      </Link>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </nav>
+        {(headerPagerState.pagerVisibility === PagerVisibility.RIGHT ||
+          headerPagerState.pagerVisibility === PagerVisibility.BOTH) && (
+          <button className={styles.rightHeaderPagination} onClick={nextPage}>
+            <svg
+              className="omrs-icon omrs-type-body-regular"
+              fill="var(--omrs-color-ink-medium-contrast)"
+            >
+              <use xlinkHref="#omrs-icon-arrow-forward"></use>
+            </svg>
+          </button>
+        )}
+      </div>
       {views.length > 0 && (
         <Route exact path={defaultPath}>
           <Redirect to={defaultPath + views[0].path} />
