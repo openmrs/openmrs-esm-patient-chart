bf1ebeb2f093ba9d3054fc12c724c9d48eda8197
Brandon Istenes <bistenes@gmail.com>
2020-12-03 18:46:15 -0800

    Add slot to the top of every dashboard (#194)

--- src/view-components/dashboard/dashboard.component.tsx
+++ src/view-components/dashboard/dashboard.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./dashboard.css";
 import Widget, { WidgetConfig } from "../widget/widget.component";
+import { ExtensionSlot } from "@openmrs/esm-react-utils";
 
 export default function Dashboard(props: DashboardProps) {
   const [widgets, setWidgets] = React.useState<JSX.Element[]>([]);
@@ -27,32 +28,48 @@ export default function Dashboard(props: DashboardProps) {
   }
 
   return (
-    <div className={styles.container}>
-      <div
-        className={styles.dashboard}
-        style={{ gridTemplateColumns: getColumnsLayoutStyle() || 2 }}
-      >
-        {widgets.map((widget, index) => {
-          const widgetConfig = props.dashboardConfig.widgets[index];
-          let rows = (widgetConfig.layout && widgetConfig.layout.rowSpan) || 1;
-          let columns =
-            widgetConfig.layout && (widgetConfig.layout.columnSpan || 1);
-          return (
-            <div
-              key={widgetConfig.name}
-              className={styles.widgetContainer}
-              style={{
-                gridRow: `span ${rows}`,
-                gridColumn: `span ${columns}`
-              }}
-            >
-              {widget}
-            </div>
-          );
-        })}
+    <>
+      <div className={styles.container}>
+        <div className={styles.dashboard} style={{ gridTemplateColumns: 1 }}>
+          <ExtensionSlot
+            extensionSlotName={`patient-chart-${sanitize(
+              props.dashboardConfig.name
+            )}`}
+          />
+        </div>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div
+          className={styles.dashboard}
+          style={{ gridTemplateColumns: getColumnsLayoutStyle() || 2 }}
+        >
+          {widgets.map((widget, index) => {
+            const widgetConfig = props.dashboardConfig.widgets[index];
+            let rows =
+              (widgetConfig.layout && widgetConfig.layout.rowSpan) || 1;
+            let columns =
+              widgetConfig.layout && (widgetConfig.layout.columnSpan || 1);
+            return (
+              <div
+                key={widgetConfig.name}
+                className={styles.widgetContainer}
+                style={{
+                  gridRow: `span ${rows}`,
+                  gridColumn: `span ${columns}`
+                }}
+              >
+                {widget}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
+}
+
+function sanitize(name: string) {
+  return name.toLowerCase().replace(/\s/, "-");
 }
 
 export type DashboardProps = {
