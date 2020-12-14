1bc3d63c534cc9dd5e0262fc508135a64a8df5cd
Florian Rappl <florian.rappl@smapiot.com>
2020-12-14 01:27:16 +0100

    Improved use route match with specific base route

--- src/useUrlData.ts
+++ src/useUrlData.ts
@@ -1,11 +1,13 @@
 import { useRouteMatch } from "react-router-dom";
+import { basePath } from "./constants";
 
 interface RouteParams {
   patientUuid: string;
 }
 
 export function useUrlData() {
-  const match = useRouteMatch<RouteParams>();
+  const match = useRouteMatch<RouteParams>(basePath);
+
   return {
     basePath: match.url,
     patientUuid: match.params.patientUuid
