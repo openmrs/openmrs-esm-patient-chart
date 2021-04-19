59de73f10a36bc9a8c1bd1bbe810f149a3d63570
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2021-04-19 13:04:39 +0200

    initialize test results package

---
+++ packages/esm-patient-test-results-app/src/index.ts
@@ -0,0 +1,42 @@
+import { defineConfigSchema, getAsyncLifecycle } from "@openmrs/esm-framework";
+import { configSchema } from "./config-schema";
+import { backendDependencies } from "./openmrs-backend-dependencies";
+
+const realConsole = console;
+globalThis.console = new Proxy({} as typeof realConsole, {
+  get: () => () => {},
+  set: () => false
+});
+
+const importTranslation = require.context(
+  "../translations",
+  false,
+  /.json$/,
+  "lazy"
+);
+
+function setupOpenMRS() {
+  const moduleName = "@openmrs/esm-patient-test-results-app";
+
+  const options = {
+    featureName: "patient-test-results",
+    moduleName
+  };
+
+  defineConfigSchema(moduleName, configSchema);
+
+  return {
+    extensions: [
+      {
+        id: "test-results-summary-widget",
+        slot: "patient-chart-summary-dashboard-slot",
+        load: getAsyncLifecycle(() => import("./test"), options),
+        meta: {
+          columnSpan: 4
+        }
+      }
+    ]
+  };
+}
+
+export { backendDependencies, importTranslation, setupOpenMRS };
