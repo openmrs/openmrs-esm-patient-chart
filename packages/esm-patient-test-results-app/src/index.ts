a0f69a81aa403fa4d0f7ea951022335911671f1e
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-06 13:56:54 +0200

    Offlineified test-results.

--- packages/esm-patient-test-results-app/src/index.ts
+++ packages/esm-patient-test-results-app/src/index.ts
@@ -1,4 +1,10 @@
-import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmrs/esm-framework';
+import {
+  defineConfigSchema,
+  fhirBaseUrl,
+  getAsyncLifecycle,
+  getSyncLifecycle,
+  messageOmrsServiceWorker,
+} from '@openmrs/esm-framework';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { configSchema } from './config-schema';
 import { backendDependencies } from './openmrs-backend-dependencies';
@@ -7,6 +13,11 @@ import { dashboardMeta } from './dashboard.meta';
 const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 function setupOpenMRS() {
+  messageOmrsServiceWorker({
+    type: 'registerDynamicRoute',
+    pattern: `.+${fhirBaseUrl}/Observation.+`,
+  });
+
   const moduleName = '@openmrs/esm-patient-test-results-app';
 
   const options = {
@@ -25,17 +36,23 @@ function setupOpenMRS() {
         meta: {
           columnSpan: 2,
         },
+        online: true,
+        offline: true,
       },
       {
         id: 'test-results-dashboard-widget',
         slot: dashboardMeta.slot,
         load: getAsyncLifecycle(() => import('./desktopView/index'), options),
+        online: true,
+        offline: true,
       },
       {
         id: 'test-results-summary-dashboard',
         slot: 'patient-chart-dashboard-slot',
         load: getSyncLifecycle(createDashboardLink(dashboardMeta), options),
         meta: dashboardMeta,
+        online: true,
+        offline: true,
       },
     ],
   };
