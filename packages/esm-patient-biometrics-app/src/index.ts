e153abdf7906e1f76aabf5ee5bc68efdaf1642e8
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-06 10:53:11 +0200

    Offlineified biometrics.

--- packages/esm-patient-biometrics-app/src/index.ts
+++ packages/esm-patient-biometrics-app/src/index.ts
@@ -1,4 +1,10 @@
-import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmrs/esm-framework';
+import {
+  defineConfigSchema,
+  fhirBaseUrl,
+  getAsyncLifecycle,
+  getSyncLifecycle,
+  messageOmrsServiceWorker,
+} from '@openmrs/esm-framework';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { configSchema } from './config-schema';
 import { dashboardMeta } from './dashboard.meta';
@@ -7,6 +13,16 @@ import { backendDependencies } from './openmrs-backend-dependencies';
 const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 function setupOpenMRS() {
+  messageOmrsServiceWorker({
+    type: 'registerDynamicRoute',
+    pattern: `.+${fhirBaseUrl}/Observation.+`,
+  });
+
+  messageOmrsServiceWorker({
+    type: 'registerDynamicRoute',
+    pattern: `.+/ws/rest/v1/concept.+`,
+  });
+
   const moduleName = '@openmrs/esm-patient-biometrics-app';
 
   const options = {
@@ -25,12 +41,16 @@ function setupOpenMRS() {
         meta: {
           columnSpan: 2,
         },
+        online: { showAddBiometrics: true },
+        offline: { showAddBiometrics: false },
       },
       {
         id: 'results-summary-dashboard',
         slot: 'patient-chart-dashboard-slot',
         load: getSyncLifecycle(createDashboardLink(dashboardMeta), options),
         meta: dashboardMeta,
+        online: true,
+        offline: true,
       },
       {
         id: 'biometrics-details-widget',
@@ -40,6 +60,8 @@ function setupOpenMRS() {
           view: 'biometrics',
           title: 'Biometrics',
         },
+        online: { showAddBiometrics: true },
+        offline: { showAddBiometrics: false },
       },
     ],
   };
