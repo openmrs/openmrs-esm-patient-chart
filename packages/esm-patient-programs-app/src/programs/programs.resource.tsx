84dc0acdcfa2bb4b9827afebf0e3e94cdd9ae4fd
Dennis Kigen <kigen.work@gmail.com>
2021-05-19 14:36:24 +0300

    Improved type annotations
    Adds improved type annotations to the programs form source.

--- packages/esm-patient-programs-app/src/programs/programs.resource.tsx
+++ packages/esm-patient-programs-app/src/programs/programs.resource.tsx
@@ -1,24 +1,38 @@
-import { openmrsObservableFetch, openmrsFetch } from '@openmrs/esm-framework';
+import { openmrsObservableFetch } from '@openmrs/esm-framework';
 import { map } from 'rxjs/operators';
-import { PatientProgram, Program, LocationData, SessionData } from '../types';
+import { LocationData, PatientProgram, Program } from '../types';
 
-export function fetchEnrolledPrograms(patientID: string) {
-  return openmrsObservableFetch<PatientProgram>(`/ws/rest/v1/programenrollment?patient=${patientID}`).pipe(
-    map(({ data }) => data['results'].sort((a, b) => (b.dateEnrolled > a.dateEnrolled ? 1 : -1))),
+export function fetchActiveEnrollments(patientID: string) {
+  return openmrsObservableFetch<{ results: Array<PatientProgram> }>(
+    `/ws/rest/v1/programenrollment?patient=${patientID}`,
+  ).pipe(
+    map(({ data }) =>
+      data.results.filter((res) => !res.dateCompleted).sort((a, b) => (b.dateEnrolled > a.dateEnrolled ? 1 : -1)),
+    ),
   );
 }
 
-export function fetchActiveEnrollments(patientID: string) {
-  return openmrsObservableFetch<Array<PatientProgram>>(`/ws/rest/v1/programenrollment?patient=${patientID}`).pipe(
-    map(({ data }) =>
-      data['results'].filter((res) => !res.dateCompleted).sort((a, b) => (b.dateEnrolled > a.dateEnrolled ? 1 : -1)),
-    ),
-  );
+export function fetchAvailablePrograms() {
+  return openmrsObservableFetch<{ results: Array<Program> }>(
+    `/ws/rest/v1/program?v=custom:(uuid,display,allWorkflows,concept:(uuid,display))`,
+  ).pipe(map(({ data }) => data.results));
+}
+
+export function fetchEnrolledPrograms(patientID: string) {
+  return openmrsObservableFetch<{ results: Array<PatientProgram> }>(
+    `/ws/rest/v1/programenrollment?patient=${patientID}`,
+  ).pipe(map(({ data }) => data.results.sort((a, b) => (b.dateEnrolled > a.dateEnrolled ? 1 : -1))));
 }
 
 export function getPatientProgramByUuid(programUuid: string) {
   return openmrsObservableFetch<PatientProgram>(`/ws/rest/v1/programenrollment/${programUuid}`).pipe(
     map(({ data }) => data),
+  );
+}
+
+export function fetchLocations() {
+  return openmrsObservableFetch<{ results: Array<LocationData> }>(`/ws/rest/v1/location?v=custom:(uuid,display)`).pipe(
+    map(({ data }) => data.results),
   );
 }
 
@@ -50,16 +64,4 @@ export function updateProgramEnrollment(payload, abortController) {
     body: { dateEnrolled, dateCompleted, location },
     signal: abortController.signal,
   });
-}
-
-export function fetchAvailablePrograms() {
-  return openmrsObservableFetch<Array<Program>>(
-    `/ws/rest/v1/program?v=custom:(uuid,display,allWorkflows,concept:(uuid,display))`,
-  ).pipe(map(({ data }) => data['results']));
-}
-
-export function fetchLocations() {
-  return openmrsObservableFetch<Array<LocationData>>(`/ws/rest/v1/location?v=custom:(uuid,display)`).pipe(
-    map(({ data }) => data['results']),
-  );
 }
