cc4dbf7668fc44a60394c1be178925017980eeb9
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2021-05-26 12:48:25 +0200

    add translations

--- packages/esm-patient-test-results-app/src/trendline/RangeSelector.tsx
+++ packages/esm-patient-test-results-app/src/trendline/RangeSelector.tsx
@@ -3,6 +3,7 @@ import Tab from 'carbon-components-react/es/components/Tab';
 import * as React from 'react';
 
 import styles from './trendline.scss';
+import { useTranslation } from 'react-i18next';
 
 export enum RangeTypes {
   DAY_1,
@@ -14,7 +15,7 @@ export enum RangeTypes {
   ALL,
 }
 
-export const deduceRange = (type: RangeTypes, upperRange: Date, lowerRange: Date): [Date, Date] | [] => {
+export function deduceRange(type: RangeTypes, upperRange: Date, lowerRange: Date): [Date, Date] | [] {
   switch (type) {
     case RangeTypes.DAY_1:
       return [new Date(Date.parse(upperRange.toString()) - 1 * 24 * 3600 * 1000), upperRange];
@@ -32,18 +33,19 @@ export const deduceRange = (type: RangeTypes, upperRange: Date, lowerRange: Date
     default:
       return [lowerRange, upperRange];
   }
-};
+}
 
 export const RangeSelector: React.FC<{ setRange: (range: RangeTypes) => void }> = ({ setRange }) => {
+  const { t } = useTranslation();
   return (
     <Tabs light selected={6} className={styles['range-tabs']}>
-      <Tab label="1 day" onClick={() => setRange(RangeTypes.DAY_1)}></Tab>
-      <Tab label="5 days" onClick={() => setRange(RangeTypes.DAY_5)}></Tab>
-      <Tab label="1 month" onClick={() => setRange(RangeTypes.MONTH_1)}></Tab>
-      <Tab label="6 months" onClick={() => setRange(RangeTypes.MONTH_6)}></Tab>
-      <Tab label="1 year" onClick={() => setRange(RangeTypes.YEAR_1)}></Tab>
-      <Tab label="5 years" onClick={() => setRange(RangeTypes.YEAR_5)}></Tab>
-      <Tab label="All" onClick={() => setRange(RangeTypes.ALL)}></Tab>
+      <Tab label={t('trendlineRangeSelector1Day', '1 day')} onClick={() => setRange(RangeTypes.DAY_1)} />
+      <Tab label={t('trendlineRangeSelector5Days', '5 days')} onClick={() => setRange(RangeTypes.DAY_5)} />
+      <Tab label={t('trendlineRangeSelector1Month', '1 month')} onClick={() => setRange(RangeTypes.MONTH_1)} />
+      <Tab label={t('trendlineRangeSelectorMonths', '6 months')} onClick={() => setRange(RangeTypes.MONTH_6)} />
+      <Tab label={t('trendlineRangeSelector1Year', '1 year')} onClick={() => setRange(RangeTypes.YEAR_1)} />
+      <Tab label={t('trendlineRangeSelector5Years', '5 years')} onClick={() => setRange(RangeTypes.YEAR_5)} />
+      <Tab label={t('trendlineRangeSelectorAll', 'All')} onClick={() => setRange(RangeTypes.ALL)} />
     </Tabs>
   );
 };
