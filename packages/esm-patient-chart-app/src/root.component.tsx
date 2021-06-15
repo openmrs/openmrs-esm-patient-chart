754724137159e131b2584eab83cc2bee5e4a6bb9
Donald Kibet <chelashawdonald@yahoo.com>
2021-06-15 16:05:08 +0300

    MF-624: Improve Workspace visual formatting (#291)
    * MF-624: Improve Workspace visual formatting
    
    * MF-624: Code Review

--- packages/esm-patient-chart-app/src/root.component.tsx
+++ packages/esm-patient-chart-app/src/root.component.tsx
@@ -4,13 +4,16 @@ import PatientChart from './ui-components/patient-chart.component';
 import SideMenu from './view-components/side-menu.component';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { basePath, dashboardPath, spaRoot } from './constants';
+import styles from './root.scss';
 
 export default function Root() {
   return (
     <BrowserRouter basename={spaRoot}>
-      <SideMenu />
-      <Route path={dashboardPath} component={PatientChart} />
-      <Route path={basePath} component={ContextWorkspace} />
+      <div className={styles.patientChartWrapper}>
+        <SideMenu />
+        <Route path={dashboardPath} component={PatientChart} />
+        <Route path={basePath} component={ContextWorkspace} />
+      </div>
     </BrowserRouter>
   );
 }
