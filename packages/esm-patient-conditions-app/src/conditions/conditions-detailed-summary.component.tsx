48b870c2ac3ad09670cb189d814f13e5bf3709f3
Dennis Kigen <kigen.work@gmail.com>
2021-06-21 22:34:55 +0300

    Conditions widget mods
    - Carbonize detailed summary widgets.
    - Restore tests.

--- packages/esm-patient-conditions-app/src/conditions/conditions-detailed-summary.component.tsx
+++ packages/esm-patient-conditions-app/src/conditions/conditions-detailed-summary.component.tsx
@@ -1,103 +1,133 @@
 import React from 'react';
 import dayjs from 'dayjs';
-import capitalize from 'lodash-es/capitalize';
-import ConditionsForm from './conditions-form.component';
-import styles from './conditions-detailed-summary.css';
-import { EmptyState, SummaryCard, openWorkspaceTab } from '@openmrs/esm-patient-common-lib';
-import { Link } from 'react-router-dom';
-import { useTranslation, Trans } from 'react-i18next';
-import { attach, createErrorHandler } from '@openmrs/esm-framework';
+import { useTranslation } from 'react-i18next';
+import Button from 'carbon-components-react/es/components/Button';
+import DataTableSkeleton from 'carbon-components-react/es/components/DataTableSkeleton';
+import DataTable, {
+  Table,
+  TableCell,
+  TableContainer,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from 'carbon-components-react/es/components/DataTable';
+import Add16 from '@carbon/icons-react/es/add/16';
+import styles from './conditions-detailed-summary.scss';
+import { attach } from '@openmrs/esm-framework';
+import { EmptyState, ErrorState } from '@openmrs/esm-patient-common-lib';
 import { Condition, performPatientConditionsSearch } from './conditions.resource';
+import { useConditionsContext } from './conditions.context';
+import { capitalize } from 'lodash-es';
 
-interface ConditionsDetailedSummaryProps {
-  basePath: string;
-  patient: fhir.Patient;
-}
-
-const ConditionsDetailedSummary: React.FC<ConditionsDetailedSummaryProps> = ({ patient, basePath }) => {
+const ConditionsDetailedSummary: React.FC = () => {
   const { t } = useTranslation();
-  const path = `${basePath}/details`;
+  const { patient } = useConditionsContext();
   const displayText = t('conditions', 'Conditions');
   const headerTitle = t('conditions', 'Conditions');
-  const [patientConditions, setPatientConditions] = React.useState<Array<Condition>>(null);
+  const [error, setError] = React.useState(null);
+  const [conditions, setConditions] = React.useState<Array<Condition>>(null);
 
   React.useEffect(() => {
     if (patient) {
-      const sub = performPatientConditionsSearch(patient.identifier[0].value).subscribe((conditions) => {
-        setPatientConditions(conditions);
-      }, createErrorHandler());
+      const sub = performPatientConditionsSearch(patient.identifier[0].value).subscribe(setConditions, setError);
 
       return () => sub.unsubscribe();
     }
   }, [patient]);
+
+  const headers = React.useMemo(
+    () => [
+      {
+        key: 'condition',
+        header: t('condition', 'Condition'),
+      },
+      {
+        key: 'onsetDateTime',
+        header: t('since', 'Since'),
+      },
+      {
+        key: 'status',
+        header: t('status', 'Status'),
+      },
+    ],
+    [t],
+  );
+
+  const tableRows: Array<Condition> = React.useMemo(() => {
+    return conditions?.map((condition) => {
+      return {
+        ...condition,
+        id: condition.id,
+        condition: condition.display,
+        onsetDateTime: dayjs(condition.onsetDateTime).format('MMM-YYYY'),
+        status: capitalize(condition.clinicalStatus),
+      };
+    });
+  }, [conditions]);
 
   const launchConditionsForm = React.useCallback(
     () => attach('patient-chart-workspace-slot', 'conditions-form-workspace'),
     [],
   );
 
+  const RenderConditionsSummary: React.FC = () => {
+    if (conditions.length) {
+      return (
+        <div>
+          <div className={styles.conditionsHeader}>
+            <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
+            <Button kind="ghost" renderIcon={Add16} iconDescription="Add conditions" onClick={launchConditionsForm}>
+              {t('add', 'Add')}
+            </Button>
+          </div>
+          <TableContainer>
+            <DataTable rows={tableRows} headers={headers} isSortable={true} size="short">
+              {({ rows, headers, getHeaderProps, getTableProps }) => (
+                <Table {...getTableProps()}>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map((header) => (
+                        <TableHeader
+                          className={`${styles.productiveHeading01} ${styles.text02}`}
+                          {...getHeaderProps({
+                            header,
+                            isSortable: header.isSortable,
+                          })}>
+                          {header.header?.content ?? header.header}
+                        </TableHeader>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.id}>
+                        {row.cells.map((cell) => (
+                          <TableCell key={cell.id}>{cell.value?.content ?? cell.value}</TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
+            </DataTable>
+          </TableContainer>
+        </div>
+      );
+    }
+    return <EmptyState displayText={displayText} headerTitle={headerTitle} launchForm={launchConditionsForm} />;
+  };
+
   return (
-    <div className="styles.conditionSummary">
-      {patientConditions?.length ? (
-        <SummaryCard
-          name={t('conditions', 'Conditions')}
-          styles={{ width: '100%' }}
-          addComponent
-          showComponent={() => openWorkspaceTab(ConditionsForm, `${t('conditionsForm', 'Conditions form')}`)}>
-          <table className={`omrs-type-body-regular ${styles.conditionTable}`}>
-            <thead>
-              <tr>
-                <td>
-                  <Trans i18nKey="condition">Condition</Trans>
-                </td>
-                <td>
-                  <Trans i18nKey="onsetDate">Onset date</Trans>
-                </td>
-                <td>
-                  <Trans i18nKey="status">Status</Trans>
-                </td>
-                <td></td>
-              </tr>
-            </thead>
-            <tbody>
-              {patientConditions.map((condition) => {
-                return (
-                  <React.Fragment key={condition.id}>
-                    <tr
-                      className={`${
-                        condition.clinicalStatus === 'active' ? `${styles.active}` : `${styles.inactive}`
-                      }`}>
-                      <td className="omrs-medium">{condition.display}</td>
-                      <td>
-                        <div className={`${styles.alignRight}`}>
-                          {condition.onsetDateTime ? dayjs(condition.onsetDateTime).format('MMM-YYYY') : '-'}
-                        </div>
-                      </td>
-                      <td>
-                        <div className={`${styles.centerItems} ${styles.alignLeft}`}>
-                          <span>{capitalize(condition.clinicalStatus)}</span>
-                        </div>
-                      </td>
-                      <td>
-                        {
-                          <Link to={`${path}/${condition.id}`}>
-                            <svg className="omrs-icon" fill="var(--omrs-color-ink-low-contrast)">
-                              <use xlinkHref="#omrs-icon-chevron-right" />
-                            </svg>
-                          </Link>
-                        }
-                      </td>
-                    </tr>
-                  </React.Fragment>
-                );
-              })}
-            </tbody>
-          </table>
-        </SummaryCard>
+    <>
+      {conditions ? (
+        <RenderConditionsSummary />
+      ) : error ? (
+        <ErrorState error={error} headerTitle={headerTitle} />
       ) : (
-        <EmptyState displayText={displayText} headerTitle={headerTitle} launchForm={launchConditionsForm} />
+        <DataTableSkeleton role="progressbar" />
       )}
-    </div>
+    </>
   );
 };
 
