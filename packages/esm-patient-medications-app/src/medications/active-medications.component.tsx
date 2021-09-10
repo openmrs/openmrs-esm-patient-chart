fbd0934bc50b03719405ce7acefac3ac2e74b586
Dennis Kigen <kigen.work@gmail.com>
2021-09-11 01:09:31 +0300

    MF-774: Add empty states to medications widget (#376)
    This PR adds empty states to both the Active and Past medications widgets. Additionally, this PR also:
    
    - Tweaks the header title of the medications widget card so it matches the style of the other widgets (as well as the designs).
    - Renames a SASS stylesheet.

--- packages/esm-patient-medications-app/src/medications/active-medications.component.tsx
+++ packages/esm-patient-medications-app/src/medications/active-medications.component.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { usePatientOrders } from '../utils/use-current-patient-orders.hook';
-import styles from './active-medications.component.scss';
+import styles from './active-medications.scss';
 import MedicationsDetailsTable from '../components/medications-details-table.component';
 import { Provider } from 'unistore/react';
 import { orderBasketStore } from './order-basket-store';
-import Add16 from '@carbon/icons-react/es/add/16';
-import Button from 'carbon-components-react/es/components/Button';
-import { attach } from '@openmrs/esm-framework';
 import DataTableSkeleton from 'carbon-components-react/es/components/DataTableSkeleton';
 
 interface ActiveMedicationsProps {
@@ -19,31 +16,21 @@ const ActiveMedications: React.FC<ActiveMedicationsProps> = ({ patientUuid, show
   const { t } = useTranslation();
   const [activePatientOrders] = usePatientOrders(patientUuid, 'ACTIVE');
 
-  const launchOrderBasket = () => attach('patient-chart-workspace-slot', 'order-basket-workspace');
-
   return (
     <Provider store={orderBasketStore}>
       {activePatientOrders ? (
         <div className={styles.activeMedicationContainer}>
-          <div className={styles.activeMedicationHeader}>
-            <h4 className={styles.title}>{t('activeMedications', 'Active Medications')}</h4>
-            {showAddMedications && (
-              <Button kind="ghost" renderIcon={Add16} iconDescription="Add notes" onClick={launchOrderBasket}>
-                {t('add', 'Add')}
-              </Button>
-            )}
-          </div>
-
           <MedicationsDetailsTable
+            title={t('activeMedications', 'Active Medications')}
             medications={activePatientOrders}
             showDiscontinueButton={true}
             showModifyButton={true}
             showReorderButton={false}
-            showAddNewButton={false}
+            showAddNewButton={true}
           />
         </div>
       ) : (
-        <DataTableSkeleton />
+        <DataTableSkeleton role="progressbar" />
       )}
     </Provider>
   );
