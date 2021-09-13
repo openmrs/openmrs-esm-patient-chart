3d4127731da546d62ee1f4bf0ab0dedb8ac098cb
Dennis Kigen <kigen.work@gmail.com>
2021-09-14 00:05:00 +0300

    MF-635: Biometrics and Vitals widgets toggle buttons (#379)
    * MF-635: Biometrics and Vitals widgets content switcher
    
    This PR modifies the table/chart toggle on the Biometrics and Vitals widgets to match the provided design.
    
    * Review feedback

--- packages/esm-patient-biometrics-app/src/biometrics/biometrics-base.component.tsx
+++ packages/esm-patient-biometrics-app/src/biometrics/biometrics-base.component.tsx
@@ -60,31 +60,34 @@ const RenderBiometrics: React.FC<RenderBiometricsProps> = ({
       <div className={styles.biometricsWidgetContainer}>
         <div className={styles.biometricsHeaderContainer}>
           <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
-          <div className={styles.toggleButtons}>
-            <Button
-              className={styles.toggle}
-              size="field"
-              hasIconOnly
-              kind={chartView ? 'ghost' : 'secondary'}
-              renderIcon={Table16}
-              iconDescription={t('tableView', 'Table View')}
-              onClick={() => setChartView(false)}
-            />
-            <Button
-              className={styles.toggle}
-              size="field"
-              kind={chartView ? 'secondary' : 'ghost'}
-              hasIconOnly
-              renderIcon={ChartLineSmooth16}
-              iconDescription={t('chartView', 'Chart View')}
-              onClick={() => setChartView(true)}
-            />
+          <div className={styles.biometricsHeaderActionItems}>
+            <div className={styles.toggleButtons}>
+              <Button
+                className={styles.toggle}
+                size="field"
+                hasIconOnly
+                kind={chartView ? 'ghost' : 'tertiary'}
+                renderIcon={Table16}
+                iconDescription={t('tableView', 'Table View')}
+                onClick={() => setChartView(false)}
+              />
+              <Button
+                className={styles.toggle}
+                size="field"
+                kind={chartView ? 'tertiary' : 'ghost'}
+                hasIconOnly
+                renderIcon={ChartLineSmooth16}
+                iconDescription={t('chartView', 'Chart View')}
+                onClick={() => setChartView(true)}
+              />
+            </div>
+            <span className={styles.divider}>|</span>
+            {showAddBiometrics && (
+              <Button kind="ghost" renderIcon={Add16} iconDescription="Add biometrics" onClick={launchBiometricsForm}>
+                {t('add', 'Add')}
+              </Button>
+            )}
           </div>
-          {showAddBiometrics && (
-            <Button kind="ghost" renderIcon={Add16} iconDescription="Add biometrics" onClick={launchBiometricsForm}>
-              {t('add', 'Add')}
-            </Button>
-          )}
         </div>
         {chartView ? (
           <BiometricsChart patientBiometrics={biometrics} conceptsUnits={conceptsUnits} />
--- packages/esm-patient-vitals-app/src/vitals/vitals-overview.component.tsx
+++ packages/esm-patient-vitals-app/src/vitals/vitals-overview.component.tsx
@@ -105,35 +105,38 @@ const VitalsOverview: React.FC<VitalsOverviewProps> = ({ patientUuid, showAddVit
             <div className={styles.vitalsWidgetContainer}>
               <div className={styles.vitalsHeaderContainer}>
                 <h4>{headerTitle}</h4>
-                <div className={styles.toggleButtons}>
-                  <Button
-                    className={styles.toggle}
-                    size="field"
-                    kind={chartView ? 'ghost' : 'secondary'}
-                    hasIconOnly
-                    renderIcon={Table16}
-                    iconDescription={t('tableView', 'Table View')}
-                    onClick={() => setChartView(false)}
-                  />
-                  <Button
-                    className={styles.toggle}
-                    size="field"
-                    kind={chartView ? 'secondary' : 'ghost'}
-                    hasIconOnly
-                    renderIcon={ChartLineSmooth16}
-                    iconDescription={t('chartView', 'Chart View')}
-                    onClick={() => setChartView(true)}
-                  />
+                <div className={styles.vitalsHeaderActionItems}>
+                  <div className={styles.toggleButtons}>
+                    <Button
+                      className={styles.toggle}
+                      size="field"
+                      kind={chartView ? 'ghost' : 'tertiary'}
+                      hasIconOnly
+                      renderIcon={Table16}
+                      iconDescription={t('tableView', 'Table View')}
+                      onClick={() => setChartView(false)}
+                    />
+                    <Button
+                      className={styles.toggle}
+                      size="field"
+                      kind={chartView ? 'tertiary' : 'ghost'}
+                      hasIconOnly
+                      renderIcon={ChartLineSmooth16}
+                      iconDescription={t('chartView', 'Chart View')}
+                      onClick={() => setChartView(true)}
+                    />
+                  </div>
+                  <span className={styles.divider}>|</span>
+                  {showAddVitals && (
+                    <Button
+                      kind="ghost"
+                      renderIcon={Add16}
+                      iconDescription="Add vitals"
+                      onClick={launchVitalsBiometricsForm}>
+                      {t('add', 'Add')}
+                    </Button>
+                  )}
                 </div>
-                {showAddVitals && (
-                  <Button
-                    kind="ghost"
-                    renderIcon={Add16}
-                    iconDescription="Add vitals"
-                    onClick={launchVitalsBiometricsForm}>
-                    {t('add', 'Add')}
-                  </Button>
-                )}
               </div>
               {chartView ? (
                 <VitalsChart patientVitals={vitals} conceptsUnits={conceptsUnits} />
