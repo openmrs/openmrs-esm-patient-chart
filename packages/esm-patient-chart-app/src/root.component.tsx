d35a27241b16b3932b7aff37f8c15c80f2c202e9
Dennis Kigen <kigen.work@gmail.com>
2021-09-17 17:02:12 +0300

    Wrap patient chart in SWRConfig context (#382)
    * Wrap patient chart in SWRConfig context
    
    This PR adds the SWR dependency to the patient chart and wraps the `PatientChart` component in an SWRConfig context that
    specifies some sensible global configuration default options for all SWR hooks.
    
    * Review feedback

--- packages/esm-patient-chart-app/src/root.component.tsx
+++ packages/esm-patient-chart-app/src/root.component.tsx
@@ -6,17 +6,25 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { basePath, dashboardPath, spaRoot } from './constants';
 import styles from './root.scss';
 import { ContextWindowSizeProvider } from './hooks/useContextWindowSize';
+import { SWRConfig } from 'swr';
+
+const swrConfiguration = {
+  // Maximum number of retries when the backend returns an error
+  errorRetryCount: 3,
+};
 
 export default function Root() {
   return (
-    <BrowserRouter basename={spaRoot}>
-      <ContextWindowSizeProvider>
-        <div className={styles.patientChartWrapper}>
-          <SideMenu />
-          <Route path={dashboardPath} component={PatientChart} />
-          <Route path={basePath} component={ContextWorkspace} />
-        </div>
-      </ContextWindowSizeProvider>
-    </BrowserRouter>
+    <SWRConfig value={swrConfiguration}>
+      <BrowserRouter basename={spaRoot}>
+        <ContextWindowSizeProvider>
+          <div className={styles.patientChartWrapper}>
+            <SideMenu />
+            <Route path={dashboardPath} component={PatientChart} />
+            <Route path={basePath} component={ContextWorkspace} />
+          </div>
+        </ContextWindowSizeProvider>
+      </BrowserRouter>
+    </SWRConfig>
   );
 }
