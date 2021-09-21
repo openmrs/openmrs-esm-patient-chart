4b8353e0381e85040517bb8664abd48e4a75bdc8
Vineet Sharma <sharmava05@gmail.com>
2021-09-21 14:49:33 +0530

    lint fixup

--- packages/esm-patient-test-results-app/src/overview/common-overview.tsx
+++ packages/esm-patient-test-results-app/src/overview/common-overview.tsx
@@ -84,10 +84,9 @@ interface CommonOverviewPropsWithoutToolbar {
 
 type Only<T, U> = {
   [P in keyof T]: T[P];
-} &
-  {
-    [P in keyof U]?: never;
-  };
+} & {
+  [P in keyof U]?: never;
+};
 
 type Either<T, U> = Only<T, U> | Only<U, T>;
 
