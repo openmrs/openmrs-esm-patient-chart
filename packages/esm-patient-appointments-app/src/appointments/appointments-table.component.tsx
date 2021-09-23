73e65033366af7e917ed35f68002b32d18943039
Dennis Kigen <kigen.work@gmail.com>
2021-09-23 14:03:42 +0300

    Add swr to appointments widget

--- packages/esm-patient-appointments-app/src/appointments/appointments-table.component.tsx
+++ packages/esm-patient-appointments-app/src/appointments/appointments-table.component.tsx
@@ -1,5 +1,4 @@
 import React, { useMemo } from 'react';
-import styles from './appointment-table.component.scss';
 import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { Appointment } from '../types';
@@ -16,6 +15,7 @@ import {
 } from 'carbon-components-react';
 import { PatientChartPagination } from '@openmrs/esm-patient-common-lib';
 import { usePagination } from '@openmrs/esm-framework';
+import styles from './appointments-table.component.scss';
 
 const pageSize = 5;
 
@@ -23,10 +23,11 @@ interface AppointmentTableProps {
   patientAppointments: Array<Appointment>;
 }
 
-const AppointmentTable: React.FC<AppointmentTableProps> = ({ patientAppointments = [] }) => {
+const AppointmentsTable: React.FC<AppointmentTableProps> = ({ patientAppointments }) => {
   const { t } = useTranslation();
-  const { results, currentPage, goTo } = usePagination(patientAppointments, pageSize);
-  const tableHeader: Array<DataTableHeader> = useMemo(
+  const { results: paginatedAppointments, currentPage, goTo } = usePagination(patientAppointments, pageSize);
+
+  const tableHeaders: Array<DataTableHeader> = useMemo(
     () => [
       { key: 'date', header: t('date', 'Date') },
       { key: 'location', header: t('location', 'Location') },
@@ -37,21 +38,21 @@ const AppointmentTable: React.FC<AppointmentTableProps> = ({ patientAppointments
 
   const tableRows = useMemo(
     () =>
-      results.map((appointment) => {
+      paginatedAppointments?.map((appointment) => {
         return {
-          id: `${appointment.uuid}`,
+          id: appointment.uuid,
           date: dayjs((appointment.startDateTime / 1000) * 1000).format('DD - MMM -YYYY , HH:MM'),
           location: appointment.location.name,
           service: appointment.service.name,
         };
       }),
-    [results],
+    [paginatedAppointments],
   );
 
   return (
     <div>
       <TableContainer>
-        <DataTable rows={tableRows} headers={tableHeader} isSortable={true} size="short">
+        <DataTable rows={tableRows} headers={tableHeaders} isSortable={true} size="short">
           {({ rows, headers, getHeaderProps, getTableProps }) => (
             <Table {...getTableProps()} useZebraStyles>
               <TableHead>
@@ -82,7 +83,7 @@ const AppointmentTable: React.FC<AppointmentTableProps> = ({ patientAppointments
         </DataTable>
       </TableContainer>
       <PatientChartPagination
-        currentItems={results.length}
+        currentItems={paginatedAppointments.length}
         totalItems={patientAppointments.length}
         onPageNumberChange={({ page }) => goTo(page)}
         pageNumber={currentPage}
@@ -92,4 +93,4 @@ const AppointmentTable: React.FC<AppointmentTableProps> = ({ patientAppointments
   );
 };
 
-export default AppointmentTable;
+export default AppointmentsTable;
--- packages/esm-patient-appointments-app/src/appointments/appointments.resource.tsx
+++ packages/esm-patient-appointments-app/src/appointments/appointments.resource.tsx
@@ -1,5 +1,49 @@
+import dayjs from 'dayjs';
+import useSWR from 'swr';
 import { openmrsFetch } from '@openmrs/esm-framework';
-import { Appointment, AppointmentPayload } from '../types';
+import { AppointmentPayload } from '../types';
+
+export function useAppointments(patientUuid: string, startDate: string, abortController: AbortController) {
+  const appointmentsUrl = `/ws/rest/v1/appointments/search`;
+
+  /* 
+    Disclaimer: SWR isn't meant to do POST requests for data fetching.
+    This works but isn't recommended.
+  */
+  const fetcher = () =>
+    openmrsFetch(appointmentsUrl, {
+      method: 'POST',
+      signal: abortController.signal,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        patientUuid: patientUuid,
+        startDate: startDate,
+      },
+    });
+
+  const { data, error, isValidating } = useSWR<{ data: any }, Error>(appointmentsUrl, fetcher);
+
+  const appointments = data?.data?.length
+    ? data.data.sort((a, b) => (b.startDateTime > a.startDateTime ? 1 : -1))
+    : null;
+
+  const pastAppointments = appointments?.filter((appointment) =>
+    dayjs((appointment.startDateTime / 1000) * 1000).isBefore(dayjs()),
+  );
+
+  const upcomingAppointments = appointments?.filter((appointment) =>
+    dayjs((appointment.startDateTime / 1000) * 1000).isAfter(dayjs()),
+  );
+
+  return {
+    data: data ? { pastAppointments, upcomingAppointments } : null,
+    isError: error,
+    isLoading: !data && !error,
+    isValidating,
+  };
+}
 
 export function createAppointment(appointment: AppointmentPayload, abortController: AbortController) {
   return openmrsFetch(`/ws/rest/v1/appointment`, {
@@ -9,20 +53,6 @@ export function createAppointment(appointment: AppointmentPayload, abortControll
       'Content-Type': 'application/json',
     },
     body: appointment,
-  });
-}
-
-export function getAppointments(patientUuid: string, startDate: string, abortController: AbortController) {
-  return openmrsFetch(`/ws/rest/v1/appointments/search`, {
-    method: 'POST',
-    signal: abortController.signal,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: {
-      patientUuid: patientUuid,
-      startDate: startDate,
-    },
   });
 }
 
@@ -37,6 +67,7 @@ export function getAppointmentServiceAll(abortController: AbortController) {
     signal: abortController.signal,
   });
 }
+
 export function getAppointmentService(abortController: AbortController, uuid) {
   return openmrsFetch(`/ws/rest/v1/appointmentService?uuid=` + uuid, {
     signal: abortController.signal,
