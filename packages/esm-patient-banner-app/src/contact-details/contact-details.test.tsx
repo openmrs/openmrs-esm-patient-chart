c8bd659afa720cd6f403582c67bfaa5ea5169656
Dennis Kigen <kigen.work@gmail.com>
2021-09-28 22:46:56 +0300

    Add swr to relationships widget

--- packages/esm-patient-banner-app/src/contact-details/contact-details.test.tsx
+++ packages/esm-patient-banner-app/src/contact-details/contact-details.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { fetchPatientRelationships } from './relationships.resource';
+import { screen } from '@testing-library/react';
+import { openmrsFetch } from '@openmrs/esm-framework';
+import { swrRender, waitForLoadingToFinish } from '../../../../tools/test-helpers';
 import ContactDetails from './contact-details.component';
-import { waitForLoadingToFinish } from '../../../../tools/test-helpers';
 
 const testProps = {
   address: [
@@ -21,39 +21,39 @@ const testProps = {
 
 const mockRelationships = [
   {
-    uuid: 1111,
-    display: 'Amanda Testerson',
-    relativeAge: 30,
-    relativeUuid: 2222,
-    relationshipType: 'Cousin',
+    display: 'Amanda is the Sibling of John',
+    uuid: '993bc79d-5ca5-4c76-b4b3-adf49e25bd0b',
+    personA: {
+      uuid: '07006bcb-91d4-4c57-a5f7-49751899d9b5',
+      display: '100ADT - Amanda Robinson',
+      person: {
+        age: 24,
+        display: 'Amanda Robinson',
+      },
+    },
+    personB: {
+      uuid: '8673ee4f-e2ab-4077-ba55-4980f408773e',
+      display: '100GEJ - John Wilson',
+      person: {
+        age: 49,
+        display: 'John Wilson',
+      },
+    },
+    relationshipType: {
+      uuid: '8d91a01c-c2cc-11de-8d13-0010c6dffd0f',
+      display: 'Sibling/Sibling',
+      description: 'Relationship between brother/sister, brother/brother, and sister/sister',
+      aIsToB: 'Sibling',
+      bIsToA: 'Sibling',
+    },
   },
 ];
 
-const mockFetchPatientRelationships = fetchPatientRelationships as jest.Mock;
-
-jest.mock('./relationships.resource', () => ({
-  fetchPatientRelationships: jest.fn(),
-}));
+const mockOpenmrsFetch = openmrsFetch as jest.Mock;
 
 describe('ContactDetails: ', () => {
-  it("shows the patient's contact details and relationships when available", async () => {
-    mockFetchPatientRelationships.mockResolvedValue(mockRelationships);
-
-    renderContactDetails();
-
-    await waitForLoadingToFinish();
-
-    screen.findByText('Relationships');
-    expect(screen.getByText('Address')).toBeInTheDocument();
-    expect(screen.getByText('Contact Details')).toBeInTheDocument();
-    expect(screen.getByText('Relationships')).toBeInTheDocument();
-    expect(screen.getByText('Amanda Testerson')).toBeInTheDocument();
-    expect(screen.getByText(/Cousin/i)).toBeInTheDocument();
-    expect(screen.getByText(/30 yrs/i)).toBeInTheDocument();
-  });
-
-  it('shows an empty state when relationships data is not available', async () => {
-    mockFetchPatientRelationships.mockResolvedValue([]);
+  it('renders an empty state view when relationships data is not available', async () => {
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: [] } });
 
     renderContactDetails();
 
@@ -63,8 +63,23 @@ describe('ContactDetails: ', () => {
     expect(screen.getByText('Relationships')).toBeInTheDocument();
     expect(screen.getByText('-')).toBeInTheDocument();
   });
+
+  it("renders the patient's address, contact details and relationships when available", async () => {
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: mockRelationships } });
+
+    renderContactDetails();
+
+    await waitForLoadingToFinish();
+
+    expect(screen.getByText('Address')).toBeInTheDocument();
+    expect(screen.getByText('Contact Details')).toBeInTheDocument();
+    expect(screen.getByText('Relationships')).toBeInTheDocument();
+    expect(screen.getByText(/Amanda Robinson/)).toBeInTheDocument();
+    expect(screen.getByText(/Sibling/i)).toBeInTheDocument();
+    expect(screen.getByText(/24 yrs/i)).toBeInTheDocument();
+  });
 });
 
 function renderContactDetails() {
-  render(<ContactDetails {...testProps} />);
+  swrRender(<ContactDetails {...testProps} />);
 }
