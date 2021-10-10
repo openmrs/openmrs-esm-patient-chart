4f83e97ddb28ac655106afda6ef2d8ae7abd0a7d
Dennis Kigen <kigen.work@gmail.com>
2021-10-10 22:56:08 +0300

    Fix broken tests

--- packages/esm-patient-vitals-app/src/vitals/vitals-header/vitals-header-title.test.tsx
+++ packages/esm-patient-vitals-app/src/vitals/vitals-header/vitals-header-title.test.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import dayjs from 'dayjs';
-dayjs.extend(isToday);
 import isToday from 'dayjs/plugin/isToday';
-import VitalsHeaderTitle from './vitals-header-title.component';
+dayjs.extend(isToday);
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import { PatientVitals } from '../vitals.resource';
-import { attach } from '@openmrs/esm-framework';
+import VitalsHeaderTitle from './vitals-header-title.component';
 
 const mockToggleView = jest.fn();
-const mockAttach = attach as jest.Mock;
 
 const mockVitals: PatientVitals = {
   id: 'bca4d5f1-ee6a-4282-a5ff-c8db12c4247c',
   date: new Date('12-Mar-2019'),
-  systolic: '120',
-  diastolic: '80',
-  temperature: ' 36.5',
-  oxygenSaturation: '88',
-  weight: '85',
-  height: '185',
-  bmi: '24.8',
-  respiratoryRate: '45',
+  systolic: 120,
+  diastolic: 80,
+  temperature: 36.5,
+  oxygenSaturation: 88,
+  weight: 85,
+  height: 185,
+  bmi: 24.8,
+  respiratoryRate: 45,
 };
 
 const testProps = {
@@ -31,6 +30,15 @@ const testProps = {
   view: 'Warning',
   vitals: mockVitals,
 };
+
+jest.mock('@openmrs/esm-patient-common-lib', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-patient-common-lib');
+
+  return {
+    ...originalModule,
+    launchPatientWorkspace: jest.fn(),
+  };
+});
 
 describe('VitalsHeaderTitle: ', () => {
   it("renders an empty state view when there's no vitals data to show", async () => {
@@ -65,8 +73,8 @@ describe('VitalsHeaderTitle: ', () => {
     const recordVitalsButton = screen.getByText(/Record Vitals/i);
     userEvent.click(recordVitalsButton);
 
-    expect(mockAttach).toHaveBeenCalled();
-    expect(mockAttach).toHaveBeenCalledWith('patient-chart-workspace-slot', 'patient-vitals-biometrics-form-workspace');
+    expect(launchPatientWorkspace).toHaveBeenCalled();
+    expect(launchPatientWorkspace).toHaveBeenCalledWith('patient-vitals-biometrics-form-workspace');
   });
 });
 
