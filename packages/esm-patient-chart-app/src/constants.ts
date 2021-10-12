c55223fcc30d5a3ecb5a75effc3e1b5c7d496a46
pirupius <piruville@gmail.com>
2021-10-12 14:17:28 +0300

    Add extension slot to context workspace header

--- packages/esm-patient-chart-app/src/constants.ts
+++ packages/esm-patient-chart-app/src/constants.ts
@@ -4,3 +4,4 @@ export const dashboardPath = `${basePath}/:view?/:subview?`;
 export const spaBasePath = `${window.spaBase}${basePath}`;
 export const moduleName = '@openmrs/esm-patient-chart-app';
 export const patientChartWorkspaceSlot = 'patient-chart-workspace-slot';
+export const patientChartWorkspaceHeaderSlot = 'patient-chart-workspace-header-slot';
