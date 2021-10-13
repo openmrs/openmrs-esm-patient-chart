ed5f375622c7e6edd017abd74603929b9c8e25be
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-10-13 12:41:07 +0200

    Improved usage of SWR hooks. Added isValidating span.

--- packages/esm-patient-forms-app/src/forms/forms.component.tsx
+++ packages/esm-patient-forms-app/src/forms/forms.component.tsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import FormView from './form-view.component';
 import styles from './forms.component.scss';
 import EmptyFormView from './empty-form.component';
-import { ContentSwitcher, Switch, DataTableSkeleton } from 'carbon-components-react';
+import { ContentSwitcher, Switch, DataTableSkeleton, InlineLoading } from 'carbon-components-react';
 import { ErrorState } from '@openmrs/esm-patient-common-lib';
 import { useTranslation } from 'react-i18next';
 import { useForms } from '../hooks/useForms';
 
-enum FormViewState {
-  recommended = 0,
-  completed,
-  all,
+const enum FormsCategory {
+  Recommended,
+  Completed,
+  All,
 }
 
 interface FormsProps {
@@ -24,64 +24,67 @@ interface FormsProps {
 const Forms: React.FC<FormsProps> = ({ patientUuid, patient, pageSize, pageUrl, urlLabel }) => {
   const { t } = useTranslation();
   const headerTitle = t('forms', 'Forms');
-  const [selectedFormView, setSelectedFormView] = useState<FormViewState>(FormViewState.all);
-  const { data, error } = useForms(patientUuid);
+  const [formsCategory, setFormsCategory] = useState(FormsCategory.All);
+  const { isValidating, data, error } = useForms(patientUuid);
+
+  if (!data && !error) {
+    return <DataTableSkeleton role="progressbar" rowCount={5} />;
+  }
+
+  if (error) {
+    return <ErrorState headerTitle={t('forms', 'Forms')} error={error} />;
+  }
+
+  if (data.length === 0) {
+    return <EmptyFormView action={t('noFormsAvailable', 'There are no Forms to display for this patient')} />;
+  }
 
   return (
-    <>
-      {!data && !error && <DataTableSkeleton rowCount={5} />}
-      {data && !error && (
-        <>
-          {data.length > 0 ? (
-            <div className={styles.formsWidgetContainer}>
-              <div className={styles.formsHeaderContainer}>
-                <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
-                <div className={styles.contextSwitcherContainer}>
-                  <ContentSwitcher
-                    className={styles.contextSwitcherWidth}
-                    onChange={(event) => setSelectedFormView(event.name as any)}
-                    selectedIndex={selectedFormView}>
-                    <Switch name={FormViewState.recommended} text="Recommended" />
-                    <Switch name={FormViewState.completed} text="Completed" />
-                    <Switch name={FormViewState.all} text="All" />
-                  </ContentSwitcher>
-                </div>
-              </div>
-              <div style={{ width: '100%' }}>
-                {selectedFormView === FormViewState.completed && (
-                  <FormView
-                    forms={data.filter((formInfo) => formInfo.associatedEncounters.length > 0)}
-                    patientUuid={patientUuid}
-                    patient={patient}
-                    pageSize={pageSize}
-                    pageUrl={pageUrl}
-                    urlLabel={urlLabel}
-                  />
-                )}
-                {selectedFormView === FormViewState.all && (
-                  <FormView
-                    forms={data}
-                    patientUuid={patientUuid}
-                    patient={patient}
-                    pageSize={pageSize}
-                    pageUrl={pageUrl}
-                    urlLabel={urlLabel}
-                  />
-                )}
-                {selectedFormView === FormViewState.recommended && (
-                  <EmptyFormView
-                    action={t('noRecommendedFormsAvailable', 'No recommended forms available at the moment')}
-                  />
-                )}
-              </div>
-            </div>
-          ) : (
-            <EmptyFormView action={t('noFormsAvailable', 'There are no Forms to display for this patient')} />
-          )}
-        </>
-      )}
-      {error && <ErrorState headerTitle={t('errorHeader', 'Forms Error')} error={error} />}
-    </>
+    <div className={styles.formsWidgetContainer}>
+      <div className={styles.formsHeaderContainer}>
+        <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
+        {isValidating ? (
+          <span>
+            <InlineLoading />
+          </span>
+        ) : null}
+        <div className={styles.contextSwitcherContainer}>
+          <ContentSwitcher
+            className={styles.contextSwitcherWidth}
+            onChange={(event) => setFormsCategory(event.name as any)}
+            selectedIndex={formsCategory}>
+            <Switch name={FormsCategory.Recommended} text="Recommended" />
+            <Switch name={FormsCategory.Completed} text="Completed" />
+            <Switch name={FormsCategory.All} text="All" />
+          </ContentSwitcher>
+        </div>
+      </div>
+      <div style={{ width: '100%' }}>
+        {formsCategory === FormsCategory.Completed && (
+          <FormView
+            forms={data.filter((formInfo) => formInfo.associatedEncounters.length > 0)}
+            patientUuid={patientUuid}
+            patient={patient}
+            pageSize={pageSize}
+            pageUrl={pageUrl}
+            urlLabel={urlLabel}
+          />
+        )}
+        {formsCategory === FormsCategory.All && (
+          <FormView
+            forms={data}
+            patientUuid={patientUuid}
+            patient={patient}
+            pageSize={pageSize}
+            pageUrl={pageUrl}
+            urlLabel={urlLabel}
+          />
+        )}
+        {formsCategory === FormsCategory.Recommended && (
+          <EmptyFormView action={t('noRecommendedFormsAvailable', 'No recommended forms available at the moment')} />
+        )}
+      </div>
+    </div>
   );
 };
 
