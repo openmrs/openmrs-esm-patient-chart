e983e315344d3dd2f085d221ca519b97bd8eb328
Brandon Istenes <bistenes@gmail.com>
2021-10-14 06:20:15 -0700

    Prettier

--- packages/esm-patient-test-results-app/src/overview/common-overview.tsx
+++ packages/esm-patient-test-results-app/src/overview/common-overview.tsx
@@ -84,9 +84,10 @@ interface CommonOverviewPropsWithoutToolbar {
 
 type Only<T, U> = {
   [P in keyof T]: T[P];
-} & {
-  [P in keyof U]?: never;
-};
+} &
+  {
+    [P in keyof U]?: never;
+  };
 
 type Either<T, U> = Only<T, U> | Only<U, T>;
 
