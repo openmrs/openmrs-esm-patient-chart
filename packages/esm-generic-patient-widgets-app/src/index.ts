dddc14577d214babdb87845579c7ec35b9d2d717
Brandon Istenes <bistenes@gmail.com>
2021-11-02 18:07:49 -0700

    Proof of concept: generic patient summary widget (#443)

---
+++ packages/esm-generic-patient-widgets-app/src/index.ts
@@ -0,0 +1,37 @@
+import { defineConfigSchema, getAsyncLifecycle } from '@openmrs/esm-framework';
+import { configSchema } from './config-schema';
+
+const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+
+const backendDependencies = {
+  fhir2: '^1.2.0',
+};
+
+const frontendDependencies = {
+  '@openmrs/esm-framework': process.env.FRAMEWORK_VERSION,
+};
+
+function setupOpenMRS() {
+  const moduleName = '@openmrs/esm-generic-patient-widgets-app';
+
+  const options = {
+    featureName: 'Generic widgets',
+    moduleName,
+  };
+
+  defineConfigSchema(moduleName, configSchema);
+
+  return {
+    extensions: [
+      {
+        id: 'obs-by-encounter-widget',
+        load: getAsyncLifecycle(() => import('./obs-switchable/obs-switchable.component'), options),
+        meta: {
+          columnSpan: 4,
+        },
+      },
+    ],
+  };
+}
+
+export { backendDependencies, frontendDependencies, importTranslation, setupOpenMRS };
