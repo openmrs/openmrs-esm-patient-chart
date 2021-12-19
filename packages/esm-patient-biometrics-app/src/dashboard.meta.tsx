86ba70540ffc1a9ef7f961de2ff7e88c9a389245
Donald Kibet <chelashawdonald@yahoo.com>
2021-12-19 14:04:59 +0300

    Change Vitals and Biometrics Detailed page to grid view (#474)

--- packages/esm-patient-biometrics-app/src/dashboard.meta.tsx
+++ packages/esm-patient-biometrics-app/src/dashboard.meta.tsx
@@ -1,6 +1,6 @@
 export const dashboardMeta = {
   name: 'vitalsAndBiometrics',
-  slot: 'patient-chart-results-dashboard-slot',
-  config: { type: 'tabs' },
+  slot: 'patient-chart-vitals-biometrics-dashboard-slot',
+  config: { columns: 1, type: 'grid' },
   title: 'Vitals & Biometrics',
 };
