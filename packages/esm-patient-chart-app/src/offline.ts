b0e19617e1f1e2bd85b0dbad58085e42474edacd
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-01-20 17:37:03 +0100

    Adapt FHIR mapping to use correct `id` prop.

--- packages/esm-patient-chart-app/src/offline.ts
+++ packages/esm-patient-chart-app/src/offline.ts
@@ -168,7 +168,7 @@ function mapPatientCreateFromPatientRegistrationToFhirPatient(patient: any = {})
   // https://github.com/openmrs/openmrs-module-fhir/blob/669b3c52220bb9abc622f815f4dc0d8523687a57/api/src/main/java/org/openmrs/module/fhir/api/util/FHIRPatientUtil.java#L36
   // https://github.com/openmrs/openmrs-esm-patient-management/blob/94e6f637fb37cf4984163c355c5981ea6b8ca38c/packages/esm-patient-search-app/src/patient-search-result/patient-search-result.component.tsx#L21
   return {
-    _id: patient.uuid,
+    id: patient.uuid,
     gender: genderMap[patient.person?.gender],
     birthDate: patient.person?.birthdate ?? patient.person?.birthdateEstimated,
     deceasedBoolean: patient.dead,
