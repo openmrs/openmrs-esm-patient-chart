aacc821cd89048b3212f1b3de890178eabad58d3
nanfuka <kalungi2k6@gmail.com>
2022-01-21 12:34:04 +0300

    O3-849: Add more tests to the deceased component

--- packages/esm-patient-banner-app/src/banner-tags/deceased-tag.test.tsx
+++ packages/esm-patient-banner-app/src/banner-tags/deceased-tag.test.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import DeceasedBannerTag from './deceased-tag.component';
 import { mockDeceasedPatient } from '../../../../__mocks__/mockDeceasedPatient';
 
 describe('deceasedTag', () => {
   it('renders a deceased tag in the patient banner for patients who died', () => {
     renderDeceasedBannerTag();
+    expect(screen.getByRole('button', { name: /Deceased/i })).toBeInTheDocument();
+
+  });
+
+  it('doesnot render Deceased tag for patients who are still alive', () => {
+    alivePatient();
+    expect(screen.getByRole('button', { name: /Deceased/i })).not.toBeInTheDocument();
   });
 });
 
-function renderDeceasedBannerTag() {
+const renderDeceasedBannerTag = () => {
   render(<DeceasedBannerTag patient={mockDeceasedPatient} />);
-}
+};
+const alivePatient = () => {
+  render(<DeceasedBannerTag patient={mockPatient} />);
+};
