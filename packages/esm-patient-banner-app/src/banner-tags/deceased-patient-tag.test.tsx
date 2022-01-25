16ac5c68de4b046241a0b674f67d03a73a365b2e
Dennis Kigen <kigen.work@gmail.com>
2022-01-25 15:49:23 +0300

    Enhanced deceased patient banner tag implementation
    Makes the following improvements:
    
    - Add a check for the `deceasedBoolean` property in the patient object. If truthy, proceed to render the component. Otherwise, return `null`. Presently, an error gets rendered in production because some patient objects do not have this property.
    - Simplify the type annotation for the `patient` prop by picking only the `deceasedBoolean` property.
    - Rename the component from `DeceasedBannerTag` to the more informative `DeceasedPatientBannerTag`.
    - Rename the extension from `deceased-tag` to `deceased-patient-tag`.
    - Add a missing translation string.

--- packages/esm-patient-banner-app/src/banner-tags/deceased-patient-tag.test.tsx
+++ packages/esm-patient-banner-app/src/banner-tags/deceased-patient-tag.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import DeceasedBannerTag from './deceased-tag.component';
+import DeceasedPatientBannerTag from './deceased-patient-tag.component';
 import { mockDeceasedPatient } from '../../../../__mocks__/mockDeceasedPatient';
 import { mockPatient } from '../../../../__mocks__/patient.mock';
 
-describe('deceasedTag', () => {
+describe('DeceasedPatientTag', () => {
   it('renders a deceased tag in the patient banner for patients who died', () => {
-    render(<DeceasedBannerTag patient={mockDeceasedPatient} />);
+    render(<DeceasedPatientBannerTag patient={mockDeceasedPatient} />);
 
     expect(screen.getByRole('button', { name: /Deceased/ })).toBeInTheDocument();
   });
 
   it('doesnot render Deceased tag for patients who are still alive', () => {
-    render(<DeceasedBannerTag patient={mockPatient} />);
+    render(<DeceasedPatientBannerTag patient={mockPatient} />);
 
     expect(screen.queryByRole('button', { name: /Deceased/ })).not.toBeInTheDocument();
   });
