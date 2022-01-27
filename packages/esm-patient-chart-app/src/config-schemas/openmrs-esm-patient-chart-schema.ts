57d9a748158e6b78b7f93424c4a9f45dde21377c
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-01-27 14:02:02 +0100

    Make the offline visit type UUID configurable.

--- packages/esm-patient-chart-app/src/config-schemas/openmrs-esm-patient-chart-schema.ts
+++ packages/esm-patient-chart-app/src/config-schemas/openmrs-esm-patient-chart-schema.ts
@@ -1,3 +1,13 @@
-export const esmPatientChartSchema = {};
+import { Type } from '@openmrs/esm-framework';
 
-export interface ChartConfig {}
+export const esmPatientChartSchema = {
+  offlineVisitTypeUuid: {
+    _type: Type.UUID,
+    _description: 'The UUID of the visit type to be used for the automatically created offline visits.',
+    _default: 'a22733fa-3501-4020-a520-da024eeff088',
+  },
+};
+
+export interface ChartConfig {
+  offlineVisitTypeUuid: string;
+}
