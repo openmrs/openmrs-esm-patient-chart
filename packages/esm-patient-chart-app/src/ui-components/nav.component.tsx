819d724c48c5c27e922edd3066ab2d17dbf5e336
Ian <52504170+ibacher@users.noreply.github.com>
2022-02-03 14:41:59 -0500

    Nav menu should use standard `usePatient()` hook (#536)

--- packages/esm-patient-chart-app/src/ui-components/nav.component.tsx
+++ packages/esm-patient-chart-app/src/ui-components/nav.component.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { ExtensionSlot } from '@openmrs/esm-framework';
+import React, { useMemo } from 'react';
+import { ExtensionSlot, usePatient } from '@openmrs/esm-framework';
 import { spaBasePath } from '../constants';
 
-function getPatientUuidFromUrl() {
-  const match = /\/patient\/([a-zA-Z0-9\-]+)\/?/.exec(location.pathname);
-  return match && match[1];
-}
-
 const PatientChartNavMenu: React.FC = () => {
-  const [patientUuid, setPatientUuid] = useState(getPatientUuidFromUrl);
-  const basePath = spaBasePath.replace(':patientUuid', patientUuid);
-
-  useEffect(() => {
-    const handler = () => {
-      const currentPatientUuid = getPatientUuidFromUrl();
-
-      if (currentPatientUuid !== patientUuid) {
-        setPatientUuid(currentPatientUuid);
-      }
-    };
-    window.addEventListener('popstate', handler);
-    return () => window.removeEventListener('popstate', handler);
-  }, [patientUuid]);
+  const { patientUuid } = usePatient();
+  const basePath = useMemo(() => spaBasePath.replace(':patientUuid', patientUuid), [patientUuid]);
 
   return <ExtensionSlot state={{ basePath }} extensionSlotName="patient-chart-dashboard-slot" />;
 };
