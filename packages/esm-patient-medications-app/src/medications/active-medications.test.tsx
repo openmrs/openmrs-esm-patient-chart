e76498949c97d84185faa4fbb0f220bbdb2b6737
Dennis Kigen <kigen.work@gmail.com>
2022-03-02 14:02:41 +0300

    (feat) Remove pagination from medications widget
    The original scope of this feature involved replacing the stock Carbon pagination used in the medications widget with our custom `PatientChartPagination` component.
    
    Instead, Ciaran's guidance is that the medications widget shouldn't have pagination controls anyway. This change is corroborated by the [designs](https://app.zeplin.io/project/60d59321e8100b0324762e05/screen/60d5e12ebc329c17022caf7e) as well.

--- packages/esm-patient-medications-app/src/medications/active-medications.test.tsx
+++ packages/esm-patient-medications-app/src/medications/active-medications.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { openmrsFetch } from '@openmrs/esm-framework';
+import { screen, within } from '@testing-library/react';
 import { mockPatient } from '../../../../__mocks__/patient.mock';
 import { mockDrugOrders } from '../../../../__mocks__/medication.mock';
 import { swrRender, waitForLoadingToFinish } from '../../../../tools/test-helpers';
-import { openmrsFetch } from '@openmrs/esm-framework';
 import ActiveMedications from './active-medications.component';
 
 const testProps = {
@@ -14,7 +14,41 @@ const testProps = {
 const mockOpenmrsFetch = openmrsFetch as jest.Mock;
 
 describe('ActiveMedications: ', () => {
-  test('renders the active medications widget', async () => {
+  test('renders an empty state view when there are no active medications to display', async () => {
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: [] } });
+
+    renderActiveMedications();
+
+    await waitForLoadingToFinish();
+
+    expect(screen.getByRole('heading', { name: /active medications/i })).toBeInTheDocument();
+    expect(screen.getByTitle(/empty data illustration/i)).toBeInTheDocument();
+    expect(screen.getByText(/There are no active medications to display for this patient/i)).toBeInTheDocument();
+    expect(screen.getByText(/Record active medications/i)).toBeInTheDocument();
+  });
+
+  test('renders an error state view if there is a problem fetching medications data', async () => {
+    const error = {
+      message: 'You are not logged in',
+      response: {
+        status: 401,
+        statusText: 'Unauthorized',
+      },
+    };
+
+    mockOpenmrsFetch.mockRejectedValueOnce(error);
+
+    renderActiveMedications();
+
+    await waitForLoadingToFinish();
+
+    expect(screen.queryByRole('table')).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /medications/i })).toBeInTheDocument();
+    expect(screen.getByText(/Error 401: Unauthorized/i)).toBeInTheDocument();
+    expect(screen.getByText(/Sorry, there was a problem displaying this information/i)).toBeInTheDocument();
+  });
+
+  test('renders a tabular overview of the active medications recorded for a patient', async () => {
     mockOpenmrsFetch.mockReturnValueOnce(mockDrugOrders);
 
     renderActiveMedications();
@@ -22,13 +56,24 @@ describe('ActiveMedications: ', () => {
     await waitForLoadingToFinish();
 
     expect(screen.getByRole('heading', { name: /active medications/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /previous page/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /next page/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /add/i })).toBeInTheDocument();
+
+    const table = screen.getByRole('table');
+    expect(screen.getByRole('table')).toBeInTheDocument();
 
     const expectedColumnHeaders = [/start date/, /details/];
     expectedColumnHeaders.forEach((header) => {
       expect(screen.getByRole('columnheader', { name: new RegExp(header, 'i') })).toBeInTheDocument();
     });
+
+    const expectedTableRows = [
+      /09-Sept-2021 -- Aspirin — 81mg — tablet DOSE 81 mg — oral — once daily — indefinite duration/,
+      /09-Sept-2021 -- Efavirenz — 600mg — tablet DOSE 600 mg — oral — once daily — indefinite duration/,
+    ];
+
+    expectedTableRows.map((row) =>
+      expect(within(table).getByRole('row', { name: new RegExp(row, 'i') })).toBeInTheDocument(),
+    );
   });
 });
 
