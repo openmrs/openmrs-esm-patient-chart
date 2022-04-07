1cfb81a7e72697a1ddb46ccc3b8473307d0b6068
Brandon Istenes <bistenes@gmail.com>
2022-04-07 08:47:45 -0700

    Prettier

--- packages/esm-patient-medications-app/src/config-schema.ts
+++ packages/esm-patient-medications-app/src/config-schema.ts
@@ -3,7 +3,8 @@ import { Type } from '@openmrs/esm-framework';
 export const configSchema = {
   durationUnitsConcept: {
     _type: Type.ConceptUuid,
-    _description: 'A concept which has Answers that are medication duration units (for example, days, weeks, or months).',
+    _description:
+      'A concept which has Answers that are medication duration units (for example, days, weeks, or months).',
     _default: '1732AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
   },
   careSettingUuid: {
@@ -12,7 +13,8 @@ export const configSchema = {
     _default: '6f0c9a92-6f24-11e3-af88-005056821db0',
   },
   daysDurationUnit: {
-    _description: 'The default medication duration unit is days. The concept for that medication duration unit is specified here.',
+    _description:
+      'The default medication duration unit is days. The concept for that medication duration unit is specified here.',
     uuid: {
       _type: Type.UUID,
       _default: '1072AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
