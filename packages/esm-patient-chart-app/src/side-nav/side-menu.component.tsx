0f58255302d8ae855a1c9ba8dbb1e592d9335ce9
Brandon Istenes <bistenes@gmail.com>
2022-04-22 16:07:29 -0700

    Start refactoring left nav

--- packages/esm-patient-chart-app/src/side-nav/side-menu.component.tsx
+++ packages/esm-patient-chart-app/src/side-nav/side-menu.component.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import styles from './side-menu.scss';
-import { SideNav, SideNavProps } from 'carbon-components-react';
-import { useLayoutType } from '@openmrs/esm-framework';
+import { SideNavProps } from 'carbon-components-react';
+import { LeftNavMenu, useLayoutType } from '@openmrs/esm-framework';
 import { isDesktop } from '../utils';
-import PatientChartNavMenu from './nav.component';
 
 interface SideMenuPanelProps extends SideNavProps {}
 
 const SideMenuPanel: React.FC<SideMenuPanelProps> = () => {
   const layout = useLayoutType();
 
-  return (
-    isDesktop(layout) && (
-      <SideNav expanded aria-label="Menu" className={styles.link}>
-        <PatientChartNavMenu />
-      </SideNav>
-    )
-  );
+  return isDesktop(layout) && <LeftNavMenu />;
 };
 
 export default SideMenuPanel;
