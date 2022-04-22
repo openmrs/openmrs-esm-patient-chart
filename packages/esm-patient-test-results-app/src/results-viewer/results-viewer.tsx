ff11879623c3e4489bd6317442c96e6572cdd38f
Vineet Sharma <sharmava05@gmail.com>
2022-04-22 11:06:02 +0530

    Added skeletons to the results viewer screen

--- packages/esm-patient-test-results-app/src/results-viewer/results-viewer.tsx
+++ packages/esm-patient-test-results-app/src/results-viewer/results-viewer.tsx
@@ -1,5 +1,15 @@
 import React, { useContext, useState } from 'react';
-import { Button, Column, ContentSwitcher, Grid, InlineLoading, Row, Switch } from 'carbon-components-react';
+import {
+  AccordionSkeleton,
+  Button,
+  Column,
+  ContentSwitcher,
+  DataTableSkeleton,
+  Grid,
+  InlineLoading,
+  Row,
+  Switch,
+} from 'carbon-components-react';
 import FilterSet, { FilterContext, FilterProvider } from '../filter';
 import GroupedTimeline, { useGetManyObstreeData } from '../grouped-timeline';
 import { ErrorState } from '@openmrs/esm-patient-common-lib';
@@ -29,11 +39,13 @@ const RoutedResultsViewer: React.FC<ResultsViewerProps> = ({ type, basePath, tes
   const { t } = useTranslation();
 
   if (loading) {
-    return <InlineLoading />;
+    return <LoadingResultsViewer patientUuid={patientUuid} testUuid={testUuid} type={type} basePath={basePath} />;
   }
+
   if (errors.length) {
     return <ErrorState error={errors[0]} headerTitle={t('dataLoadError', 'Data Load Error')} />;
   }
+
   if (!loading && !errors.length && roots?.length) {
     return (
       <FilterProvider roots={roots} testUuid={testUuid} type={type} basePath={basePath}>
@@ -148,6 +160,119 @@ const ResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, ty
           }
         >
           <FilterSet hideFilterSetHeader />
+        </TabletOverlay>
+      )}
+      {tablet && testUuid && type === 'trendline' && (
+        <TabletOverlay
+          headerText={t('trendline', 'Trendline')}
+          close={() => navigate({ to: testResultsBasePath(basePath) })}
+          buttonsGroup={<></>}
+        >
+          <Trendline patientUuid={patientUuid} conceptUuid={testUuid} basePath={basePath} />
+        </TabletOverlay>
+      )}
+    </>
+  );
+};
+
+const LoadingResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, type, testUuid }) => {
+  const { t } = useTranslation();
+  const tablet = useLayoutType() === 'tablet';
+  const [view, setView] = useState<viewOpts>('split');
+  const [leftContent, setLeftContent] = useState<panelOpts>('tree');
+  const [showTreeOverlay, setShowTreeOverlay] = useState<boolean>(false);
+  const expanded = view === 'full';
+
+  return (
+    <>
+      <Grid className={styles.resultsContainer}>
+        <Row className={styles.resultsHeader}>
+          <Column sm={12} lg={!tablet ? 5 : 12}>
+            <div className={styles.leftHeader}>
+              <h4 style={{ flexGrow: 1 }}>{t('results', 'Results')}</h4>
+              <div className={styles.leftHeaderActions}>
+                {tablet && (
+                  <Button
+                    size={tablet ? 'md' : 'sm'}
+                    kind="ghost"
+                    renderIcon={TreeViewAlt16}
+                    onClick={() => setShowTreeOverlay(true)}
+                    style={{
+                      marginRight: '1rem',
+                    }}
+                  >
+                    {t('showTreeButtonText', 'Show tree')}
+                  </Button>
+                )}
+                {!expanded && (
+                  <ContentSwitcher
+                    size={tablet ? 'lg' : 'md'}
+                    selectedIndex={1}
+                    onChange={(e) => setLeftContent(e.name as panelOpts)}
+                  >
+                    <Switch name="panel" text={t('panel', 'Panel')} />
+                    <Switch name="tree" text={t('tree', 'Tree')} />
+                  </ContentSwitcher>
+                )}
+              </div>
+            </div>
+          </Column>
+          {!tablet && (
+            <Column sm={12} lg={7}>
+              <div
+                className={styles.viewOptsContentSwitcherContainer}
+                style={{ display: 'flex', justifyContent: 'flex-end' }}
+              >
+                <ContentSwitcher
+                  size={tablet ? 'lg' : 'md'}
+                  style={{ maxWidth: '10rem' }}
+                  onChange={(e) => setView(e.name as viewOpts)}
+                  selectedIndex={expanded ? 1 : 0}
+                >
+                  <Switch name="split" text={t('split', 'Split')} />
+                  <Switch name="full" text={t('full', 'Full')} />
+                </ContentSwitcher>
+              </div>
+            </Column>
+          )}
+        </Row>
+        <Row className={styles.resultsViewer}>
+          {!tablet && (
+            <Column sm={16} lg={tablet || expanded ? 0 : 5} className={`${styles.columnPanel} ${styles.treeColumn}`}>
+              {leftContent === 'tree' && <AccordionSkeleton open count={4} align="start" />}
+              {leftContent === 'panel' && <DataTableSkeleton />}
+            </Column>
+          )}
+          <Column sm={16} lg={tablet || expanded ? 12 : 7} className={`${styles.columnPanel}`}>
+            {!tablet && testUuid && type === 'trendline' ? (
+              <Trendline
+                patientUuid={patientUuid}
+                conceptUuid={testUuid}
+                basePath={basePath}
+                showBackToTimelineButton
+              />
+            ) : (
+              <DataTableSkeleton />
+            )}
+          </Column>
+        </Row>
+      </Grid>
+      {tablet && showTreeOverlay && (
+        <TabletOverlay
+          headerText={t('tree', 'Tree')}
+          close={() => setShowTreeOverlay(false)}
+          buttonsGroup={
+            <>
+              <Button kind="secondary" size="lg" disabled>
+                {t('resetTreeText', 'Reset tree')}
+              </Button>
+              <Button kind="primary" size="lg" disabled>
+                {`${t('view', 'View')} ${t('resultsText', 'results')}`}
+              </Button>
+            </>
+          }
+        >
+          <AccordionSkeleton open count={4} align="start" />
         </TabletOverlay>
       )}
       {tablet && testUuid && type === 'trendline' && (
