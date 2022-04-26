c4a1837e222719661982129ab3842c238c9050bd
Brandon Istenes <bistenes@gmail.com>
2022-04-25 18:55:54 -0700

    Fix test

--- packages/esm-patient-chart-app/src/side-nav/side-menu.test.tsx
+++ packages/esm-patient-chart-app/src/side-nav/side-menu.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SideMenu from './side-menu.component';
 import { render } from '@testing-library/react';
-import { ExtensionSlot, useLayoutType } from '@openmrs/esm-framework';
+import { LeftNavMenu, useLayoutType } from '@openmrs/esm-framework';
 global.window.matchMedia = jest.fn().mockImplementation(() => {
   return {
     matches: false,
@@ -10,29 +10,19 @@ global.window.matchMedia = jest.fn().mockImplementation(() => {
   };
 });
 
-const mockExtensionSlot = ExtensionSlot as jest.Mock;
-mockExtensionSlot.mockImplementation(() => <div>rendered</div>);
+const mockUseLayoutType = useLayoutType as jest.Mock;
+const mockLeftNavMenu = LeftNavMenu as any as jest.Mock;
 
-jest.mock('@openmrs/esm-framework', () => {
-  const originalModule = jest.requireActual('@openmrs/esm-framework');
-
-  return {
-    ...originalModule,
-    useLayoutType: jest.fn(() => 'tablet'),
-    ExtensionSlot: jest.fn(() => <div>rendered</div>),
-  };
-});
-
-jest.mock('./side-menu.component.scss', () => ({}));
+mockLeftNavMenu.mockReturnValue('left nav menu');
 
 describe('sidemenu', () => {
   it('is rendered when viewport != tablet', () => {
-    (useLayoutType as jest.Mock).mockImplementationOnce(() => 'desktop');
-    expect(render(<SideMenu />).getByText('rendered')).toBeTruthy();
+    mockUseLayoutType.mockImplementationOnce(() => 'desktop');
+    expect(render(<SideMenu />).getByText(/left nav menu/)).toBeTruthy();
   });
 
   it('is not rendered when viewport == tablet', () => {
-    (useLayoutType as jest.Mock).mockImplementationOnce(() => 'tablet');
-    expect(render(<SideMenu />).queryAllByText('rendered')).toHaveLength(0);
+    mockUseLayoutType.mockImplementationOnce(() => 'tablet');
+    expect(render(<SideMenu />).queryAllByText(/left nav menu/)).toHaveLength(0);
   });
 });
