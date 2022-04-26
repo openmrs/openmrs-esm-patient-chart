09d53f6bdf57a6f2fcfd13425b3dd3fa82bf967f
Donald Kibet <chelashawdonald@yahoo.com>
2022-04-26 21:27:00 +0300

    O3-1269: Form names should try use display property (#679)

--- packages/esm-patient-forms-app/src/constants.ts
+++ packages/esm-patient-forms-app/src/constants.ts
@@ -1,5 +1,5 @@
 export const customFormRepresentation =
-  '(uuid,name,encounterType:(uuid,name),version,published,retired,resources:(uuid,name,dataType,valueReference))';
+  '(uuid,name,display,encounterType:(uuid,name),version,published,retired,resources:(uuid,name,dataType,valueReference))';
 export const customEncounterRepresentation = `custom:(uuid,encounterDatetime,encounterType:(uuid,name),form:${customFormRepresentation}`;
 
 export const formEncounterUrl = `/ws/rest/v1/form?v=custom:${customFormRepresentation}`;
