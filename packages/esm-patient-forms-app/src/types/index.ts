56ea6545a4b4dbe194b3de4fe0514781fe418a68
Pedro Sousa <psousa@icrc.org>
2022-04-28 10:59:47 +0100

    Treat encounter privilege as an object instead of string

--- packages/esm-patient-forms-app/src/types/index.ts
+++ packages/esm-patient-forms-app/src/types/index.ts
@@ -31,12 +31,17 @@ export interface EncounterWithFormRef {
   encounterDatetime: string;
   form?: FormEncounter;
 }
-
+export interface Privilege {
+  uuid: string;
+  name: string;
+  display?: string;
+  description?: string;
+}
 export interface EncounterType {
   uuid: string;
   name: string;
-  viewPrivilege: string;
-  editPrivilege: string;
+  viewPrivilege: Privilege;
+  editPrivilege: Privilege;
 }
 
 export interface ListResponse<T> {
