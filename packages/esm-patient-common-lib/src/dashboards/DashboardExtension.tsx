7be00e281e26fd18b80c21510570be531d181586
hadijahkyampeire <hadijah315@gmail.com>
2022-06-17 01:24:07 +0300

    Wrap dashboard extension in router. Use location from react-router

--- packages/esm-patient-common-lib/src/dashboards/DashboardExtension.tsx
+++ packages/esm-patient-common-lib/src/dashboards/DashboardExtension.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { last } from 'lodash';
+import { useLocation } from 'react-router-dom';
 import { ConfigurableLink } from '@openmrs/esm-framework';
+import styles from './dashboardextension.scss';
 
 export interface DashboardExtensionProps {
   title: string;
   basePath: string;
 }
 
-export const DashboardExtension = ({ title, basePath }: DashboardExtensionProps) => {
+export const DashboardExtension = ({ title, basePath, ...props }: DashboardExtensionProps) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navLink = useMemo(() => decodeURIComponent(last(currentPath.split('/'))), [location.pathname]);
+
+  const activeClassName = title === navLink ? styles.activeNavItem : styles.notActive;
+
   return (
-    <div key={title}>
+    <div key={title} className={activeClassName}>
       <ConfigurableLink to={`${basePath}/${encodeURIComponent(title)}`} className="bx--side-nav__link">
         {title}
       </ConfigurableLink>
--- packages/esm-patient-common-lib/src/dashboards/createDashboardLink.tsx
+++ packages/esm-patient-common-lib/src/dashboards/createDashboardLink.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { DashboardLinkConfig } from '../types';
 import { DashboardExtension } from './DashboardExtension';
 
 export const createDashboardLink = (db: DashboardLinkConfig) => {
   const Dashboard = ({ basePath }: { basePath: string }) => {
-    return <DashboardExtension title={db.title} basePath={basePath} />;
+    return (
+      <BrowserRouter>
+        <DashboardExtension title={db.title} basePath={basePath} />
+      </BrowserRouter>
+    );
   };
   return Dashboard;
 };
