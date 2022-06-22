85d175a2bfaaffee3a1f943290bd7bf199f666f8
Ian <52504170+ibacher@users.noreply.github.com>
2022-06-22 17:12:50 -0400

    (chore): Fix size-generator script (#726)
    See #725

--- tools/size-generator.mjs
+++ tools/size-generator.mjs
@@ -37,7 +37,7 @@ await setPackages();
 
 export const fileSizeReport = await generateFileSizeReport({
   log: process.argv.includes('--log'),
-  projectDirectoryUrl: new URL('../', import.meta.url),
+  directoryUrl: new URL('../', import.meta.url),
   manifestConfig,
   trackingConfig,
 });
