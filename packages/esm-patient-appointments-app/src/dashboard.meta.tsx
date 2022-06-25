8935dad7918dfc1b6e1290148f21dbbb88a7a1d5
Brandon Istenes <bistenes@gmail.com>
2022-06-25 11:55:39 +0200

    (feat) Add basic support for a multi-column layout for custom dashboards

--- packages/esm-patient-appointments-app/src/dashboard.meta.tsx
+++ packages/esm-patient-appointments-app/src/dashboard.meta.tsx
@@ -1,5 +1,5 @@
 export const dashboardMeta = {
   slot: 'patient-chart-appointments-dashboard-slot',
-  config: { columns: 1 },
+  columns: 1,
   title: 'Appointments',
 };
--- packages/esm-patient-programs-app/src/dashboard.meta.tsx
+++ packages/esm-patient-programs-app/src/dashboard.meta.tsx
@@ -1,5 +1,5 @@
 export const dashboardMeta = {
   slot: 'patient-chart-programs-dashboard-slot',
-  config: { columns: 1 },
+  columns: 1,
   title: 'Programs',
 };
