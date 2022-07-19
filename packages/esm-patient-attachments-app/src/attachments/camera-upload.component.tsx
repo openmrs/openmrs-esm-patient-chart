448dafa8b7e87e75a50d2839d24635135f3b27a8
Vineet Sharma <sharmava05@gmail.com>
2022-07-19 23:18:47 +0530

    Improved the UI for capturing the picture from camera

--- packages/esm-patient-attachments-app/src/attachments/camera-upload.component.tsx
+++ packages/esm-patient-attachments-app/src/attachments/camera-upload.component.tsx
@@ -6,6 +6,7 @@ import { showToast } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
 import { readFileAsString } from './utils';
 import 'react-html5-camera-photo/build/css/index.css';
+import { Button, Tab, TabContent, Tabs } from 'carbon-components-react';
 
 export interface CameraUploadProps {
   collectCaption?: boolean;
@@ -15,23 +16,33 @@ export interface CameraUploadProps {
 
 const CameraUpload: React.FC<CameraUploadProps> = ({ onSavePhoto, onTakePhoto, collectCaption = true }) => {
   const mediaStream = useRef<MediaStream | undefined>();
+  const [tab, setTab] = useState<'webcam' | 'upload'>('webcam');
   const [error, setError] = useState<Error>(undefined);
-  const [dataUri, setDataUri] = useState('');
+  const [dataUri, setDataUri] = useState([]);
   const { t } = useTranslation();
 
-  const clearCamera = useCallback(() => setDataUri(''), []);
+  console.log(dataUri);
+
+  const clearCamera = useCallback(() => setDataUri([]), []);
 
   const handleTakePhoto = useCallback(
     (dataUri: string) => {
-      setDataUri(dataUri);
+      setDataUri([dataUri]);
       onTakePhoto?.(dataUri);
     },
     [onTakePhoto],
   );
 
-  const upload = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    readFileAsString(e.target.files[0]).then(setDataUri);
-  }, []);
+  const upload = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      Object.values(e.target.files)?.forEach((file) =>
+        readFileAsString(file).then((file) => {
+          setDataUri((uriData) => [...uriData, file]);
+        }),
+      );
+    },
+    [setDataUri],
+  );
 
   const setMediaStream = useCallback((ms: MediaStream) => {
     mediaStream.current = ms;
@@ -58,32 +69,41 @@ const CameraUpload: React.FC<CameraUploadProps> = ({ onSavePhoto, onTakePhoto, c
     [onSavePhoto],
   );
 
+  if (dataUri.length) {
+    return (
+      <FilePreview
+        content={dataUri}
+        onCancelCapture={clearCamera}
+        onSaveFile={willSaveAttachment}
+        collectCaption={collectCaption}
+      />
+    );
+  }
+
   return (
     <div className={styles.cameraSection}>
-      <div className={styles.frameContent}>
-        {dataUri ? (
-          <FilePreview
-            content={dataUri}
-            onCancelCapture={clearCamera}
-            onSaveFile={willSaveAttachment}
-            collectCaption={collectCaption}
-          />
+      <h3 className={styles.paddedProductiveHeading03}>{t('addAttachment', 'Add Attachment')}</h3>
+      <Tabs className={styles.tabs}>
+        <Tab label={t('webcam', 'Webcam')} onClick={() => setTab('webcam')} />
+        <Tab label={t('uploadMedia', 'Upload media')} onClick={() => setTab('upload')} />
+      </Tabs>
+      <div className={styles.tabContent}>
+        {tab === 'webcam' ? (
+          !error && <Camera onTakePhoto={handleTakePhoto} onCameraStart={setMediaStream} onCameraError={setError} />
         ) : (
-          <>
-            {!error && <Camera onTakePhoto={handleTakePhoto} onCameraStart={setMediaStream} onCameraError={setError} />}
-            <div>
-              <label htmlFor="uploadFile" className={styles.choosePhotoOrPdf}>
-                {t('selectFile', 'Select local File instead')}
-              </label>
-              <input
-                type="file"
-                id="uploadFile"
-                accept="image/*, application/pdf"
-                className={styles.uploadFile}
-                onChange={upload}
-              />
-            </div>
-          </>
+          <div>
+            <label htmlFor="uploadFile" className={styles.choosePhotoOrPdf}>
+              {t('selectFile', 'Select local File instead')}
+            </label>
+            <input
+              type="file"
+              id="uploadFile"
+              accept="image/*, application/pdf"
+              className={styles.uploadFile}
+              onChange={upload}
+              multiple
+            />
+          </div>
         )}
       </div>
     </div>
--- packages/esm-patient-attachments-app/src/attachments/image-preview.component.tsx
+++ packages/esm-patient-attachments-app/src/attachments/image-preview.component.tsx
@@ -1,27 +1,71 @@
 import React, { SyntheticEvent, useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { UserHasAccess } from '@openmrs/esm-framework';
-import { Button, ButtonSet, TextInput } from 'carbon-components-react';
+import { Button, ButtonSet, TextArea, TextInput } from 'carbon-components-react';
 import styles from './image-preview.scss';
+
+interface FilePreviewContainerProps {
+  content: Array<string>;
+  collectCaption: boolean;
+  collectDescription?: boolean;
+  onSaveFile?(dataUri: string, caption: string, description?: string): void;
+  onCancelCapture?(): void;
+}
+
+const FilePreviewContainer: React.FC<FilePreviewContainerProps> = ({
+  content,
+  collectCaption,
+  collectDescription,
+  onSaveFile,
+  onCancelCapture,
+}) => {
+  const { t } = useTranslation();
+  const [currentFile, setCurrentFile] = useState(1);
+
+  const moveToNextFile = useCallback(() => {
+    if (currentFile < content.length) {
+      setCurrentFile(currentFile + 1);
+    }
+  }, [setCurrentFile, currentFile, content]);
+
+  return (
+    <>
+      <h3 className={styles.paddedProductiveHeading03}>
+        {t('addAttachment', 'Add Attachment')} {content.length > 1 && `(${currentFile} of ${content.length})`}
+      </h3>
+      <FilePreview
+        content={content[currentFile - 1]}
+        onCancelCapture={onCancelCapture}
+        onSaveFile={onSaveFile}
+        collectCaption={collectCaption}
+        moveToNextFile={moveToNextFile}
+      />
+    </>
+  );
+};
 
 interface FilePreviewProps {
   content: string;
   collectCaption: boolean;
-  onSaveFile?(dataUri: string, caption: string): void;
+  collectDescription?: boolean;
+  onSaveFile?(dataUri: string, caption: string, description?: string): void;
   onCancelCapture?(): void;
+  moveToNextFile: () => void;
 }
 
-export default function FilePreview(props: FilePreviewProps) {
+function FilePreview(props: FilePreviewProps) {
   const [saving, setSaving] = useState(false);
   const [caption, setCaption] = useState('');
+  const [description, setDescription] = useState('');
   const { t } = useTranslation();
 
   const saveImageOrPdf = useCallback(
     (e: SyntheticEvent) => {
       if (!saving) {
         e.preventDefault();
-        props.onSaveFile?.(props.content, caption);
+        props.onSaveFile?.(props.content, caption, description);
         setSaving(true);
+        props.moveToNextFile();
       }
     },
     [props.onSaveFile, saving, caption],
@@ -42,32 +86,54 @@ export default function FilePreview(props: FilePreviewProps) {
     setCaption(e.target.value);
   }, []);
 
+  const updateDescription = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    setDescription(e.target.value);
+  }, []);
+
   return (
-    <form className={styles.overview} onSubmit={saveImageOrPdf}>
-      <embed src={props.content} />
-      {props.collectCaption && (
-        <div className={styles.captionFrame}>
-          <TextInput
-            id="caption"
+    <form onSubmit={saveImageOrPdf}>
+      <div className={styles.overview}>
+        <img src={props.content} />
+        <div className={styles.imageDetails}>
+          {props.collectCaption && (
+            <div className={styles.captionFrame}>
+              <TextInput
+                id="caption"
+                autoFocus
+                labelText={t('imageName', 'Image name')}
+                autoComplete="off"
+                disabled={saving}
+                placeholder={t('attachmentCaptionInstruction', 'Enter caption')}
+                onChange={updateCaption}
+              />
+            </div>
+          )}
+          {/* {props.collectDescription && ( */}
+          <TextArea
+            id="description"
             autoFocus
-            labelText={``}
+            labelText={t('imageDescription', 'Image description')}
             autoComplete="off"
-            readOnly={saving}
+            disabled={saving}
             placeholder={t('attachmentCaptionInstruction', 'Enter caption')}
-            onChange={updateCaption}
+            onChange={updateDescription}
           />
+          {/* )} */}
         </div>
-      )}
+      </div>
       <UserHasAccess privilege="Create Attachment">
         <ButtonSet className={styles.buttonSetOverrides}>
-          <Button size="small" onClick={saveImageOrPdf} disabled={saving}>
-            {t('save', 'Save')}
-          </Button>
-          <Button kind="danger" size="small" onClick={cancelCapture} disabled={saving}>
+          <Button kind="secondary" size="lg" onClick={cancelCapture} disabled={saving}>
             {t('cancel', 'Cancel')}
+          </Button>
+          <Button size="lg" onClick={saveImageOrPdf} disabled={saving}>
+            {t('addAttachment', 'Add attachment')}
           </Button>
         </ButtonSet>
       </UserHasAccess>
     </form>
   );
 }
+
+export default FilePreviewContainer;
