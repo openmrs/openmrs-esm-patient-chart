72fbfafc445b2e7ddf7a18abb18fb2bb224449a2
Ian <ian_bacher@brown.edu>
2022-08-25 09:47:29 -0400

    (fix) add __VERSION__ constant for form-entry-app

--- packages/esm-form-entry-app/extra-webpack.config.js
+++ packages/esm-form-entry-app/extra-webpack.config.js
@@ -3,7 +3,12 @@ const { StatsWriterPlugin } = require('webpack-stats-plugin');
 const { basename } = require('path');
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
 
-const packageJson = require('./package.json');
+const {
+  version,
+  peerDependencies,
+  browser,
+  main,
+} = require('./package.json');
 
 function getFrameworkVersion() {
   try {
@@ -14,20 +19,23 @@ function getFrameworkVersion() {
   }
 }
 
+const mode = argv.mode || process.env.NODE_ENV || "development";
+
 module.exports = (angularWebpackConfig, options) => {
-  const filename = basename(packageJson.browser || packageJson.main);
+  const filename = basename(browser || main);
   const frameworkVersion = getFrameworkVersion();
   const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options);
 
   singleSpaWebpackConfig.output.filename = filename;
 
-  for (const dependency of Object.keys(packageJson.peerDependencies)) {
+  for (const dependency of Object.keys(peerDependencies)) {
     singleSpaWebpackConfig.externals.push(dependency);
   }
 
   singleSpaWebpackConfig.plugins.push(
     new IgnorePlugin(/^\.\/locale$/, /moment$/),
     new DefinePlugin({
+      __VERSION__: mode === production ? JSON.stringify(version) : JSON.stringify(inc(version, 'prerelease', 'local')),
       'process.env.FRAMEWORK_VERSION': JSON.stringify(frameworkVersion),
     }),
     new StatsWriterPlugin({
