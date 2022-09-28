a39bda2fd0a9e4fe97e23e5484c633ebed970ec4
jnsereko <nserekojowashi@gmail.com>
2022-09-28 15:16:27 +0300

    clean code

--- packages/esm-patient-chart-app/src/actions-buttons/mark-patient-deceased.tsx
+++ packages/esm-patient-chart-app/src/actions-buttons/mark-patient-deceased.tsx
@@ -1,27 +1,21 @@
 import React, { useCallback } from 'react';
+import { OverflowMenuItem } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 
 const MarkPatientDeceasedOverflowMenuItem = () => {
   const { t } = useTranslation();
-  const handleClick = useCallback(() => launchPatientWorkspace('mark-patient-deceased-workspace-form'), []);
+  const handleClick = launchPatientWorkspace('mark-patient-deceased-workspace-form');
 
   return (
     <>
-      <li className="cds--overflow-menu-options__option">
-        <button
-          className="cds--overflow-menu-options__btn"
-          role="menuitem"
-          title={t('markDeceased', 'Mark Deceased')}
-          data-floating-menu-primary-focus
-          onClick={handleClick}
-          style={{
-            maxWidth: '100vw',
-          }}
-        >
-          <span className="cds--overflow-menu-options__option-content">{t('Mark Deceased')}</span>
-        </button>
-      </li>
+      <OverflowMenuItem
+        itemText={t('markDeceased', 'Mark Deceased')}
+        onClick={handleClick}
+        style={{
+          maxWidth: '100vw',
+        }}
+      />
     </>
   );
 };
