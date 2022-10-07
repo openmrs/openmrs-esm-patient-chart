bdc332dca3a47755ac3a4d5ee4d3c0ea705ce0aa
Zac Butko <zac.butko@gmail.com>
2022-10-07 05:18:01 -0700

    Factor out EncountersTableLifecycle, add config schema

--- packages/esm-patient-chart-app/src/visit/visits-widget/encounters-table/encounters-table.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visits-widget/encounters-table/encounters-table.component.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import {
   DataTable,
   DataTableHeader,
-  Dropdown,
   Table,
   TableBody,
   TableCell,
@@ -18,11 +17,13 @@ import {
   TableToolbarContent,
   TableToolbarSearch,
   Tile,
+  InlineLoading,
 } from '@carbon/react';
-import { formatDatetime, formatTime, parseDate, useLayoutType, usePagination } from '@openmrs/esm-framework';
-import { PatientChartPagination } from '@openmrs/esm-patient-common-lib';
+import { formatDatetime, parseDate, useLayoutType, usePagination, ErrorState } from '@openmrs/esm-framework';
+import { EmptyState, PatientChartPagination } from '@openmrs/esm-patient-common-lib';
 import styles from './encounters-table.scss';
 import EncounterObservations from '../encounter-observations';
+import { useEncounters } from '../visit.resource';
 
 interface Encounter {
   datetime?: string;
@@ -162,4 +163,24 @@ const EncountersTable: React.FC<EncountersTableProps> = ({ showAllEncounters, en
   );
 };
 
+const EncountersTableLifecycle = ({ patientUuid }) => {
+  const { t } = useTranslation();
+  const { encounters, error, isLoading } = useEncounters(patientUuid);
+
+  if (isLoading) {
+    return <InlineLoading description={t('loading', 'Loading...')} role="progressbar" />;
+  }
+
+  if (error) {
+    return <ErrorState headerTitle={t('encounters', 'encounters')} error={error} />;
+  }
+
+  if (!encounters?.length) {
+    return <EmptyState headerTitle={t('encounters', 'encounters')} displayText={t('Encounters', 'Encounters')} />;
+  }
+
+  return <EncountersTable encounters={encounters} showAllEncounters />;
+};
+
 export default EncountersTable;
+export { EncountersTableLifecycle };
