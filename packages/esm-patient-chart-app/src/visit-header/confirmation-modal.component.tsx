37cb45fcb31700319129c262bbd112ee0597bdbd
hadijahkyampeire <hadijah315@gmail.com>
2022-11-17 00:15:56 +0300

    make some tweaks to extractions

--- packages/esm-patient-chart-app/src/visit-header/confirmation-modal.component.tsx
+++ packages/esm-patient-chart-app/src/visit-header/confirmation-modal.component.tsx
@@ -12,21 +12,31 @@ interface ConfirmationDialogProps {
 const ConfirmLeavingPatientChart: React.FC<ConfirmationDialogProps> = ({ closeDialog, leavePatientChart, props }) => {
   const { t } = useTranslation();
   const { type = '', title = '', additionalProps = {} } = props;
+  const workspaceTitle = additionalProps && additionalProps?.workspaceTitle;
 
   const unifiedTitle = title.includes('Form') ? title : `${title} Form`;
-  const confirmationText =
-    type === 'form'
-      ? additionalProps?.workspaceTitle
-        ? `The ${additionalProps?.workspaceTitle} form will not be saved if you exit the patient chart now.`
-        : 'The form will not be saved if you exit the patient chart now'
-      : null;
-
-  const confirmationHeading = type === 'form' ? `You haven't saved the ${unifiedTitle}` : null;
 
   return (
     <div>
-      <ModalHeader closeModal={closeDialog} title={t('openWorkspaceWarning', { confirmationHeading })} />
-      <ModalBody>{t('closePatientChartText', { confirmationText })}</ModalBody>
+      <ModalHeader
+        closeModal={closeDialog}
+        title={
+          type === 'form'
+            ? t('openFormWarningHeader', "You haven't saved the {formTitle}", { formTitle: unifiedTitle })
+            : t('openWorkspaceWarningHeader', "You haven't saved this workspace")
+        }
+      />
+      <ModalBody>
+        {type === 'form' && workspaceTitle
+          ? workspaceTitle
+            ? t(
+                'openFormSpecificWarningText',
+                'The {workspaceTitle} form will not be saved if you exit the patient chart now.',
+                { workspaceTitle },
+              )
+            : t('openFormWarningText', 'The form will not be saved if you exit the patient chart now.')
+          : t('openWorkspaceWarningText', 'The workspace will not be saved if you exit the patient chart now.')}
+      </ModalBody>
       <ModalFooter>
         <Button kind="secondary" onClick={closeDialog}>
           {t('no', 'No')}
