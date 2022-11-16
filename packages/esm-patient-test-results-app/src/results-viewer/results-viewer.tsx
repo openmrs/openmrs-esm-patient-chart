e65561f7bfa07d8ca4448bd4df8bf2f7db33c193
jwnasambu <julietwamalwa@yahoo.com>
2022-11-16 03:35:05 +0300

    fixed the marge conflicts

--- packages/esm-patient-test-results-app/src/results-viewer/results-viewer.tsx
+++ packages/esm-patient-test-results-app/src/results-viewer/results-viewer.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { AccordionSkeleton, Button, Column, ContentSwitcher, DataTableSkeleton, Grid, Switch } from '@carbon/react';
+import { AccordionSkeleton, Button, ContentSwitcher, DataTableSkeleton, Switch } from '@carbon/react';
 import { TreeViewAlt } from '@carbon/react/icons';
 import { EmptyState, ErrorState } from '@openmrs/esm-patient-common-lib';
 import { isDesktop, navigate, useConfig, useLayoutType } from '@openmrs/esm-framework';
@@ -10,20 +10,19 @@ import GroupedTimeline, { useGetManyObstreeData } from '../grouped-timeline';
 import TabletOverlay from '../tablet-overlay';
 import Trendline from '../trendline/trendline.component';
 import styles from './results-viewer.styles.scss';
-import { useParams } from 'react-router-dom';
-import PanelView from '../panel-view';
-import TreeViewWrapper from '../tree-view';
 
 type viewOpts = 'split' | 'full';
 type panelOpts = 'tree' | 'panel';
 
 interface ResultsViewerProps {
   basePath: string;
+  type?: string;
+  testUuid?: string;
   patientUuid?: string;
   loading?: boolean;
 }
 
-const RoutedResultsViewer: React.FC<ResultsViewerProps> = ({ basePath, patientUuid }) => {
+const RoutedResultsViewer: React.FC<ResultsViewerProps> = ({ type, basePath, testUuid, patientUuid }) => {
   const config = useConfig();
   const conceptUuids = config?.concepts?.map((c) => c.conceptUuid) ?? [];
   const { roots, loading, error } = useGetManyObstreeData(conceptUuids);
@@ -36,7 +35,13 @@ const RoutedResultsViewer: React.FC<ResultsViewerProps> = ({ basePath, patientUu
   if (roots?.length) {
     return (
       <FilterProvider roots={!loading ? roots : []}>
-        <ResultsViewer patientUuid={patientUuid} basePath={basePath} loading={loading} />
+        <ResultsViewer
+          patientUuid={patientUuid}
+          testUuid={testUuid}
+          type={type}
+          basePath={basePath}
+          loading={loading}
+        />
       </FilterProvider>
     );
   }
@@ -49,7 +54,7 @@ const RoutedResultsViewer: React.FC<ResultsViewerProps> = ({ basePath, patientUu
   );
 };
 
-const ResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, loading }) => {
+const ResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, type, testUuid, loading }) => {
   const { t } = useTranslation();
   const tablet = useLayoutType() === 'tablet';
   const [view, setView] = useState<viewOpts>('split');
@@ -57,45 +62,75 @@ const ResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, lo
   const [showTreeOverlay, setShowTreeOverlay] = useState<boolean>(false);
   const { resetTree, timelineData, totalResultsCount } = useContext(FilterContext);
   const expanded = view === 'full';
-  const { type, testUuid } = useParams();
+
+  const left = useRef(null);
+  const right = useRef(null);
+  const div = useRef(null);
+  let md = null;
+
+  const onMouseMove = (e) => {
+    const dx = e.clientX - md.e.clientX;
+    left.current.style.width = md.leftWidth + dx + 'px';
+    right.current.style.width = md.rightWidth - dx + 'px';
+  };
+
+  const onMouseDown = (e) => {
+    md = {
+      e: e,
+      leftWidth: left.current?.offsetWidth,
+      rightWidth: right.current?.offsetWidth,
+    };
+
+    document.onmousemove = (e) => onMouseMove(e);
+    document.onmouseup = () => {
+      document.onmousemove = document.onmouseup = md = null;
+    };
+  };
 
   return (
     <>
-      <Grid className={styles.resultsContainer}>
-        <Column className={styles.resultsHeader} sm={12} lg={!tablet ? 5 : 12}>
-          <div className={styles.leftHeader}>
-            <h4 style={{ flexGrow: 1 }}>{`${t('results', 'Results')} ${
-              totalResultsCount ? `(${totalResultsCount})` : ''
-            }`}</h4>
-            <div className={styles.leftHeaderActions}>
-              {tablet && (
-                <Button
-                  size={tablet ? 'md' : 'sm'}
-                  kind="ghost"
-                  renderIcon={(props) => <TreeViewAlt {...props} size={16} />}
-                  onClick={() => setShowTreeOverlay(true)}
-                  style={{
-                    marginRight: '1rem',
-                  }}
-                >
-                  {t('showTreeButtonText', 'Show tree')}
-                </Button>
-              )}
-              {!expanded && (
-                <ContentSwitcher
-                  size={tablet ? 'lg' : 'md'}
-                  selectedIndex={['panel', 'tree'].indexOf(leftContent)}
-                  onChange={(e) => setLeftContent(e.name as panelOpts)}
-                >
-                  <Switch name="panel" text={t('panel', 'Panel')} />
-                  <Switch name="tree" text={t('tree', 'Tree')} />
-                </ContentSwitcher>
-              )}
+      <div className={styles.resultsContainer}>
+        <div className={styles.Left + ' ' + styles.col} ref={left}>
+          <div className={styles.resultsHeader}>
+            <div className={styles.leftHeader}>
+              <h4 style={{ flexGrow: 1 }}>{`${t('results', 'Results')} ${
+                totalResultsCount ? `(${totalResultsCount})` : ''
+              }`}</h4>
+              <div className={styles.leftHeaderActions}>
+                {tablet && (
+                  <Button
+                    size={tablet ? 'md' : 'sm'}
+                    kind="ghost"
+                    renderIcon={(props) => <TreeViewAlt {...props} size={16} />}
+                    onClick={() => setShowTreeOverlay(true)}
+                    style={{
+                      marginRight: '1rem',
+                    }}
+                  >
+                    {t('showTreeButtonText', 'Show tree')}
+                  </Button>
+                )}
+                {!expanded && (
+                  <ContentSwitcher
+                    size={tablet ? 'lg' : 'md'}
+                    selectedIndex={1}
+                    onChange={(e) => setLeftContent(e.name as panelOpts)}
+                  >
+                    <Switch name="panel" text={t('panel', 'Panel')} disabled={loading} />
+                    <Switch name="tree" text={t('tree', 'Tree')} disabled={loading} />
+                  </ContentSwitcher>
+                )}
+              </div>
             </div>
           </div>
-        </Column>
-        {!tablet && (
-          <Column className={styles.resultsHeader} sm={12} lg={7}>
+          <div className={`${styles.columnPanel} ${styles.treeColumn}`}>
+            {leftContent === 'tree' && (!loading ? <FilterSet /> : <AccordionSkeleton open count={4} align="start" />)}
+            {leftContent === 'panel' && 'isDesktop'}
+          </div>
+        </div>
+        <div className={styles.dragHandler} ref={div} tabIndex={0} onMouseDown={onMouseDown}></div>
+        <div className={styles.Right + ' ' + styles.col} ref={right}>
+          {!tablet && (
             <div
               className={styles.viewOptsContentSwitcherContainer}
               style={{ display: 'flex', justifyContent: 'flex-end' }}
@@ -110,27 +145,23 @@ const ResultsViewer: React.FC<ResultsViewerProps> = ({ patientUuid, basePath, lo
                 <Switch name="full" text={t('full', 'Full')} disabled={loading} />
               </ContentSwitcher>
             </div>
-          </Column>
-        )}
-        {leftContent === 'tree' ? (
-          <TreeViewWrapper
-            patientUuid={patientUuid}
-            basePath={basePath}
-            type={type}
-            expanded={expanded}
-            testUuid={testUuid}
-          />
-        ) : (
-          <PanelView
-            expanded={expanded}
-            patientUuid={patientUuid}
-            basePath={basePath}
-            type={type}
-            testUuid={testUuid}
-          />
-        )}
-      </Grid>
-
+          )}
+          <div className={`${styles.columnPanel}`}>
+            {!tablet && testUuid && type === 'trendline' ? (
+              <Trendline
+                patientUuid={patientUuid}
+                conceptUuid={testUuid}
+                basePath={basePath}
+                showBackToTimelineButton
+              />
+            ) : !loading ? (
+              <GroupedTimeline />
+            ) : (
+              <DataTableSkeleton />
+            )}
+          </div>
+        </div>
+      </div>
       {tablet && showTreeOverlay && (
         <TabletOverlay
           headerText={t('tree', 'Tree')}
