bcaa149f987abbeafdeafb3f3f566a7a9d3f2f91
hadijahkyampeire <hadijah315@gmail.com>
2023-01-06 03:07:05 +0300

    add patient uuid to saved drug order

--- packages/esm-patient-chart-app/src/patient-chart/action-menu/action-menu.component.tsx
+++ packages/esm-patient-chart-app/src/patient-chart/action-menu/action-menu.component.tsx
@@ -3,17 +3,13 @@ import { ExtensionSlot } from '@openmrs/esm-framework';
 import styles from './action-menu.scss';
 
 interface ActionMenuInterface {
-  patientUuid: string;
+  open: boolean;
 }
 
-export const ActionMenu: React.FC<ActionMenuInterface> = ({ patientUuid }) => {
+export const ActionMenu: React.FC<ActionMenuInterface> = ({ open }) => {
   return (
     <aside className={styles.sideRail}>
-      <ExtensionSlot
-        className={styles.extensionStyles}
-        extensionSlotName={'action-menu-items-slot'}
-        state={{ patientUuid }}
-      />
+      <ExtensionSlot className={styles.extensionStyles} extensionSlotName={'action-menu-items-slot'} />
     </aside>
   );
 };
