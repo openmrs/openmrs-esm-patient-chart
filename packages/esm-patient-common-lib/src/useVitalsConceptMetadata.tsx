4b20b0a75c960d2896b6838a7702a8fcc751ff34
Jessie <104269786+Jexsie@users.noreply.github.com>
2023-02-28 16:19:43 +0300

    (feat) O3-1896: Number input steppers in vitals and biometrics form shouldn't exceed ranges (#1022)

--- packages/esm-patient-common-lib/src/useVitalsConceptMetadata.tsx
+++ packages/esm-patient-common-lib/src/useVitalsConceptMetadata.tsx
@@ -17,11 +17,25 @@ export function useVitalsConceptMetadata() {
   const conceptUnits = conceptMetadata?.length
     ? new Map<string, string>(conceptMetadata.map((concept) => [concept.uuid, concept.units]))
     : new Map<string, string>([]);
+
+  const conceptRanges = conceptMetadata?.length
+    ? new Map<string, { lowAbsolute: number | null; highAbsolute: number | null }>(
+        conceptMetadata.map((concept) => [
+          concept.uuid,
+          {
+            lowAbsolute: concept.lowAbsolute ?? null,
+            highAbsolute: concept.hiAbsolute ?? null,
+          },
+        ]),
+      )
+    : new Map<string, { lowAbsolute: number | null; highAbsolute: number | null }>([]);
+
   return {
     data: conceptUnits,
     isError: error,
     isLoading,
     conceptMetadata,
+    conceptRanges,
   };
 }
 
@@ -32,12 +46,12 @@ export const withUnit = (label: string, unit: string | null | undefined) => {
 export interface ConceptMetadata {
   uuid: string;
   display: string;
-  hiNormal: number | string | null;
-  hiAbsolute: number | string | null;
-  hiCritical: number | string | null;
-  lowNormal: number | string | null;
-  lowAbsolute: number | string | null;
-  lowCritical: number | string | null;
+  hiNormal: number | null;
+  hiAbsolute: number | null;
+  hiCritical: number | null;
+  lowNormal: number | null;
+  lowAbsolute: number | null;
+  lowCritical: number | null;
   units: string | null;
 }
 
