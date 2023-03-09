884fb76db1fcd58fc203152567595f0ff6163bee
hadijahkyampeire <hadijah315@gmail.com>
2023-03-09 22:10:17 +0300

    Do nothing when a form is open. Reopen the previous form when the workspace is hidden

--- packages/esm-patient-forms-app/src/clinical-form-action-button.component.tsx
+++ packages/esm-patient-forms-app/src/clinical-form-action-button.component.tsx
@@ -3,7 +3,7 @@ import { Button } from '@carbon/react';
 import { Document } from '@carbon/react/icons';
 import { useTranslation } from 'react-i18next';
 import { useLayoutType } from '@openmrs/esm-framework';
-import { useWorkspaces } from '@openmrs/esm-patient-common-lib';
+import { useWorkspaces, launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import useLaunchFormsWorkspace from './forms/use-launch-forms-workspace';
 import styles from './clinical-form-action-button.scss';
 
@@ -17,6 +17,7 @@ const ClinicalFormActionButton: React.FC = () => {
     workspaces?.[0]?.name?.match(/clinical-forms-workspace/i) ||
     workspaces?.[0]?.name?.match(/patient-form-entry-workspace/i);
 
+  const recentlyOpenedForm = workspaces.filter((w) => w.name === 'patient-form-entry-workspace')[0];
   const isFormOpen = workspaces.filter((w) => w.name === 'patient-form-entry-workspace')?.length >= 1;
 
   if (layout === 'tablet') {
@@ -25,8 +26,14 @@ const ClinicalFormActionButton: React.FC = () => {
         kind="ghost"
         className={`${styles.container} ${isActiveWorkspace ? styles.active : ''}`}
         tabIndex={0}
-        disabled={isFormOpen}
-        onClick={launchFormsWorkspace}
+        onClick={
+          isFormOpen
+            ? () =>
+                launchPatientWorkspace('patient-form-entry-workspace', {
+                  workspaceTitle: recentlyOpenedForm?.additionalProps?.['workspaceTitle'],
+                })
+            : launchFormsWorkspace
+        }
       >
         <Document size={16} />
         <span>{t('clinicalForm', 'Clinical form')}</span>
@@ -41,11 +48,17 @@ const ClinicalFormActionButton: React.FC = () => {
       renderIcon={(props) => <Document size={20} {...props} />}
       hasIconOnly
       iconDescription={t('form', 'Form')}
-      onClick={launchFormsWorkspace}
+      onClick={
+        isFormOpen
+          ? () =>
+              launchPatientWorkspace('patient-form-entry-workspace', {
+                workspaceTitle: recentlyOpenedForm?.additionalProps?.['workspaceTitle'],
+              })
+          : launchFormsWorkspace
+      }
       enterDelayMs={1000}
       tooltipAlignment="center"
       tooltipPosition="left"
-      disabled={isFormOpen}
       size="sm"
     />
   );
