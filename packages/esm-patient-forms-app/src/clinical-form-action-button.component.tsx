8fd5bbb0c571e2a5d506f076f1d5caf5fd0eb432
hadijahkyampeire <hadijah315@gmail.com>
2023-03-12 00:38:01 +0300

    some refactoring

--- packages/esm-patient-forms-app/src/clinical-form-action-button.component.tsx
+++ packages/esm-patient-forms-app/src/clinical-form-action-button.component.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { useLayoutType } from '@openmrs/esm-framework';
 import { useWorkspaces, launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import useLaunchFormsWorkspace from './forms/use-launch-forms-workspace';
+import { clinicalFormsWorkspace, formEntryWorkspace } from './constants';
 import styles from './clinical-form-action-button.scss';
 
 const ClinicalFormActionButton: React.FC = () => {
@@ -13,12 +14,25 @@ const ClinicalFormActionButton: React.FC = () => {
   const { workspaces } = useWorkspaces();
   const { launchFormsWorkspace } = useLaunchFormsWorkspace();
 
-  const isActiveWorkspace =
-    workspaces?.[0]?.name?.match(/clinical-forms-workspace/i) ||
-    workspaces?.[0]?.name?.match(/patient-form-entry-workspace/i);
+  const workspaceIndex = workspaces.findIndex(
+    (workspace) => workspace.name === clinicalFormsWorkspace || workspace.name === formEntryWorkspace,
+  );
+  const isActiveWorkspace = workspaceIndex === 0;
 
-  const recentlyOpenedForm = workspaces.filter((w) => w.name === 'patient-form-entry-workspace')[0];
-  const isFormOpen = workspaces.filter((w) => w.name === 'patient-form-entry-workspace')?.length >= 1;
+  const formEntryWorkspaces = workspaces.filter((w) => w.name === formEntryWorkspace);
+  const recentlyOpenedForm = formEntryWorkspaces[0];
+
+  const isClinicalFormOpen = formEntryWorkspaces?.length >= 1;
+
+  const launchWorkspace = () => {
+    if (isClinicalFormOpen) {
+      launchPatientWorkspace('patient-form-entry-workspace', {
+        workspaceTitle: recentlyOpenedForm?.additionalProps?.['workspaceTitle'],
+      });
+    } else {
+      launchFormsWorkspace();
+    }
+  };
 
   if (layout === 'tablet') {
     return (
@@ -26,14 +40,7 @@ const ClinicalFormActionButton: React.FC = () => {
         kind="ghost"
         className={`${styles.container} ${isActiveWorkspace ? styles.active : ''}`}
         tabIndex={0}
-        onClick={
-          isFormOpen
-            ? () =>
-                launchPatientWorkspace('patient-form-entry-workspace', {
-                  workspaceTitle: recentlyOpenedForm?.additionalProps?.['workspaceTitle'],
-                })
-            : launchFormsWorkspace
-        }
+        onClick={launchWorkspace}
       >
         <Document size={16} />
         <span>{t('clinicalForm', 'Clinical form')}</span>
@@ -48,14 +55,7 @@ const ClinicalFormActionButton: React.FC = () => {
       renderIcon={(props) => <Document size={20} {...props} />}
       hasIconOnly
       iconDescription={t('form', 'Form')}
-      onClick={
-        isFormOpen
-          ? () =>
-              launchPatientWorkspace('patient-form-entry-workspace', {
-                workspaceTitle: recentlyOpenedForm?.additionalProps?.['workspaceTitle'],
-              })
-          : launchFormsWorkspace
-      }
+      onClick={launchWorkspace}
       enterDelayMs={1000}
       tooltipAlignment="center"
       tooltipPosition="left"
