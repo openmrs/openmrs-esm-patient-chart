6a6784746a56d71a95fc9bf028e77ab854f622bd
Jose Francisco <josecarlos.francisco@emea.merkleinc.com>
2023-03-23 17:20:44 +0000

    i18n - Add support for i18n inside the form-entry-app

--- packages/esm-form-entry-app/src/app/app.module.ts
+++ packages/esm-form-entry-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core'; 
+import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { FeWrapperComponent } from './fe-wrapper/fe-wrapper.component';
@@ -17,7 +20,7 @@ import { ConfigResourceService } from './services/config-resource.service';
 import { LoaderComponent } from './loader/loader.component';
 import { SingleSpaPropsService } from './single-spa-props/single-spa-props.service';
 import { FormCreationService } from './form-creation/form-creation.service';
-import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { JsonLoader } from './loaders/json-loader';
 
 @NgModule({
   declarations: [AppComponent, EmptyRouteComponent, FeWrapperComponent, LoaderComponent],
@@ -25,6 +28,11 @@ import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translat
     BrowserModule,
     TranslateModule.forRoot({
       isolate: false,
+      loader: {
+        provide: TranslateLoader,
+        useClass: JsonLoader,
+        deps: [HttpClient],
+      },
     }),
     FormEntryModule,
     ReactiveFormsModule,
