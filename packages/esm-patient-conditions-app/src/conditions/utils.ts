4f9f9d85708c53ed23c476bd87bda6e4aa7b833a
hadijahkyampeire <hadijah315@gmail.com>
2023-03-27 20:32:41 +0300

    modify the compare function

--- packages/esm-patient-conditions-app/src/conditions/utils.ts
+++ packages/esm-patient-conditions-app/src/conditions/utils.ts
@@ -1,19 +1,23 @@
+import { parseDate } from '@openmrs/esm-framework';
 /**
  * Enables a comparison of arbitrary values with support for undefined/null.
  * Requires the `<` and `>` operators to return something reasonable for the provided values.
  */
-export function compare<T>(x?: T, y?: T) {
-  if (x == undefined && y == undefined) {
+export function compare<T extends string>(x?: T, y?: T) {
+  if (x === y || (!x && !y)) {
     return 0;
-  } else if (x == undefined) {
+  } else if (!x) {
     return -1;
-  } else if (y == undefined) {
-    return 1;
-  } else if (x < y) {
-    return -1;
-  } else if (x > y) {
+  } else if (!y) {
     return 1;
   } else {
-    return 0;
+    const xDate = parseDate(x);
+    const yDate = parseDate(y);
+
+    if (xDate === yDate) {
+      return 0;
+    }
+
+    return xDate < yDate ? -1 : 1;
   }
 }
