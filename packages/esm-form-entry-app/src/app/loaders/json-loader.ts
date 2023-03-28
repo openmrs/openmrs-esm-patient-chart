26c6545045c98fcc4576ba40e655c827e674fc11
Jose Francisco <josecarlos.francisco@emea.merkleinc.com>
2023-03-28 15:35:48 +0100

    i18n - prevent error when translate file is not provided.

--- packages/esm-form-entry-app/src/app/loaders/json-loader.ts
+++ packages/esm-form-entry-app/src/app/loaders/json-loader.ts
@@ -3,7 +3,20 @@ import { Observable, of } from 'rxjs';
 
 export class JsonLoader implements TranslateLoader {
   constructor() {}
+
   getTranslation(lang: string): Observable<any> {
-    return of(require(`../../../translations/${lang}.json`));
+    const jsonFile = `../../../translations/${lang}.json`;
+
+    // Check if the specified language file exists
+    try {
+      require.resolve(jsonFile);
+    } catch (error) {
+      // If the file does not exist, load the en.json file
+      console.warn(`Language file ${jsonFile} not found. Loading en.json instead.`);
+      return of(require(`../../../translations/en.json`));
+    }
+
+    // If the file exists, load it
+    return of(require(jsonFile));
   }
 }
