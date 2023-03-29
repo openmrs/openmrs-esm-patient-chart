ae7cfe90c80877641309358f798b40d6fc3c3383
Luis Oliveira <luis.oliveira@icrc.org>
2023-03-29 11:29:26 +0100

    Moved forms-workspace inside patient-forms

--- packages/esm-patient-forms-app/src/forms-workspace/form-list/forms-list.test.tsx
+++ packages/esm-patient-forms-app/src/forms-workspace/form-list/forms-list.test.tsx
@@ -1,222 +1,83 @@
 import React from 'react';
-import { screen, within } from '@testing-library/react';
-import userEvent from '@testing-library/user-event/';
+import ReactDOM from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import { useLayoutType, usePagination } from '@openmrs/esm-framework';
 import FormsList from './forms-list.component';
-import { mockCurrentVisit } from '../../../../../__mocks__/visits.mock';
-import { mockPatient } from '../../../../../__mocks__/patient.mock';
-import { renderWithSwr, waitForLoadingToFinish } from '../../../../../tools/test-helpers';
-import { openmrsFetch, useConfig } from '@openmrs/esm-framework';
+import { mockFormsSection } from '../../../../../__mocks__/forms-list.mock';
 
-const mockedOpenmrsFetch = openmrsFetch as jest.Mock;
-const mockedUseConfig = useConfig as jest.Mock;
+const testProps = {
+  patientUuid: 'patientUuid',
+  patient: { id: 'patientUuid' },
+  visit: {
+    uuid: '17f512b4-d264-4113-a6fe-160cb38cb46e',
+    encounters: [],
+    visitType: {
+      uuid: '7b0f5697-27e3-40c4-8bae-f4049abfb4ed',
+      name: 'Facility Visit',
+      display: 'Facility Visit',
+    },
+    startDatetime: '2021-03-16T08:16:00.000+0000',
+  },
+  formsSection: mockFormsSection,
+  searchTerm: '',
+  pageSize: 10000,
+  undefined,
+};
 
-it('renders an empty state if there are no forms persisted on the server', async () => {
-  mockedOpenmrsFetch.mockReturnValue({
-    data: { results: [] },
-  });
+const mockedUseLayoutType = useLayoutType as jest.Mock;
+const mockUsePagination = usePagination as jest.Mock;
 
-  renderFormsList();
+jest.mock('@openmrs/esm-framework', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-framework');
 
-  await waitForLoadingToFinish();
-
-  expect(screen.queryByRole('table')).not.toBeInTheDocument();
-  expect(screen.getByRole('heading', { name: /forms/i })).toBeInTheDocument();
-  expect(screen.getByText(/there are no forms to display/i)).toBeInTheDocument();
-});
-
-it('renders a list of forms fetched from the server', async () => {
-  const user = userEvent.setup();
-
-  mockedOpenmrsFetch.mockReturnValue({
-    data: { results: forms },
-  });
-
-  mockedUseConfig.mockImplementation(() => ({
-    showHtmlFormEntryForms: true,
-    showConfigurableForms: false,
-  }));
-
-  renderFormsList();
-
-  await waitForLoadingToFinish();
-
-  const searchbox = screen.getByRole('searchbox');
-  expect(searchbox).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /clear search input/i })).toBeInTheDocument();
-  expect(screen.getByRole('table')).toBeInTheDocument();
-
-  const expectedColumnHeaders = [/form name \(A-Z\)/, /last completed/];
-
-  expectedColumnHeaders.forEach((header) =>
-    expect(
-      within(screen.getByRole('table')).getByRole('columnheader', { name: new RegExp(header, 'i') }),
-    ).toBeInTheDocument(),
-  );
-
-  const expectedTableRows = [/laboratory tests never/, /surgical operation never/, /test form 1 never/];
-
-  expectedTableRows.map((row) =>
-    expect(within(screen.getByRole('table')).getByRole('row', { name: new RegExp(row, 'i') })).toBeInTheDocument(),
-  );
-
-  await user.type(searchbox, 'registration');
-
-  expect(screen.getByText(/no matching forms to display/i)).toBeInTheDocument();
-
-  await user.clear(searchbox);
-  await user.type(searchbox, 'lab');
-
-  expect(screen.getByRole('row', { name: /laboratory tests never/i })).toBeInTheDocument();
-});
-
-function renderFormsList() {
-  const testProps = {
-    currentVisit: mockCurrentVisit,
-    htmlFormEntryForms: [],
-    patient: mockPatient,
-    patientUuid: mockPatient.id,
+  return {
+    ...originalModule,
+    usePagination: jest.fn().mockImplementation(() => ({
+      currentPage: 1,
+      goTo: () => {},
+      results: mockFormsSection.completedFromsInfo,
+    })),
   };
+});
 
-  renderWithSwr(<FormsList {...testProps} />);
+let container;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('<FormsList/>', () => {
+  test('should display a list of forms', () => {
+    mockedUseLayoutType.mockReturnValue('tablet');
+
+    renderFormsDashboard();
+
+    expect(screen.getByRole('heading', { name: /Section Name/i })).toBeInTheDocument();
+    expect(screen.getByRole('columnheader', { name: /Last completed/i })).toBeInTheDocument();
+    expect(screen.getByRole('row', { name: /Display Form 1/i })).toBeInTheDocument();
+    expect(screen.getByRole('row', { name: /Display Form 2/i })).toBeInTheDocument();
+    expect(screen.getByRole('row', { name: /Display Form 3 01-Jan-2022, 10:00 AM/i })).toBeInTheDocument();
+  });
+});
+
+describe('<FormsList/>', () => {
+  test('should not display a list of forms', () => {
+    mockedUseLayoutType.mockReturnValue('tablet');
+    mockUsePagination.mockReturnValue([]);
+
+    renderFormsDashboard();
+
+    expect(screen.getByText(/There are no forms to display/i)).toBeInTheDocument();
+  });
+});
+
+function renderFormsDashboard() {
+  act(() => {
+    ReactDOM.createRoot(container).render(<FormsList {...testProps} />);
+  });
 }
-
-const forms = [
-  {
-    uuid: '4077b82a-6d5b-4fc6-abb9-8bc6846600f0',
-    name: 'BirthTest',
-    display: 'BirthTest',
-    encounterType: {
-      uuid: '0e8230ce-bd1d-43f5-a863-cf44344fa4b0',
-      name: 'Adult Visit',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '1.0.2',
-    published: false,
-    retired: false,
-    resources: [
-      {
-        uuid: 'd8c78580-538e-4c8c-8a7c-a31d07318602',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: '50d9f3ba-437c-444f-b675-fdda8052aefb',
-      },
-    ],
-  },
-  {
-    uuid: '9e1a0c68-ca19-3482-9ffb-0a6b4e591c2a',
-    name: 'Covid 19',
-    display: 'Covid 19',
-    encounterType: {
-      uuid: 'dd528487-82a5-4082-9c72-ed246bd49591',
-      name: 'Consultation',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '1',
-    published: false,
-    retired: false,
-    resources: [
-      {
-        uuid: '7d51a728-edbc-492b-aaf2-bea4d0d0e7db',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: 'a2374100-7398-4eb2-b39e-5cb3f50230a8',
-      },
-    ],
-  },
-  {
-    uuid: '336e13c6-0042-356e-9773-252382a3c7be',
-    name: 'Laboratory Tests',
-    display: 'Laboratory Tests',
-    encounterType: {
-      uuid: 'dd528487-82a5-4082-9c72-ed246bd49591',
-      name: 'Consultation',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '2',
-    published: true,
-    retired: false,
-    resources: [
-      {
-        uuid: '4bb4222b-4eb1-4e13-9730-dab367360ac1',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: 'e8525f5e-98e8-4599-8f53-8cd80679a225',
-      },
-    ],
-  },
-  {
-    uuid: '13c459d1-6768-4eaf-a865-4230987796ff',
-    name: 'PiusRocks',
-    display: 'PiusRocks',
-    encounterType: {
-      uuid: 'e22e39fd-7db2-45e7-80f1-60fa0d5a4378',
-      name: 'Admission',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '1.1.1',
-    published: false,
-    retired: false,
-    resources: [
-      {
-        uuid: '783a949a-706d-4c1a-94c5-c2ee204e1b0f',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: '77978fe1-8f41-4e3b-bec3-aa38b1ade6bc',
-      },
-    ],
-  },
-  {
-    uuid: '96637f12-3c04-311f-b477-3fa6a866e895',
-    name: 'Surgical Operation',
-    display: 'Surgical Operation',
-    encounterType: {
-      uuid: 'dd528487-82a5-4082-9c72-ed246bd49591',
-      name: 'Consultation',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '1',
-    published: true,
-    retired: false,
-    resources: [
-      {
-        uuid: '7b725cfd-4391-4e80-a9c3-aaee9ce7f5cd',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: '75b5fe60-b529-4d7b-9c40-cbc72fc3e05f',
-      },
-    ],
-  },
-  {
-    uuid: '2ddde996-b1c3-37f1-a53e-378dd1a4f6b5',
-    name: 'Test Form 1',
-    display: 'Test Form 1',
-    encounterType: {
-      uuid: 'dd528487-82a5-4082-9c72-ed246bd49591',
-      name: 'Consultation',
-      viewPrivilege: null,
-      editPrivilege: null,
-    },
-    version: '1',
-    published: true,
-    retired: false,
-    resources: [
-      {
-        uuid: '3214f6db-4da6-488a-b4eb-e6c9aabdbacb',
-        name: 'JSON schema',
-        dataType: 'AmpathJsonSchema',
-        valueReference: '56b08772-3145-428d-946d-4bb6455783ed',
-      },
-      {
-        uuid: '6381a458-9504-418a-b3fe-2e023e6d2550',
-        name: 'Test Form 1_translations_fr',
-        dataType: 'org.openmrs.customdatatype.datatype.LongFreeTextDatatype',
-        valueReference: '96c0472d-240f-4dc4-b63d-f5bfb90afa25',
-      },
-    ],
-  },
-];
