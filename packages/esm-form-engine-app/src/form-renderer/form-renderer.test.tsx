57345c9b4ba4a65ea13a2ec1f6e439e72148c2d8
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-08 13:09:29 +0300

    (feat) Add correct form-error UI as shown on the designs

---
+++ packages/esm-form-engine-app/src/form-renderer/form-renderer.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+import FormRenderer from './form-renderer.component';
+import useForm from '../hooks/useForm';
+import useSchema from '../hooks/useSchema';
+
+jest.mock('@openmrs/openmrs-form-engine-lib', () => ({
+  OHRIForm: jest
+    .fn()
+    .mockImplementation(() => React.createElement('div', { 'data-testid': 'openmrs form' }, 'FORM ENGINE LIB')),
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string, defaultValue: string) => defaultValue,
+  }),
+}));
+
+jest.mock('../hooks/useForm');
+jest.mock('../hooks/useSchema', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('FormRenderer', () => {
+  const defaultProps = {
+    formUuid: 'test-form-uuid',
+    patientUuid: 'test-patient-uuid',
+    closeWorkspace: jest.fn(),
+  };
+
+  beforeEach(() => {
+    (useForm as jest.Mock).mockReset();
+    (useSchema as jest.Mock).mockReset();
+  });
+
+  test('renders FormError component when there is an error', () => {
+    (useForm as jest.Mock).mockReturnValue({ form: null, isLoading: false, error: 'Error message' });
+    (useSchema as jest.Mock).mockReturnValue({ schema: null, isLoading: false, error: null });
+
+    render(<FormRenderer {...defaultProps} />);
+    expect(screen.getByText('There was an error with this form')).toBeInTheDocument();
+  });
+
+  test('renders InlineLoading component when loading', () => {
+    (useForm as jest.Mock).mockReturnValue({ form: null, isLoading: true, error: null });
+    (useSchema as jest.Mock).mockReturnValue({ schema: null, isLoading: true, error: null });
+
+    const { getByText } = render(<FormRenderer {...defaultProps} />);
+    expect(getByText('Loading ...')).toBeInTheDocument();
+  });
+
+  test('renders FORM Engine component when schema is available', async () => {
+    const mockForm = {
+      resources: [
+        {
+          name: 'JSON schema',
+          valueReference: 'test-schema-uuid',
+        },
+      ],
+    };
+
+    (useForm as jest.Mock).mockReturnValue({ form: mockForm, isLoading: false, error: null });
+    (useSchema as jest.Mock).mockReturnValue({ schema: { id: 'test-schema' }, isLoading: false, error: null });
+
+    render(<FormRenderer {...defaultProps} />);
+    await waitFor(() => expect(screen.getByText('FORM ENGINE LIB')).toBeInTheDocument());
+  });
+});
