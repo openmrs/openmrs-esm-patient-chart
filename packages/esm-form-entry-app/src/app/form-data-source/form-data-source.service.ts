openmrs/openmrs-esm-patient-chart

commit 9afb521d0368de1d9de722cc2f0af264e6feed20

(feat) Extend the patient object to support `other` and `unknown` genders (#1042)
* (fix) model object now handles for all genders

* Set `U` as the default

Co-authored-by: jnsereko <58003327+jnsereko@users.noreply.github.com>

---------

Co-authored-by: Vivek Alladi <valladi@icrc.org>
Co-authored-by: Dennis Kigen <kigen.work@gmail.com>
Co-authored-by: jnsereko <58003327+jnsereko@users.noreply.github.com>

packages/esm-form-entry-app/src/app/form-data-source/form-data-source.service.ts
@@ -303,7 +303,21 @@ export class FormDataSourceService {
 
   public getPatientObject(patient): object {
     const model: object = {};
-    model['sex'] = patient?.gender === 'male' ? 'M' : 'F';
+    const gender = patient?.gender;
+    model['sex'] = (gender) => {
+      switch (gender) {
+        case 'male':
+          return 'M';
+        case 'female':
+          return 'F';
+        case 'other':
+          return 'O';
+        case 'unknown':
+          return 'U';
+        default:
+          return 'U';
+      }
+    };
     model['birthdate'] = new Date(patient?.birthDate);
     model['age'] = this.calculateAge(patient?.birthDate);
 

commit 2b7009d94059c6dad4c75ba88af95a3c298230d4

(fix) KH-175 Fix regression Introduced by https://github.com/openmrs/openmrs-esm-patient-chart/pull/1042

packages/esm-form-entry-app/src/app/form-data-source/form-data-source.service.ts
@@ -18,7 +18,7 @@ export class FormDataSourceService {
     private locationResourceService: LocationResourceService,
     private conceptResourceService: ConceptResourceService,
     private localStorageService: LocalStorageService,
-  ) {}
+  ) { }
 
   public getDataSources(formSchema: FormSchema) {
     return {
@@ -189,38 +189,38 @@ export class FormDataSourceService {
     ).then((response: FetchResponse<{ entry: Array<FHIRResource> }>) =>
       response.ok
         ? // The output of this reduce() call is intended to be an object similar to an OpenMRS REST Encounter
-          // This allows it to be consumed by the historical data source
-          response.data.entry?.reduce<{ obs: Array<Partial<Observation>> }>(
-            (acc, entry) => {
-              if (entry.resource && entry.resource.resourceType === 'Observation' && entry.resource.code) {
-                const code = entry.resource.code.coding.find((c) => !Boolean(c.system))?.code;
-                let value: string | number | { uuid: string };
-                if (typeof entry.resource.valueString !== 'undefined' && entry.resource.valueString !== null) {
-                  value = entry.resource.valueString;
-                } else if (
-                  typeof entry.resource.valueQuantity?.value !== 'undefined' &&
-                  entry.resource.valueQuantity?.value !== null
-                ) {
-                  value = entry.resource.valueQuantity.value;
-                } else {
-                  const coding = entry.resource.valueCodeableConcept?.coding?.find((c) => !Boolean(c.system))?.code;
-                  if (typeof coding !== 'undefined' && coding !== null) {
-                    value = { uuid: coding };
-                  }
-                }
-
-                if (typeof code !== 'undefined' && code !== null && typeof value !== 'undefined') {
-                  acc.obs.push({
-                    concept: { uuid: code, display: null, conceptClass: null },
-                    value,
-                  });
+        // This allows it to be consumed by the historical data source
+        response.data.entry?.reduce<{ obs: Array<Partial<Observation>> }>(
+          (acc, entry) => {
+            if (entry.resource && entry.resource.resourceType === 'Observation' && entry.resource.code) {
+              const code = entry.resource.code.coding.find((c) => !Boolean(c.system))?.code;
+              let value: string | number | { uuid: string };
+              if (typeof entry.resource.valueString !== 'undefined' && entry.resource.valueString !== null) {
+                value = entry.resource.valueString;
+              } else if (
+                typeof entry.resource.valueQuantity?.value !== 'undefined' &&
+                entry.resource.valueQuantity?.value !== null
+              ) {
+                value = entry.resource.valueQuantity.value;
+              } else {
+                const coding = entry.resource.valueCodeableConcept?.coding?.find((c) => !Boolean(c.system))?.code;
+                if (typeof coding !== 'undefined' && coding !== null) {
+                  value = { uuid: coding };
                 }
               }
 
-              return acc;
-            },
-            { obs: [] },
-          )
+              if (typeof code !== 'undefined' && code !== null && typeof value !== 'undefined') {
+                acc.obs.push({
+                  concept: { uuid: code, display: null, conceptClass: null },
+                  value,
+                });
+              }
+            }
+
+            return acc;
+          },
+          { obs: [] },
+        )
         : { obs: [] },
     );
   }
@@ -317,24 +317,24 @@ export class FormDataSourceService {
     const sourcekey = 'cachedproviders';
     this.localStorageService.setObject(sourcekey, searchProviderResults);
   }
-
+  private getGender(gender): string {
+    switch (gender) {
+      case 'male':
+        return 'M';
+      case 'female':
+        return 'F';
+      case 'other':
+        return 'O';
+      case 'unknown':
+        return 'U';
+      default:
+        return 'U';
+    }
+  }
   public getPatientObject(patient): object {
     const model: object = {};
     const gender = patient?.gender;
-    model['sex'] = (gender) => {
-      switch (gender) {
-        case 'male':
-          return 'M';
-        case 'female':
-          return 'F';
-        case 'other':
-          return 'O';
-        case 'unknown':
-          return 'U';
-        default:
-          return 'U';
-      }
-    };
+    model['sex'] = this.getGender(gender);
     model['birthdate'] = new Date(patient?.birthDate);
     model['age'] = this.calculateAge(patient?.birthDate);
 