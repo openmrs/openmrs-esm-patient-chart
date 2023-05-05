f378562afba25ec2bd4767dfd26ba4732e10bf59
Jexsie <jessiessebuliba@gmail.com>
2023-05-05 20:13:05 +0300

    (fix) Remove useOmrsRestPatient hook (#1148)

--- packages/esm-patient-banner-app/src/banner-tags/deceased-patient-tag.component.tsx
+++ packages/esm-patient-banner-app/src/banner-tags/deceased-patient-tag.component.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Tag, DefinitionTooltip } from '@carbon/react';
 import { formatDatetime, parseDate } from '@openmrs/esm-framework';
-import { useOmrsRestPatient } from '../hooks/usePatientAttributes';
 import styles from './deceased-patient-tag.scss';
 
 interface DeceasedPatientBannerTagProps {
   patient: Pick<fhir.Patient, 'deceasedDateTime'>;
-  patientUuid: string;
 }
-const DeceasedPatientBannerTag: React.FC<DeceasedPatientBannerTagProps> = ({ patient, patientUuid }) => {
+const DeceasedPatientBannerTag: React.FC<DeceasedPatientBannerTagProps> = ({ patient }) => {
   const { t } = useTranslation();
-  const { person } = useOmrsRestPatient(patientUuid);
-
-  const isDeceased = person?.dead || patient.deceasedDateTime;
+  const isDeceased = Boolean(patient?.deceasedDateTime);
 
   return (
     isDeceased && (
