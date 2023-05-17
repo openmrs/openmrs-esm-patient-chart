55d635363cce8e3bd42830dbf49574a8942f6b74
Kalungi Deborah <kalungi2k6@gmail.com>
2023-05-17 14:22:13 +0300

    (fix) 03-1952: Do not show the Edit Encounter button for undeveloped forms (#1118)
    Remove delete and edit button if form uuid and display are null

--- packages/esm-patient-chart-app/src/visit/visits-widget/past-visits-components/visits-table/visits-table.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visits-widget/past-visits-components/visits-table/visits-table.component.tsx
@@ -298,30 +298,34 @@ const VisitTable: React.FC<VisitTableProps> = ({ showAllEncounters, visits, pati
                       <TableExpandedRow className={styles.expandedRow} colSpan={headers.length + 2}>
                         <>
                           <EncounterObservations observations={visits[index].obs} />
-                          <Button
-                            kind="ghost"
-                            onClick={() =>
-                              launchWorkspace(
-                                visits[index].form.uuid,
-                                visits[index].visitUuid,
-                                visits[index].id,
-                                visits[index].form.display,
-                                visits[index].visitTypeUuid,
-                              )
-                            }
-                            renderIcon={(props) => <Edit size={16} {...props} />}
-                            style={{ marginLeft: '-1rem', marginTop: '0.5rem' }}
-                          >
-                            {t('editEncounter', 'Edit encounter')}
-                          </Button>
-                          <Button
-                            kind="danger--ghost"
-                            onClick={() => handleDeleteEncounter(visits[index].id, visits[index].form.display)}
-                            renderIcon={(props) => <TrashCan size={16} {...props} />}
-                            style={{ marginLeft: '-1rem', marginTop: '0.5rem' }}
-                          >
-                            {t('deleteThisEncounter', 'Delete this encounter')}
-                          </Button>
+                          {visits[index]?.form?.uuid && (
+                            <Button
+                              kind="ghost"
+                              onClick={() =>
+                                launchWorkspace(
+                                  visits[index].form.uuid,
+                                  visits[index].visitUuid,
+                                  visits[index].id,
+                                  visits[index].form.display,
+                                  visits[index].visitTypeUuid,
+                                )
+                              }
+                              renderIcon={(props) => <Edit size={16} {...props} />}
+                              style={{ marginLeft: '-1rem', marginTop: '0.5rem' }}
+                            >
+                              {t('editEncounter', 'Edit encounter')}
+                            </Button>
+                          )}
+                          {visits[index]?.form?.display && (
+                            <Button
+                              kind="danger--ghost"
+                              onClick={() => handleDeleteEncounter(visits[index].id, visits[index].form.display)}
+                              renderIcon={(props) => <TrashCan size={16} {...props} />}
+                              style={{ marginLeft: '-1rem', marginTop: '0.5rem' }}
+                            >
+                              {t('deleteThisEncounter', 'Delete this encounter')}
+                            </Button>
+                          )}
                         </>
                       </TableExpandedRow>
                     ) : (
