e6023743c08996417216afd4b40ea32d9de755fb
jwnasambu <julietwamalwa@yahoo.com>
2023-05-22 13:24:22 +0300

    Revert "Fix defaultValue to the key"
    This reverts commit 46405224b052dc10c499c7e69810a4ef4e4cd100.

--- packages/esm-patient-chart-app/src/index.ts
+++ packages/esm-patient-chart-app/src/index.ts
@@ -42,7 +42,12 @@ function setupOpenMRS() {
     {
       path: `${spaBasePath}/:view`,
       title: ([_, key]) =>
-        Promise.resolve(window.i18next.t(`${decodeURIComponent(key)} Dashboard`, { ns: moduleName })),
+        Promise.resolve(
+          window.i18next.t(`${decodeURIComponent(key)} Dashboard`, {
+            ns: moduleName,
+            defaultValue: key,
+          }),
+        ),
       parent: spaBasePath,
     },
   ]);
