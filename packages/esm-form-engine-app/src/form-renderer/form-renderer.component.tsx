be81ed466f8c445f6738630d8d8b6097295e80d6
Dennis Kigen <kigen.work@gmail.com>
2023-05-28 22:26:24 +0300

    (fix) Fix error container height

--- packages/esm-form-engine-app/src/form-renderer/form-renderer.component.tsx
+++ packages/esm-form-engine-app/src/form-renderer/form-renderer.component.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { InlineLoading } from '@carbon/react';
 import { OHRIForm } from '@openmrs/openmrs-form-engine-lib';
-
+import { Visit } from '@openmrs/esm-framework';
 import useForm from '../hooks/useForm';
 import useSchema from '../hooks/useSchema';
 import FormError from './form-error.component';
-
 import styles from './form-renderer.scss';
-import { Visit } from '@openmrs/esm-framework';
 
 interface FormRendererProps {
   formUuid: string;
@@ -34,7 +32,11 @@ const FormRenderer: React.FC<FormRendererProps> = ({ formUuid, patientUuid, visi
   }
 
   if (formLoadError || schemaLoadError) {
-    return <FormError closeWorkspace={closeWorkspace} />;
+    return (
+      <div className={styles.errorContainer}>
+        <FormError closeWorkspace={closeWorkspace} />
+      </div>
+    );
   }
 
   return (
