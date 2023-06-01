a29e2c188e923201fc21e03978d9656a535b4fba
Andre Gomes <Andre.Gomes@emea.merkleinc.com>
2023-06-01 15:09:39 +0100

    Alter app.module providers so it incorporates the LOCATE_ID

--- packages/esm-form-entry-app/src/app/app.module.ts
+++ packages/esm-form-entry-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { FeWrapperComponent } from './fe-wrapper/fe-wrapper.component';
 import { FormEntryModule } from '@openmrs/ngx-formentry';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { OpenmrsApiModule } from './openmrs-api/openmrs-api.module';
 import { FormSchemaService } from './form-schema/form-schema.service';
@@ -18,6 +18,31 @@ import { LoaderComponent } from './loader/loader.component';
 import { SingleSpaPropsService } from './single-spa-props/single-spa-props.service';
 import { FormCreationService } from './form-creation/form-creation.service';
 import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { Encounter, FormSchema } from './types';
+import { Session } from '@openmrs/esm-framework';
+
+export interface CreateFormParams {
+  /**
+   * The form schema to be used.
+   */
+  formSchema: FormSchema;
+  /**
+   * The currently signed-in user.
+   */
+  session: Session;
+  /**
+   * An optional encounter.
+   * If provided, this encounter will be edited by the form.
+   * This can be an offline encounter form the sync queue which hasn't been synchronized yet.
+   */
+  encounter?: Encounter;
+  /**
+   * A previous encounter for displaying previously entered data.
+   */
+  previousEncounter?: Encounter;
+}
+
+let session: CreateFormParams = null;
 
 @NgModule({
   declarations: [AppComponent, EmptyRouteComponent, FeWrapperComponent, LoaderComponent],
@@ -42,6 +67,10 @@ import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translat
     SingleSpaPropsService,
     TranslateService,
     TranslateStore,
+    {
+      provide: LOCALE_ID,
+      useValue: (window as any).i18next.language.toLowerCase() ? (window as any).i18next.language.toLowerCase() : 'en',
+    },
   ],
   bootstrap: [AppComponent],
 })
