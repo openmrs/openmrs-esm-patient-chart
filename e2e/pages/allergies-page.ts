23ecdf6f378542bf43235c45911ab4c71bd44d4d
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-06-09 21:25:22 +0530

    Add patient allergies e2e test

--- e2e/pages/allergies-page.ts
+++ e2e/pages/allergies-page.ts
@@ -3,7 +3,20 @@ import { Page } from '@playwright/test';
 export class AllergiesPage {
   constructor(readonly page: Page) {}
 
+  readonly addAllergyButton = () => this.page.getByText('Record');
+  readonly drugAllergenOption = () => this.page.getByText('ACE inhibitors');
+  readonly reactionOption = () => this.page.getByText('Mental status change');
+  readonly severityOption = () => this.page.getByText('Severe');
+  readonly allergySummary = () => this.page.locator('td');
+  readonly submitButton = () => this.page.getByText('Save');
   async goto(uuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Allergies');
+  }
+  async addAllergy() {
+    await this.addAllergyButton().click();
+    await this.drugAllergenOption().click();
+    await this.reactionOption().click();
+    await this.severityOption().click();
+    await this.submitButton().click();
   }
 }
---
+++ e2e/specs/record-allergies.spec.ts
@@ -0,0 +1,33 @@
+import { test } from '../core';
+import { PatientAllergiesPage } from '../pages';
+import { expect } from '@playwright/test';
+import { generateRandomPatient, deletePatient, Patient } from '../commands';
+
+let patient: Patient;
+
+test.beforeEach(async ({ api }) => {
+  patient = await generateRandomPatient(api);
+});
+
+test('Add drug allergy to patient', async ({ page, api }) => {
+  const allergiesPage = new PatientAllergiesPage(page);
+
+  await test.step('When I visit the patient allergies page', async () => {
+    await allergiesPage.goto(patient.uuid);
+  });
+  await test.step('And I add a drug allergy', async () => {
+    await expect(allergiesPage.page).toHaveURL(
+      `${process.env.E2E_BASE_URL}/spa/patient/${patient.uuid}/chart/Allergies`,
+    );
+    await allergiesPage.addAllergy();
+  });
+  await test.step('Then I should see the allergy in the list', async () => {
+    await expect(allergiesPage.drugAllergenOption()).toBeVisible();
+    await expect(allergiesPage.reactionOption()).toBeVisible();
+    await expect(allergiesPage.page.getByText('HIGH')).toBeVisible();
+  });
+});
+
+test.afterEach(async ({ api }) => {
+  await deletePatient(api, patient.uuid);
+});
