3fb83ad4f64826cf34a5f7bc46ac5a2f897db429
Dennis Kigen <kigen.work@gmail.com>
2023-06-19 22:26:20 +0300

    Migrate esm-patient-forms-app

--- packages/esm-patient-forms-app/src/index.ts
+++ packages/esm-patient-forms-app/src/index.ts
@@ -12,23 +12,16 @@ import { dashboardMeta } from './dashboard.meta';
 import { setupDynamicFormDataHandler, setupPatientFormSync } from './offline';
 import OfflineToolsNavLink from './offline-forms/offline-tools-nav-link.component';
 
-declare var __VERSION__: string;
-// __VERSION__ is replaced by Webpack with the version from package.json
-const version = __VERSION__;
+const moduleName = '@openmrs/esm-patient-forms-app';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
-
-const backendDependencies = {
-  'webservices.rest': '^2.2.0',
+const options = {
+  featureName: 'patient-forms',
+  moduleName,
 };
 
-function setupOpenMRS() {
-  const moduleName = '@openmrs/esm-patient-forms-app';
-  const options = {
-    featureName: 'patient-forms',
-    moduleName,
-  };
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
+export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 
   registerBreadcrumbs([
@@ -48,89 +41,34 @@ function setupOpenMRS() {
     title: 'Clinical Form',
     load: getAsyncLifecycle(() => import('./forms/form-entry.component'), options),
   });
-
-  return {
-    extensions: [
-      {
-        name: 'patient-form-dashboard',
-        order: 0,
-        slot: dashboardMeta.slot,
-        load: getAsyncLifecycle(() => import('./forms/forms-detailed-overview.component'), options),
-        online: {
-          isOffline: false,
-        },
-        offline: {
-          isOffline: true,
-        },
-      },
-      {
-        name: 'forms-summary-dashboard',
-        slot: 'patient-chart-dashboard-slot',
-        order: 12,
-        // t('forms_link', 'Forms & Notes')
-        load: getSyncLifecycle(
-          createDashboardLink({
-            ...dashboardMeta,
-            title: () =>
-              Promise.resolve(
-                window.i18next?.t('forms_link', { defaultValue: 'Forms & Notes', ns: moduleName }) ?? 'Forms & Notes',
-              ),
-          }),
-          options,
-        ),
-        meta: dashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        name: 'offline-tools-dashboard-forms-card',
-        slot: 'offline-tools-dashboard-cards',
-        load: getAsyncLifecycle(() => import('./offline-forms/offline-forms-overview-card.component'), options),
-        online: true,
-        offline: true,
-        order: 1,
-      },
-      {
-        name: 'offline-tools-page-forms-link',
-        slot: 'offline-tools-page-slot',
-        load: getSyncLifecycle(() => OfflineToolsNavLink({ page: 'forms', title: 'Offline forms' }), options),
-        meta: {
-          name: 'forms',
-          slot: 'offline-tools-page-forms-slot',
-        },
-        online: true,
-        offline: true,
-      },
-      {
-        name: 'offline-tools-page-forms',
-        slot: 'offline-tools-page-forms-slot',
-        load: getAsyncLifecycle(() => import('./offline-forms/offline-forms.component'), options),
-        online: {
-          canMarkFormsAsOffline: true,
-        },
-        offline: {
-          canMarkFormsAsOffline: false,
-        },
-      },
-      {
-        name: 'clinical-form-action-menu',
-        slot: 'action-menu-chart-items-slot',
-        load: getAsyncLifecycle(() => import('./clinical-form-action-button.component'), options),
-        order: 2,
-      },
-      {
-        name: 'clinical-forms-workspace',
-        load: getAsyncLifecycle(() => import('./forms/forms-workspace.component'), options),
-        meta: {
-          title: {
-            key: 'clinicalForm',
-            default: 'Clinical form',
-          },
-          type: 'order',
-        },
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS, version };
+export const formsDetailedOverview = () =>
+  getAsyncLifecycle(() => import('./forms/forms-detailed-overview.component'), options);
+
+export const formsAndNotesDashboardLink = () =>
+  // t('forms_link', 'Forms & Notes')
+  getSyncLifecycle(
+    createDashboardLink({
+      ...dashboardMeta,
+      title: () =>
+        Promise.resolve(
+          window.i18next?.t('forms_link', { defaultValue: 'Forms & Notes', ns: moduleName }) ?? 'Forms & Notes',
+        ),
+    }),
+    options,
+  );
+
+export const offlineFormOverviewCard = () =>
+  getAsyncLifecycle(() => import('./offline-forms/offline-forms-overview-card.component'), options);
+
+export const offlineFormsNavLink = () =>
+  getSyncLifecycle(() => OfflineToolsNavLink({ page: 'forms', title: 'Offline forms' }), options);
+
+export const offlineForms = () => getAsyncLifecycle(() => import('./offline-forms/offline-forms.component'), options);
+
+export const clinicalFormActionMenu = () =>
+  getAsyncLifecycle(() => import('./clinical-form-action-button.component'), options);
+
+export const clinicalFormsWorkspace = () =>
+  getAsyncLifecycle(() => import('./forms/forms-workspace.component'), options);
