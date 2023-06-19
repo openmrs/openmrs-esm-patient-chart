1a589d0150563b6a32aeba5f4432ab8a16417946
Dennis Kigen <kigen.work@gmail.com>
2023-06-19 21:55:10 +0300

    Migrate esm-patient-biometrics-app

--- packages/esm-patient-biometrics-app/src/index.ts
+++ packages/esm-patient-biometrics-app/src/index.ts
@@ -5,22 +5,20 @@ import {
   getSyncLifecycle,
   messageOmrsServiceWorker,
 } from '@openmrs/esm-framework';
-import { createDashboardLink, getPatientSummaryOrder } from '@openmrs/esm-patient-common-lib';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { configSchema } from './config-schema';
 import { dashboardMeta } from './dashboard.meta';
 
-declare var __VERSION__: string;
-// __VERSION__ is replaced by Webpack with the version from package.json
-const version = __VERSION__;
+const moduleName = '@openmrs/esm-patient-biometrics-app';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
-
-const backendDependencies = {
-  'webservices.rest': '^2.2.0',
-  fhir2: '^1.2.0',
+const options = {
+  featureName: 'patient-biometrics',
+  moduleName,
 };
 
-function setupOpenMRS() {
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+
+export function startupApp() {
   messageOmrsServiceWorker({
     type: 'registerDynamicRoute',
     pattern: `.+${fhirBaseUrl}/Observation.+`,
@@ -31,67 +29,27 @@ function setupOpenMRS() {
     pattern: `.+/ws/rest/v1/concept.+`,
   });
 
-  const moduleName = '@openmrs/esm-patient-biometrics-app';
-
-  const options = {
-    featureName: 'patient-biometrics',
-    moduleName,
-  };
-
   defineConfigSchema(moduleName, configSchema);
-
-  return {
-    extensions: [
-      {
-        name: 'biometrics-overview-widget',
-        slot: 'patient-chart-summary-dashboard-slot',
-        order: getPatientSummaryOrder('Biometrics'),
-        load: getAsyncLifecycle(() => import('./biometrics/biometrics-overview.component'), options),
-        meta: {
-          columnSpan: 4,
-        },
-        online: { showAddBiometrics: true },
-        offline: { showAddBiometrics: false },
-      },
-      {
-        name: 'results-summary-dashboard',
-        slot: 'patient-chart-dashboard-slot',
-        order: 2,
-        // t('biometrics_link', 'Vitals & Biometrics')
-        load: getSyncLifecycle(
-          createDashboardLink({
-            ...dashboardMeta,
-            title: () =>
-              Promise.resolve(
-                window.i18next?.t('biometrics_link', { defaultValue: 'Vitals & Biometrics', ns: moduleName }) ??
-                  'Vitals & Biometrics',
-              ),
-          }),
-          options,
-        ),
-        meta: dashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        name: 'biometrics-details-widget',
-        slot: dashboardMeta.slot,
-        load: getAsyncLifecycle(() => import('./biometrics/biometrics-main.component'), options),
-        meta: {
-          view: 'biometrics',
-          title: 'Biometrics',
-        },
-        online: { showAddBiometrics: true },
-        offline: { showAddBiometrics: false },
-      },
-      {
-        name: 'weight-tile',
-        order: 2,
-        slot: 'visit-form-header-slot',
-        load: getAsyncLifecycle(() => import('./biometrics/weight-tile.component'), options),
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS, version };
+export const biometricsOverview = () =>
+  getAsyncLifecycle(() => import('./biometrics/biometrics-overview.component'), options);
+
+export const biometricsDetailedSummary = () =>
+  getAsyncLifecycle(() => import('./biometrics/biometrics-main.component'), options);
+
+export const vitalsAndBiometricsDashboardLink = () =>
+  // t('biometrics_link', 'Vitals & Biometrics')
+  getSyncLifecycle(
+    createDashboardLink({
+      ...dashboardMeta,
+      title: () =>
+        Promise.resolve(
+          window.i18next?.t('biometrics_link', { defaultValue: 'Vitals & Biometrics', ns: moduleName }) ??
+            'Vitals & Biometrics',
+        ),
+    }),
+    options,
+  );
+
+export const weightTile = () => getAsyncLifecycle(() => import('./biometrics/weight-tile.component'), options);
