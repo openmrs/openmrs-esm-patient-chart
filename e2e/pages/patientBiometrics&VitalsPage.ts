e05055a8d525e171377528eecd342534ecafc683
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-06-23 13:09:05 +0530

    Add a vitals e2e test

--- e2e/pages/patientBiometrics&VitalsPage.ts
+++ e2e/pages/patientBiometrics&VitalsPage.ts
@@ -2,8 +2,17 @@ import { Page } from '@playwright/test';
 
 export class BiometricsAndVitalsPage {
   constructor(readonly page: Page) {}
-
+  readonly tableRow = () => this.page.locator('tr');
+  readonly startVisitButton = () => this.page.getByRole('button', { name: 'Start a visit' });
+  readonly visitType = () => this.page.getByText('Facility Visit');
+  readonly submitButton = () => this.page.locator('form').getByRole('button', { name: 'Start a visit' });
   async goto(uuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Vitals%20%26%20Biometrics');
+  }
+
+  async startVisit() {
+    await this.startVisitButton().click();
+    await this.visitType().click();
+    await this.submitButton().click();
   }
 }
--- e2e/specs/addVitals.spec.ts
+++ e2e/specs/addVitals.spec.ts
@@ -16,10 +16,51 @@ test('Record Vitals of the patient', async ({ page, api }) => {
     await vitalsPage.goto(patient.uuid);
   });
 
+  await test.step('And I start a visit', async () => {
+    await vitalsPage.startVisit();
+    await expect(vitalsPage.page.getByText('Active Visit')).toBeVisible();
+  });
+
   await test.step('And I click record vital signs button', async () => {
     await vitalsPage.page.getByText('Record vital signs').click();
   });
 
+  await test.step('And I enter the temperature value', async () => {
+    await vitalsPage.page.locator('#Temperature').fill('37');
+  });
+
+  await test.step('Then I enter the blood pressure value', async () => {
+    await vitalsPage.page.locator('#systolic').fill('120');
+    await vitalsPage.page.locator('#diastolic').fill('80');
+  });
+
+  await test.step('And I enter the pulse value', async () => {
+    await vitalsPage.page.locator('#Pulse').fill('80');
+  });
+
+  await test.step('And I enter the oxygen saturation value', async () => {
+    await vitalsPage.page.getByTitle('Oxygen Saturation').fill('98');
+  });
+
+  await test.step('And I enter respiratory rate value', async () => {
+    await vitalsPage.page.getByTitle('Respiration Rate').fill('20');
+  });
+
+  await test.step('Then I fill the notes field', async () => {
+    await vitalsPage.page.locator('#Notes').fill('Test notes');
+  });
+
+  await test.step('And I click the save button', async () => {
+    await vitalsPage.page.getByText('Save').click();
+  });
+
+  await test.step('Then I should see the vitals record', async () => {
+    await expect(vitalsPage.tableRow().getByText('37')).toBeVisible();
+    await expect(vitalsPage.tableRow().getByRole('cell', { name: '120 / 80' })).toBeVisible();
+    await expect(vitalsPage.tableRow().getByRole('cell', { name: '80', exact: true })).toBeVisible();
+    await expect(vitalsPage.tableRow().getByText('98')).toBeVisible();
+    await expect(vitalsPage.tableRow().getByRole('cell', { name: '20 ↑' })).toBeVisible();
+  });
 });
 test.afterEach(async ({ api }) => {
   await deletePatient(api, patient.uuid);
