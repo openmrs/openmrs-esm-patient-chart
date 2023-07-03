f7d40805a790e65456456a1f6fa1bab0857828f2
hadijahkyampeire <hadijah315@gmail.com>
2023-07-03 22:35:57 +0300

    update flags mocks

--- __mocks__/patient-flags.mock.ts
+++ __mocks__/patient-flags.mock.ts
@@ -1,17 +1,17 @@
 export const mockPatientFlags = [
   {
     uuid: '8ca6c08f-66d9-4a18-a233-4f658b1755bf',
-    name: 'Needs Follow Up',
+    flag: { display: 'Needs Follow Up' },
     message: 'Patient needs to be followed up',
   },
   {
     uuid: '5fs6c08f-66d9-4a18-a233-5f658b1755bf',
-    name: 'Unknown Diagnosis',
+    flag: { display: 'Unknown Diagnosis' },
     message: 'Diagnosis for the patient is unknown',
   },
   {
     uuid: '4da4c08f-66d9-4a18-a233-5f658b1755bf',
-    name: 'Future Appointment',
+    flag: { display: 'Future Appointment' },
     message: 'Patient has a future appointment scheduled',
   },
 ];
