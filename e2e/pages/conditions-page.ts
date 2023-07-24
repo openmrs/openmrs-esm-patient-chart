34c14460ab3046da54a0c37b856fea6eba6656e2
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-07-24 20:28:03 +0530

    Adjust required changes

--- e2e/pages/conditions-page.ts
+++ e2e/pages/conditions-page.ts
@@ -5,7 +5,7 @@ export class ConditionsPage {
     constructor (readonly page: Page) {}
 
     readonly conditionsTable = () => this.page.locator('tbody');
-    
+
     async goto(uuid: string) {
         await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Conditions`);
     }
