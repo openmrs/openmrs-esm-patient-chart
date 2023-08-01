3ab2610d0742cbd890356fcee6ac2c4c203e76d5
Ian <52504170+ibacher@users.noreply.github.com>
2023-08-01 02:51:23 -0400

    (chore) Remove forms and notes (#1294)

--- packages/esm-patient-notes-app/src/index.ts
+++ packages/esm-patient-notes-app/src/index.ts
@@ -21,11 +21,6 @@ export function startupApp() {
 
 export const notesOverview = getAsyncLifecycle(() => import('./notes/notes-overview.component'), options);
 
-export const notesDetailedSummary = getAsyncLifecycle(
-  () => import('./notes/notes-detailed-summary.component'),
-  options,
-);
-
 export const visitNotesActionButton = getAsyncLifecycle(() => import('./visit-note-action-button.component'), options);
 
 export const visitNotesForm = getAsyncLifecycle(() => import('./notes/visit-notes-form.component'), options);
