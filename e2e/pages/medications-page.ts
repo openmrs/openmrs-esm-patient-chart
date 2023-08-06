1e71b4632b7fcce005d83e13b24b369dc51c9b14
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-08-06 23:56:12 +0530

    Fix lint error

--- e2e/pages/medications-page.ts
+++ e2e/pages/medications-page.ts
@@ -1,13 +1,12 @@
-import {Page} from '@playwright/test';
+import { Page } from '@playwright/test';
 
 export class MedicationsPage {
+  constructor(readonly page: Page) {}
 
-    constructor(readonly page: Page) {};
+  readonly activeMedicationTable = () => this.page.getByTestId('active-medication-table');
+  readonly pastMedicationTable = () => this.page.getByTestId('past-medication-table');
 
-    readonly activeMedicationTable = () => this.page.getByTestId('active-medication-table');
-    readonly pastMedicationTable = () => this.page.getByTestId('past-medication-table');
-
-    async goto(uuid: string) {
-        await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Medications`);
-    }
+  async goto(uuid: string) {
+    await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Medications`);
+  }
 }
