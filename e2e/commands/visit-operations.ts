df1cdd5e38d7d87bd5c4a299bbce78d5f7ae08d4
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-08-07 00:36:00 +0530

    Adjust some changes

--- e2e/commands/visit-operations.ts
+++ e2e/commands/visit-operations.ts
@@ -17,15 +17,13 @@ export const startVisit = async (api: APIRequestContext, patientId: string): Pro
   return await visitRes.json();
 };
 
-export const endVisit = async (api: APIRequestContext, uuid: string, patientId: string): Promise<Visit> => {
+export const endVisit = async (api: APIRequestContext, uuid: string) => {
   const visitRes = await api.post(`visit/${uuid}`, {
     data: {
       location: process.env.E2E_LOGIN_DEFAULT_LOCATION_UUID,
       startDatetime: dayjs().subtract(1, 'D').format('YYYY-MM-DDTHH:mm:ss.SSSZZ'),
       visitType: '7b0f5697-27e3-40c4-8bae-f4049abfb4ed',
       stopDatetime: dayjs().format('YYYY-MM-DDTHH:mm:ss.SSSZZ'),
-      attributes: [],
-      patient: patientId,
     },
   });
 
--- e2e/pages/vitals-and-biometrics-page.ts
+++ e2e/pages/vitals-and-biometrics-page.ts
@@ -3,10 +3,8 @@ import { Page } from '@playwright/test';
 export class BiometricsAndVitalsPage {
   constructor(readonly page: Page) {}
 
-  // readonly vitalsTable = () => this.page.getByTestId('vitals-table');
-  // readonly biometricsTable = () => this.page.getByTestId('biometrics-table');
   readonly vitalsTable = () => this.page.getByTestId('vitals-table');
-  readonly biometricsTable = () => this.page.locator('tbody');
+  readonly biometricsTable = () => this.page.getByTestId('biometrics-table');
 
   async goto(uuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Vitals%20%26%20Biometrics');
