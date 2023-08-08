1f16457bc6de10db745765dd0364b0066b56a718
nanfuka <kalungi2k6@gmail.com>
2023-08-08 09:45:51 +0300

    Fix spelling

--- packages/esm-patient-test-results-app/src/config-schema.ts
+++ packages/esm-patient-test-results-app/src/config-schema.ts
@@ -33,7 +33,7 @@ export const configSchema = {
     _type: Type.Boolean,
     _default: true,
     _description:
-      'Determines whether or not to display the Print button in the testResults. If set to true, a Print button gets shown next to the tree panel button. When clicked, a datatable wrapped in a Modal pops up with a print button whilch when clicked prints out the contents of the table',
+      'Determines whether or not to display the Print button in the testResults. If set to true, a Print button gets shown next to the tree panel button. When clicked, a datatable wrapped in a Modal pops up with a print button which when clicked prints out the contents of the table',
   },
 };
 
