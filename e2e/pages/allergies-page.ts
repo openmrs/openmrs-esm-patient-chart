828fca5bc03d4ea233498eeda2b3e76b1ed7ea9d
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-08-13 23:52:11 +0530

    Adjust required changes

--- e2e/pages/allergies-page.ts
+++ e2e/pages/allergies-page.ts
@@ -4,7 +4,7 @@ export class PatientAllergiesPage {
   constructor(readonly page: Page) {}
 
   readonly allergyTable = () => this.page.getByTestId('allergy-table');
-  readonly tableRow = () => this.page.locator('tr');
+  
   async goto(uuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Allergies');
   }
