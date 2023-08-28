4a6c7d3ae94e7cde46886684a6e6985ca9801a1a
Vineet Sharma <sharmava05@gmail.com>
2023-08-28 23:16:00 +0530

    Created a common component for siderail nav buttons

--- packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
+++ packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
@@ -1,54 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Tag } from '@carbon/react';
+import { Tag } from '@carbon/react';
 import { ShoppingCart } from '@carbon/react/icons';
-import { useLayoutType } from '@openmrs/esm-framework';
-import { useLaunchWorkspaceRequiringVisit, useOrderBasket, useWorkspaces } from '@openmrs/esm-patient-common-lib';
+import { useLaunchWorkspaceRequiringVisit, useOrderBasket, SiderailNavButton } from '@openmrs/esm-patient-common-lib';
 import styles from './order-basket-action-button.scss';
 
 const OrderBasketActionButton: React.FC = () => {
-  const layout = useLayoutType();
   const { t } = useTranslation();
-  const { workspaces } = useWorkspaces();
   const { orders } = useOrderBasket();
-  const isActiveWorkspace = workspaces?.[0]?.name?.match(/order-basket/i);
   const launchOrderBasket = useLaunchWorkspaceRequiringVisit('order-basket');
 
-  if (layout === 'tablet') {
-    return (
-      <Button
-        kind="ghost"
-        className={`${styles.container} ${isActiveWorkspace ? styles.active : ''}`}
-        role="button"
-        tabIndex={0}
-        onClick={() => launchOrderBasket()}
-      >
-        <div className={styles.elementContainer}>
-          <ShoppingCart size={16} />
-          {orders?.length > 0 && <Tag className={styles.countTag}>{orders?.length}</Tag>}
-        </div>
-        <span>{t('orderBasket', 'Order basket')}</span>
-      </Button>
-    );
-  }
-
   return (
-    <Button
-      className={isActiveWorkspace && styles.active}
-      kind="ghost"
-      size="sm"
-      renderIcon={(props) => (
-        <div className={styles.elementContainer}>
-          <ShoppingCart size={20} {...props} />{' '}
-          {orders?.length > 0 && <Tag className={styles.countTag}>{orders?.length}</Tag>}
-        </div>
-      )}
-      hasIconOnly
+    <SiderailNavButton
+      name={'order-basket-action-menu'}
+      getIcon={(props) => <ShoppingCart {...props} />}
+      label={t('orderBasket', 'Order basket')}
       iconDescription={t('medications', 'Medications')}
-      enterDelayMs={1000}
-      tooltipAlignment="center"
-      tooltipPosition="left"
-      onClick={() => launchOrderBasket()}
+      handler={launchOrderBasket}
+      type={'order'}
+      tagContent={orders?.length > 0 ? orders?.length : null}
     />
   );
 };
