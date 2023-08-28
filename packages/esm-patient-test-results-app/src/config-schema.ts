c2ce7f6d2cb97c4b01aa02a6f297a6e030e8ff5a
Kalungi Deborah <kalungi2k6@gmail.com>
2023-08-28 17:02:53 +0300

    (feat) O3-2196: Implement the print functionality for Test results (#1306)
    Co-authored-by: Brandon Istenes <bistenes@gmail.com>

--- packages/esm-patient-test-results-app/src/config-schema.ts
+++ packages/esm-patient-test-results-app/src/config-schema.ts
@@ -28,6 +28,13 @@ export const configSchema = {
       },
     ],
   },
+
+  showPrintButton: {
+    _type: Type.Boolean,
+    _default: true,
+    _description:
+      'Whether or not to display the print button in the Test Results dashboard. When set to `true`, a print button is shown alongside the panel and tree view content switcher. When clicked, a modal pops up showing a datatable with the available test results. Once the user selects an appropriate date range, they can click on the print button in the modal to print the data',
+  },
 };
 
 export interface ObsTreeEntry {
@@ -36,4 +43,5 @@ export interface ObsTreeEntry {
 }
 export interface ConfigObject {
   concepts: Array<ObsTreeEntry>;
+  showPrintButton: boolean;
 }
