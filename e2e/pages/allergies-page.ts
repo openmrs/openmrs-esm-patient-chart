a6d60b5ce73e9d6570518432110fec6b178fefb3
Brandon Istenes <bistenes@gmail.com>
2023-09-25 01:08:13 -0400

    (fix) Run prettier (#1369)
    Run prettier

--- e2e/pages/allergies-page.ts
+++ e2e/pages/allergies-page.ts
@@ -4,7 +4,7 @@ export class PatientAllergiesPage {
   constructor(readonly page: Page) {}
 
   readonly allergyTable = () => this.page.getByTestId('allergy-table');
-  
+
   async goto(uuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + uuid + '/chart/Allergies');
   }
--- e2e/pages/conditions-page.ts
+++ e2e/pages/conditions-page.ts
@@ -1,12 +1,11 @@
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
 export class ConditionsPage {
+  constructor(readonly page: Page) {}
 
-    constructor (readonly page: Page) {}
+  readonly conditionsTable = () => this.page.locator('tbody');
 
-    readonly conditionsTable = () => this.page.locator('tbody');
-
-    async goto(uuid: string) {
-        await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Conditions`);
-    }
+  async goto(uuid: string) {
+    await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Conditions`);
+  }
 }
