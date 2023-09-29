ca9088a30878352ef9ef4b5b248221b8dde4858c
Donald Kibet <chelashawdonald@yahoo.com>
2023-09-30 01:20:40 +0300

    (enhc) update `react-form-engine` to use `o3` forms endpoint

---
+++ packages/esm-form-engine-app/src/hooks/useFormSchema.tsx
@@ -0,0 +1,20 @@
+import useSWR from 'swr';
+
+import { openmrsFetch } from '@openmrs/esm-framework';
+import { OHRIFormSchema } from '@openmrs/openmrs-form-engine-lib';
+
+/**
+ * Custom hook to fetch form schema based on its form UUID.
+ *
+ * @param formUuid - The UUID of the form to retrieve the schema for
+ * @returns An object containing the form schema, error, and loading state
+ */
+const useFormSchema = (formUuid: string) => {
+  const url = formUuid ? `/ws/rest/v1/o3/forms/${formUuid}` : null;
+
+  const { data, error, isLoading } = useSWR<{ data: OHRIFormSchema }>(formUuid ? url : null, openmrsFetch);
+
+  return { schema: data?.data, error, isLoading };
+};
+
+export default useFormSchema;
