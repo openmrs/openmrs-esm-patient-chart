d8543375f58a3b3ca2fadff3a24c88734c6d76f7
Jayasanka <jayasanka.sack@gmail.com>
2023-10-12 12:27:13 -0700

    Add docker configuration to run E2E on Bamboo

---
+++ e2e/support/bamboo/e2e-test-runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export E2E_BASE_URL=http://gateway/openmrs
+export CI=true
+
+while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://gateway/openmrs/login.htm)" != "200" ]; do
+  echo "Waiting for the backend to be up..."
+  sleep 10
+done
+
+yarn test-e2e
