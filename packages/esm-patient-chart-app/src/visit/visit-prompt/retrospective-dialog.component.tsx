8e534d8caa20650b8a4b284d605b1649d37f8910
Donald Kibet <chelashawdonald@yahoo.com>
2023-10-13 11:37:48 +0300

    (feat) Add ability to pick time

--- packages/esm-patient-chart-app/src/visit/visit-prompt/retrospective-dialog.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visit-prompt/retrospective-dialog.component.tsx
@@ -6,18 +6,30 @@ import {
   ModalFooter,
   DatePicker,
   DatePickerInput,
-  FlexGrid,
+  Stack,
   Row,
   Column,
   Form,
+  TimePicker,
+  TimePickerSelect,
+  SelectItem,
 } from '@carbon/react';
 import styles from './retrospective-dialog.scss';
 import { useTranslation } from 'react-i18next';
-import { NewVisitPayload, formatDate, saveVisit, setCurrentVisit, showNotification } from '@openmrs/esm-framework';
+import {
+  NewVisitPayload,
+  formatDate,
+  saveVisit,
+  setCurrentVisit,
+  showNotification,
+  toDateObjectStrict,
+  toOmrsIsoString,
+} from '@openmrs/esm-framework';
 import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import dayjs from 'dayjs';
+import { amPm, convertTime12to24 } from '@openmrs/esm-patient-common-lib';
 
 interface RetrospectiveDialogProp {
   patientUuid: string;
@@ -46,6 +58,8 @@ const RetrospectiveDialog: React.FC<RetrospectiveDialogProp> = ({
         .min(new Date(visitDate), {
           message: t('visitStopDateAfterVisitDate', 'Visit stop date should be after visit start date'),
         }),
+      visitStopTime: z.string({ required_error: t('requiredVisitStopTime', 'Visit stop time is required') }),
+      timeFormat: z.enum(['PM', 'AM']),
     });
   }, []);
 
@@ -53,6 +67,10 @@ const RetrospectiveDialog: React.FC<RetrospectiveDialogProp> = ({
   const methods = useForm<RetrospectiveDialogForm>({
     mode: 'all',
     resolver: zodResolver(retrospectiveFormSchema),
+    defaultValues: {
+      timeFormat: new Date().getHours() >= 12 ? 'PM' : 'AM',
+      visitStopTime: dayjs().format('hh:mm'),
+    },
   });
 
   const {
@@ -63,7 +81,20 @@ const RetrospectiveDialog: React.FC<RetrospectiveDialogProp> = ({
 
   const onSubmit = useCallback((formData: RetrospectiveDialogForm) => {
     {
-      payload.stopDatetime = new Date(formData.visitStopDate);
+      const { visitStopTime, timeFormat, visitStopDate } = formData;
+      const [hours, minutes] = convertTime12to24(visitStopTime, timeFormat);
+      const stopDatetime = toDateObjectStrict(
+        toOmrsIsoString(
+          new Date(
+            dayjs(visitStopDate).year(),
+            dayjs(visitStopDate).month(),
+            dayjs(visitStopDate).date(),
+            hours,
+            minutes,
+          ),
+        ),
+      );
+      payload.stopDatetime = stopDatetime;
       const ac = new AbortController(); // TODO: fix abort controller
       saveVisit(payload, ac).subscribe(
         ({ data }) => {
@@ -84,7 +115,7 @@ const RetrospectiveDialog: React.FC<RetrospectiveDialogProp> = ({
           <span className={styles.header}>{t('retrospectiveVisit', 'Retrospective visit')}</span>
         </ModalHeader>
         <ModalBody>
-          <FlexGrid>
+          <Stack gap={1} className={styles.container}>
             <Row>
               <Column>
                 <span>
@@ -96,36 +127,68 @@ const RetrospectiveDialog: React.FC<RetrospectiveDialogProp> = ({
                 </span>
               </Column>
             </Row>
-            <Row>
-              <Column lg={8} md={8} sm={4}>
-                <Controller
-                  name="visitStopDate"
-                  control={control}
-                  render={({ field: { onChange, value } }) => (
-                    <DatePicker
-                      light
-                      dateFormat="d/m/Y"
-                      datePickerType="single"
-                      id="visitDate"
-                      className={styles.datePicker}
-                      maxDate={new Date().toISOString()}
-                      minDate={new Date(visitDate).toISOString()}
-                      onChange={([date]) => onChange(date)}
-                      value={value}
-                    >
-                      <DatePickerInput
-                        id="visitStartDateInput"
-                        labelText={t('visitStopDate', 'Visit stop date')}
-                        placeholder="dd/mm/yyyy"
-                        invalid={!!errors.visitStopDate}
-                        invalidText={errors.visitStopDate?.message}
-                      />
-                    </DatePicker>
-                  )}
-                />
-              </Column>
-            </Row>
-          </FlexGrid>
+            <section style={{ display: 'flex', alignItems: 'center' }}>
+              <Controller
+                name="visitStopDate"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <DatePicker
+                    light
+                    dateFormat="d/m/Y"
+                    datePickerType="single"
+                    id="visitDate"
+                    className={styles.datePicker}
+                    maxDate={new Date().toISOString()}
+                    minDate={new Date(visitDate).toISOString()}
+                    onChange={([date]) => onChange(date)}
+                    value={value}
+                  >
+                    <DatePickerInput
+                      id="visitStartDateInput"
+                      labelText={t('visitStopDate', 'Visit stop date')}
+                      placeholder="dd/mm/yyyy"
+                      invalid={!!errors.visitStopDate}
+                      invalidText={errors.visitStopDate?.message}
+                    />
+                  </DatePicker>
+                )}
+              />
+
+              <Controller
+                name="visitStopTime"
+                control={control}
+                render={({ field: { onBlur, onChange, value } }) => (
+                  <TimePicker
+                    id="visitStartTime"
+                    labelText={t('time', 'Time')}
+                    onChange={(event) => onChange(event.target.value as amPm)}
+                    pattern="^(1[0-2]|0?[1-9]):([0-5]?[0-9])$"
+                    style={{ marginLeft: '0.125rem', flex: 'none' }}
+                    value={value}
+                    onBlur={onBlur}
+                    invalid={!!errors.visitStopTime}
+                    invalidText={errors.visitStopDate?.message}
+                  >
+                    <Controller
+                      name="timeFormat"
+                      control={control}
+                      render={({ field: { onChange, value } }) => (
+                        <TimePickerSelect
+                          id="visitStartTimeSelect"
+                          onChange={(event) => onChange(event.target.value as amPm)}
+                          value={value}
+                          aria-label={t('timeFormat ', 'Time Format')}
+                        >
+                          <SelectItem value="AM" text="AM" />
+                          <SelectItem value="PM" text="PM" />
+                        </TimePickerSelect>
+                      )}
+                    />
+                  </TimePicker>
+                )}
+              />
+            </section>
+          </Stack>
         </ModalBody>
         <ModalFooter>
           <Button kind="secondary" onClick={onCancel}>
