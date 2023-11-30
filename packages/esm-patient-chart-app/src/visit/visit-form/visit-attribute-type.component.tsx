3d659432ada2e56b0f11470cd7b52db5d14756fd
Dennis Kigen <kigen.work@gmail.com>
2023-11-30 11:55:10 +0300

    Alternative implementation that doesn't use eval or new Function

--- packages/esm-patient-chart-app/src/visit/visit-form/visit-attribute-type.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visit-form/visit-attribute-type.component.tsx
@@ -1,7 +1,4 @@
-import { useConfig } from '@openmrs/esm-framework';
 import React, { useEffect, useMemo } from 'react';
-import { type ChartConfig } from '../../config-schema';
-import { useConceptAnswersForVisitAttributeType, useVisitAttributeType } from '../hooks/useVisitAttributeType';
 import {
   TextInput,
   TextInputSkeleton,
@@ -17,6 +14,9 @@ import {
 import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { Controller, type ControllerRenderProps, useFormContext } from 'react-hook-form';
+import { useConfig } from '@openmrs/esm-framework';
+import { type ChartConfig } from '../../config-schema';
+import { useConceptAnswersForVisitAttributeType, useVisitAttributeType } from '../hooks/useVisitAttributeType';
 import { type VisitFormData } from './visit-form.resource';
 import styles from './visit-attribute-type.scss';
 
@@ -32,23 +32,45 @@ interface VisitAttributeTypeFieldsProps {
   >;
 }
 
+/**
+ * The `evaluateExpression` function is used to evaluate the optional showWhenExpression for an attribute type. If a showWhenExpression is provided evaluates to true, the attribute type field is rendered. Otherwise, if no showWhenExpression is provided, the attribute type field gets rendered by default.
+ *
+ * @param {string} expression - The showWhenExpression for the attribute type field.
+ * @param {VisitAttributes} visitAttributes - The visit attributes object
+ * @returns {boolean} - The result of the expression evaluation
+ */
+
+function evaluateExpression(expression: string, visitAttributes: VisitAttributes) {
+  const [left, operator, right] = expression.split(' ');
+  const attributeUuid = left.match(/\['(.*?)'\]/)[1];
+  const attributeValue = right.replace(/'/g, '');
+
+  switch (operator) {
+    case '===':
+      return visitAttributes[attributeUuid] === attributeValue;
+    case '!==':
+      return visitAttributes[attributeUuid] !== attributeValue;
+    default:
+      return false;
+  }
+}
+
 const VisitAttributeTypeFields: React.FC<VisitAttributeTypeFieldsProps> = ({ setErrorFetchingResources }) => {
-  const { visitAttributeTypes } = useConfig() as ChartConfig;
+  const { visitAttributeTypes } = useConfig<ChartConfig>();
   const { control, getValues } = useFormContext<VisitFormData>();
 
   if (visitAttributeTypes?.length) {
     return (
       <>
-        {visitAttributeTypes.map((attributeType) => {
-          const shouldShowAttribute = () => {
-            if (!attributeType?.showWhenExpression) return true;
-            const { visitAttributes } = getValues();
-            const attributeEval = new Function('visitAttributes', `return ${attributeType.showWhenExpression}`);
-            return attributeEval(visitAttributes);
-          };
+        {visitAttributeTypes.map((attributeType, indx) => {
+          const { visitAttributes } = getValues();
+
+          const showAttributeType = attributeType?.showWhenExpression
+            ? evaluateExpression(attributeType?.showWhenExpression, visitAttributes)
+            : true;
 
           return (
-            shouldShowAttribute() && (
+            showAttributeType && (
               <Controller
                 key={attributeType.uuid}
                 name={`visitAttributes.${attributeType.uuid}`}
