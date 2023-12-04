e74ff55300e73b7d0abd79208ba13aeba478d12c
gitcliff <gitacliff48@gmail.com>
2023-12-04 12:01:21 +0300

    tree:Changing the tree view to not expand the first row of the results tree

--- packages/esm-patient-labs-app/src/config-schema.ts
+++ packages/esm-patient-labs-app/src/config-schema.ts
@@ -16,7 +16,7 @@ export const configSchema = {
     _default: [
       {
         conceptUuid: 'ae485e65-2e3f-4297-b35e-c818bbefe894',
-        defaultOpen: true,
+        defaultOpen: false,
       },
       {
         conceptUuid: '8904fa2b-6a8f-437d-89ec-6fce3cd99093',
