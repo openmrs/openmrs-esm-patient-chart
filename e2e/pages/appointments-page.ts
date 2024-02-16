bc721c0cd023e92224a6c1c78657e3a52e4bf41c
vijay <vijaykv2228@gmail.com>
2024-02-17 00:04:02 +0530

    test for add appointment

--- e2e/pages/appointments-page.ts
+++ e2e/pages/appointments-page.ts
@@ -3,6 +3,7 @@ import { type Page } from '@playwright/test';
 export class AppointmentsPage {
   constructor(readonly page: Page) {}
 
+  readonly appointmentsTable = () => this.page.locator('tbody');
   async goto(uuid: string) {
     await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Appointments`);
   }
--- e2e/specs/add-appointments.spec.ts
+++ e2e/specs/add-appointments.spec.ts
@@ -11,6 +11,7 @@ test.beforeEach(async ({ api }) => {
 
 test('Add appointment for a patient, edit the added appointment and cancel it', async ({ page, api }) => {
   const appointmentsPage = new AppointmentsPage(page);
+  const row = appointmentsPage.appointmentsTable().locator('tr');
   await test.step('When I click on appointment tab', async () => {
     await appointmentsPage.goto(patient.uuid);
   });
@@ -19,5 +20,43 @@ test('Add appointment for a patient, edit the added appointment and cancel it', 
     await page.click('button:has-text("Add")');
   });
 
-  //having trouble with how to test the Schedule appointment workspace
+  await test.step('When I select Mobile Clinic location', async () => {
+    await page.locator('#location').selectOption('8d9045ad-50f0-45b8-93c8-3ed4bce19dbf');
+  });
+
+  await test.step('And I select “Outpatient Department” service', async () => {
+    await page.locator('#service').selectOption('Outpatient Department');
+  });
+
+  await test.step('And I make appointment as “Scheduled”', async () => {
+    await page.locator('#appointmentType').selectOption('Scheduled');
+  });
+
+  await test.step('And I set date for tomorrow', async () => {
+    await page.locator('#datePickerInput').fill('18/02/2024');
+  });
+
+  await test.step('And I set the “Duration”', async () => {
+    await page.locator('#duration').fill('60');
+  });
+
+  await test.step('And I add a note', async () => {
+    await page.getByPlaceholder(/Write any additional points here/i).fill('Testing Appointments notes');
+  });
+
+  await test.step('And I click Save button', async () => {
+    await page.getByRole('button', { name: /save and close/i }).click();
+  });
+
+  await test.step('Then I should see a success message', async () => {
+    await expect(page.getByText(/Appointment scheduled/i)).toBeVisible();
+  });
+
+  await test.step('And the created appointment should be displayed in the upcoming appointments table', async () => {
+    await expect(row).toContainText('Mobile Clinic');
+    await expect(row).toContainText('Outpatient Department');
+    await expect(row).toContainText('Scheduled');
+    await expect(row).toContainText('14-Feb-2024');
+    await expect(row).toContainText('60');
+  });
 });
