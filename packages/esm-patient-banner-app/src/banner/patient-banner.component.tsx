b7ef7b48525c4ee91502b7ab0931fcd987a409b6
Brandon Istenes <bistenes@gmail.com>
2024-02-17 19:38:16 -0500

    Start moving stuff

--- packages/esm-patient-banner-app/src/banner/patient-banner.component.tsx
+++ packages/esm-patient-banner-app/src/banner/patient-banner.component.tsx
@@ -1,9 +1,9 @@
-import React, { type MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { type useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
-import { Button, Tag } from '@carbon/react';
+import { Button } from '@carbon/react';
 import { ChevronDown, ChevronUp, OverflowMenuVertical } from '@carbon/react/icons';
-import { ExtensionSlot, age, formatDate, parseDate, useConfig, useConnectedExtensions } from '@openmrs/esm-framework';
+import { ExtensionSlot, PatientBannerPatientInfo, useConnectedExtensions } from '@openmrs/esm-framework';
 import ContactDetails from '../contact-details/contact-details.component';
 import CustomOverflowMenuComponent from '../ui-components/overflow-menu.component';
 import styles from './patient-banner.scss';
@@ -11,23 +11,14 @@ import styles from './patient-banner.scss';
 interface PatientBannerProps {
   patient: fhir.Patient;
   patientUuid: string;
-  onClick?: (patientUuid: string) => void;
-  onTransition?: () => void;
   hideActionsOverflow?: boolean;
 }
 
-const PatientBanner: React.FC<PatientBannerProps> = ({
-  patient,
-  patientUuid,
-  onClick,
-  onTransition,
-  hideActionsOverflow,
-}) => {
+const PatientBanner: React.FC<PatientBannerProps> = ({ patient, patientUuid, hideActionsOverflow }) => {
   const { t } = useTranslation();
   const overflowMenuRef = useRef(null);
   const patientBannerRef = useRef(null);
   const [isTabletViewport, setIsTabletViewport] = useState(false);
-  const { excludePatientIdentifierCodeTypes } = useConfig();
   const patientActions = useConnectedExtensions('patient-actions-slot');
 
   useEffect(() => {
@@ -45,10 +36,7 @@ const PatientBanner: React.FC<PatientBannerProps> = ({
     };
   }, [patientBannerRef, setIsTabletViewport]);
 
-  const patientActionsSlotState = useMemo(
-    () => ({ patientUuid, onClick, onTransition }),
-    [patientUuid, onClick, onTransition],
-  );
+  const patientActionsSlotState = useMemo(() => ({ patientUuid }), [patientUuid]);
 
   const patientName = `${patient?.name?.[0]?.given?.join(' ')} ${patient?.name?.[0].family}`;
   const patientPhotoSlotState = useMemo(() => ({ patientUuid, patientName }), [patientUuid, patientName]);
@@ -66,44 +54,10 @@ const PatientBanner: React.FC<PatientBannerProps> = ({
     </div>
   );
 
-  const handleNavigateToPatientChart = useCallback(
-    (event: MouseEvent) => {
-      if (onClick) {
-        !(overflowMenuRef?.current && overflowMenuRef?.current.contains(event.target)) && onClick(patientUuid);
-      }
-    },
-    [onClick, patientUuid],
-  );
-
   const [showDropdown, setShowDropdown] = useState(false);
   const closeDropdownMenu = useCallback(() => {
     setShowDropdown((value) => !value);
   }, []);
-
-  const showActionsMenu = useMemo(
-    () => !hideActionsOverflow && patientActions.length > 0,
-    [patientActions.length, hideActionsOverflow],
-  );
-
-  const getGender = (gender: string): string => {
-    switch (gender) {
-      case 'male':
-        return t('male', 'Male');
-      case 'female':
-        return t('female', 'Female');
-      case 'other':
-        return t('other', 'Other');
-      case 'unknown':
-        return t('unknown', 'Unknown');
-      default:
-        return gender;
-    }
-  };
-
-  const identifiers =
-    patient?.identifier?.filter(
-      (identifier) => !excludePatientIdentifierCodeTypes?.uuids.includes(identifier.type.coding[0].code),
-    ) ?? [];
 
   return (
     <header
@@ -113,76 +67,44 @@ const PatientBanner: React.FC<PatientBannerProps> = ({
       )}
       ref={patientBannerRef}
     >
-      <div
-        className={classNames(styles.patientBanner, { [styles.patientAvatarButton]: onClick })}
-        onClick={handleNavigateToPatientChart}
-        role="button"
-        tabIndex={0}
-      >
+      <div className={styles.patientBanner}>
         {patientAvatar}
-        <div className={styles.patientInfo}>
-          <div className={classNames(styles.row, styles.patientNameRow)}>
-            <div className={styles.flexRow}>
-              <span className={styles.patientName}>{patientName}</span>
-              <ExtensionSlot
-                name="patient-banner-tags-slot"
-                state={{ patientUuid, patient }}
-                className={styles.flexRow}
-              />
+        <PatientBannerPatientInfo />
+        <div className={styles.buttonCol}>
+          {!hideActionsOverflow && patientActions.length > 0 ? (
+            <div className={styles.overflowMenuContainer} ref={overflowMenuRef}>
+              <CustomOverflowMenuComponent
+                deceased={isDeceased}
+                menuTitle={
+                  <>
+                    <span className={styles.actionsButtonText}>{t('actions', 'Actions')}</span>{' '}
+                    <OverflowMenuVertical size={16} style={{ marginLeft: '0.5rem', fill: '#78A9FF' }} />
+                  </>
+                }
+                dropDownMenu={showDropdown}
+              >
+                <ExtensionSlot
+                  onClick={closeDropdownMenu}
+                  name="patient-actions-slot"
+                  key="patient-actions-slot"
+                  className={styles.overflowMenuItemList}
+                  state={patientActionsSlotState}
+                />
+              </CustomOverflowMenuComponent>
             </div>
-            {showActionsMenu && (
-              <div className={styles.overflowMenuContainer} ref={overflowMenuRef}>
-                <CustomOverflowMenuComponent
-                  deceased={isDeceased}
-                  menuTitle={
-                    <>
-                      <span className={styles.actionsButtonText}>{t('actions', 'Actions')}</span>{' '}
-                      <OverflowMenuVertical size={16} style={{ marginLeft: '0.5rem', fill: '#78A9FF' }} />
-                    </>
-                  }
-                  dropDownMenu={showDropdown}
-                >
-                  <ExtensionSlot
-                    onClick={closeDropdownMenu}
-                    name="patient-actions-slot"
-                    key="patient-actions-slot"
-                    className={styles.overflowMenuItemList}
-                    state={patientActionsSlotState}
-                  />
-                </CustomOverflowMenuComponent>
-              </div>
-            )}
-          </div>
-          <div className={styles.demographics}>
-            <span>{getGender(patient?.gender)}</span> &middot; <span>{age(patient?.birthDate)}</span> &middot;{' '}
-            <span>{formatDate(parseDate(patient?.birthDate), { mode: 'wide', time: false })}</span>
-          </div>
-          <div className={styles.row}>
-            <div className={styles.identifiers}>
-              {identifiers?.length
-                ? identifiers.map(({ value, type }) => (
-                    <span key={value} className={styles.identifierTag}>
-                      <Tag className={styles.tag} type="gray" title={type.text}>
-                        {type.text}
-                      </Tag>
-                      {value}
-                    </span>
-                  ))
-                : ''}
-            </div>
-            <Button
-              className={styles.toggleContactDetailsButton}
-              kind="ghost"
-              renderIcon={(props) =>
-                showContactDetails ? <ChevronUp size={16} {...props} /> : <ChevronDown size={16} {...props} />
-              }
-              iconDescription="Toggle contact details"
-              onClick={toggleContactDetails}
-              style={{ marginTop: '-0.25rem' }}
-            >
-              {showContactDetails ? t('hideDetails', 'Hide details') : t('showDetails', 'Show details')}
-            </Button>
-          </div>
+          ) : null}
+          <Button
+            className={styles.toggleContactDetailsButton}
+            kind="ghost"
+            renderIcon={(props) =>
+              showContactDetails ? <ChevronUp size={16} {...props} /> : <ChevronDown size={16} {...props} />
+            }
+            iconDescription="Toggle contact details"
+            onClick={toggleContactDetails}
+            style={{ marginTop: '-0.25rem' }}
+          >
+            {showContactDetails ? t('hideDetails', 'Hide details') : t('showDetails', 'Show details')}
+          </Button>
         </div>
       </div>
       {showContactDetails && (
