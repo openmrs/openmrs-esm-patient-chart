108b9d0f58b24bf071e13150a0ac1d57a6cd7075
nanfuka <kalungi2k6@gmail.com>
2024-02-28 11:49:09 +0300

    Mutate visits when visit note is saved

--- packages/esm-patient-notes-app/src/config-schema.ts
+++ packages/esm-patient-notes-app/src/config-schema.ts
@@ -1,7 +1,13 @@
+import { Type } from '@openmrs/esm-framework';
 import notesConfigSchema, { type VisitNoteConfigObject } from './notes/visit-note-config-schema';
 
 export const configSchema = {
   visitNoteConfig: notesConfigSchema,
+  numberOfVisitsToLoad: {
+    _type: Type.Number,
+    _description: 'The number of visits to load initially in the Visits Summary tab. Defaults to 5',
+    _default: 5,
+  },
 };
 
 export interface ConfigObject {
