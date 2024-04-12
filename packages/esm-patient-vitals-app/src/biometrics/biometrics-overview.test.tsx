da21434c1f330bb3f8ba7960ef1a7b662776c39a
Vineet Sharma <sharmava05@gmail.com>
2024-04-12 13:24:37 +0530

    Updated tests for vitals and biometrics app

--- packages/esm-patient-vitals-app/src/biometrics/biometrics-overview.test.tsx
+++ packages/esm-patient-vitals-app/src/biometrics/biometrics-overview.test.tsx
@@ -109,10 +109,17 @@ describe('BiometricsOverview: ', () => {
 
     const sortRowsButton = screen.getByRole('button', { name: /date and time/i });
 
+    // Sorting in descending order
+    // Since the date order is already in descending order, the rows should be the same
+    await user.click(sortRowsButton);
+    // Sorting in ascending order
     await user.click(sortRowsButton);
 
     expect(screen.getAllByRole('row')).not.toEqual(initialRowElements);
 
+    // Sorting order = NONE, hence it is still in the ascending order
+    await user.click(sortRowsButton);
+    // Sorting in descending order
     await user.click(sortRowsButton);
 
     expect(screen.getAllByRole('row')).toEqual(initialRowElements);
