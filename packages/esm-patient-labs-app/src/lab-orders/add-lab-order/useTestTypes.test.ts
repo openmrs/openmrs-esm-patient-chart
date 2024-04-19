5832d3beb432f2d2c706feee9b891331fe13ad40
Vineet Sharma <sharmava05@gmail.com>
2024-04-19 12:52:43 +0530

    Updated tests

--- packages/esm-patient-labs-app/src/lab-orders/add-lab-order/useTestTypes.test.ts
+++ packages/esm-patient-labs-app/src/lab-orders/add-lab-order/useTestTypes.test.ts
@@ -85,15 +85,15 @@ describe('useTestTypes filters by text', () => {
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
     expect(result.current.error).toBeFalsy();
     expect(result.current.testTypes).toEqual([
-      expect.objectContaining({ label: 'Sodium Chloride' }),
-      expect.objectContaining({ label: 'Sodium Bicarbonate' }),
       expect.objectContaining({ label: 'Potassium' }),
+      expect.objectContaining({ label: 'Sodium Bicarbonate' }),
+      expect.objectContaining({ label: 'Sodium Chloride' }),
     ]);
     rerender('sodium');
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
     expect(result.current.testTypes).toEqual([
-      expect.objectContaining({ label: 'Sodium Chloride' }),
       expect.objectContaining({ label: 'Sodium Bicarbonate' }),
+      expect.objectContaining({ label: 'Sodium Chloride' }),
     ]);
     rerender('pt');
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
