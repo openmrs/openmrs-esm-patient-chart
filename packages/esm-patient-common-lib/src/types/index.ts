4fd0cc5ae7c74dc3ad232234b47d28b4459f2739
CynthiaKamau <cynthiakamau54@gmail.com>
2024-05-07 13:12:50 +0300

    (refactor) Update PatientProgram interface to display program workflows and states (#1820)
    * (refactor) Update active programs response type to display workflows and states
    
    * code review

--- packages/esm-patient-common-lib/src/types/index.ts
+++ packages/esm-patient-common-lib/src/types/index.ts
@@ -20,10 +20,38 @@ export interface DashboardConfig extends DashboardLinkConfig {
 
 export interface PatientProgram {
   uuid: string;
+  patient?: DisplayMetadata;
+  program: {
+    uuid: string;
+    name: string;
+    allWorkflows: Array<{
+      uuid: string;
+      concept: DisplayMetadata;
+      retired: boolean;
+      states: Array<Record<string, unknown>>;
+      links?: Links;
+    }>;
+    concept: DisplayMetadata;
+    links: Links;
+  };
   display: string;
-  patient: OpenmrsResource;
-  program: OpenmrsResource;
   dateEnrolled: string;
-  dateCompleted: string;
-  location: OpenmrsResource;
+  dateCompleted: string | null;
+  location?: DisplayMetadata;
+  voided?: boolean;
+  outcome?: null;
+  states?: [];
+  links: Links;
+  resourceVersion?: string;
+}
+
+export type Links = Array<{
+  rel: string;
+  uri: string;
+}>;
+
+export interface DisplayMetadata {
+  display: string;
+  links: Links;
+  uuid: string;
 }
