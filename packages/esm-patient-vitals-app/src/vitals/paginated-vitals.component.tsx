8427782d28463fed89c9e7f6800b22d6d6e1df35
SENTHIL ATHIBAN M <93466587+senthil-k8s@users.noreply.github.com>
2024-06-14 18:44:24 +0530

    (feat) O3-2953: Add the ability to edit and delete vitals and biometrics (#1812)
    * (feat) Implement Edit and Delete actions for vitals and biometrics form
    
    * Review feedback
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-patient-vitals-app/src/vitals/paginated-vitals.component.tsx
+++ packages/esm-patient-vitals-app/src/vitals/paginated-vitals.component.tsx
@@ -16,6 +16,7 @@ import { useLayoutType, usePagination } from '@openmrs/esm-framework';
 import { PatientChartPagination } from '@openmrs/esm-patient-common-lib';
 import styles from './paginated-vitals.scss';
 import type { VitalsTableHeader, VitalsTableRow } from './types';
+import { VitalsAndBiometricsActionMenu } from '../vitals-biometrics-form/vitals-biometrics-action-menu.component';
 
 interface PaginatedVitalsProps {
   pageSize: number;
@@ -131,6 +132,7 @@ const PaginatedVitals: React.FC<PaginatedVitalsProps> = ({
                       {header.header?.content ?? header.header}
                     </TableHeader>
                   ))}
+                  <TableHeader />
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -139,13 +141,15 @@ const PaginatedVitals: React.FC<PaginatedVitalsProps> = ({
                     {row.cells.map((cell) => {
                       const vitalsObj = paginatedVitals.find((obj) => obj.id === row.id);
                       const vitalSignInterpretation = vitalsObj && vitalsObj[cell.id.substring(2) + 'Interpretation'];
-
                       return (
                         <StyledTableCell key={`styled-cell-${cell.id}`} interpretation={vitalSignInterpretation}>
                           {cell.value?.content ?? cell.value}
                         </StyledTableCell>
                       );
                     })}
+                    <TableCell className="cds--table-column-menu">
+                      <VitalsAndBiometricsActionMenu rowId={row.id} formType={'vitals'} />
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
