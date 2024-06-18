92aea9fa7a4c8429fa7b608b403a3da98fb19e7c
Dennis Kigen <kigen.work@gmail.com>
2024-06-18 09:58:47 +0300

    (refactor) O3-3445: Action button items in the patient banner should use OverflowMenuItem

--- packages/esm-patient-chart-app/src/actions-buttons/mark-patient-deceased.component.tsx
+++ packages/esm-patient-chart-app/src/actions-buttons/mark-patient-deceased.component.tsx
@@ -1,23 +1,23 @@
 import React, { useCallback } from 'react';
-import { OverflowMenuItem } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
+import { OverflowMenuItem } from '@carbon/react';
 import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import { usePatientDeceased } from '../deceased/deceased.resource';
+import styles from './action-button.scss';
 
 const MarkPatientDeceasedOverflowMenuItem = ({ patientUuid }) => {
   const { t } = useTranslation();
-  const handleClick = useCallback(() => launchPatientWorkspace('mark-patient-deceased-workspace-form'), []);
   const { isDead, isLoading: isPatientLoading } = usePatientDeceased(patientUuid);
+
+  const handleLaunchModal = useCallback(() => launchPatientWorkspace('mark-patient-deceased-workspace-form'), []);
 
   return (
     !isPatientLoading &&
     !isDead && (
       <OverflowMenuItem
+        className={styles.menuitem}
         itemText={t('markDeceased', 'Mark deceased')}
-        onClick={handleClick}
-        style={{
-          maxWidth: '100vw',
-        }}
+        onClick={handleLaunchModal}
       />
     )
   );
