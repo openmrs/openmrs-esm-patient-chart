575ea2200b5906079fb114d9afa98f5629de0962
Jamie Arodi <51090527+arodidev@users.noreply.github.com>
2024-07-15 23:23:25 +0300

    (fix) O3-3543: Fix test types search in the lab order workspace (#1910)
    * added fragment
    
    * Other useful mods
    
    * Factor out filtering from useTestTypes hook
    
    * Review feedback
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-patient-labs-app/src/lab-orders/add-lab-order/useTestTypes.ts
+++ packages/esm-patient-labs-app/src/lab-orders/add-lab-order/useTestTypes.ts
@@ -1,6 +1,5 @@
 import { useEffect, useMemo } from 'react';
 import useSWRImmutable from 'swr/immutable';
-import fuzzy from 'fuzzy';
 import { type FetchResponse, openmrsFetch, useConfig, restBaseUrl, reportError } from '@openmrs/esm-framework';
 import { type Concept } from '../../types';
 import { type ConfigObject } from '../../config-schema';
@@ -54,7 +53,7 @@ function useTestConceptsSWR(labOrderableConcepts?: Array<string>) {
   };
 }
 
-export function useTestTypes(searchTerm: string = ''): UseTestType {
+export function useTestTypes(): UseTestType {
   const { labOrderableConcepts } = useConfig<ConfigObject>().orders;
 
   const { data, isLoading, error } = useTestConceptsSWR(labOrderableConcepts.length ? labOrderableConcepts : null);
@@ -74,14 +73,8 @@ export function useTestTypes(searchTerm: string = ''): UseTestType {
       ?.sort((testConcept1, testConcept2) => testConcept1.label.localeCompare(testConcept2.label));
   }, [data]);
 
-  const filteredTestTypes = useMemo(() => {
-    return searchTerm && !isLoading && !error
-      ? fuzzy.filter(searchTerm, testConcepts, { extract: (c) => c.label }).map((result) => result.original)
-      : testConcepts;
-  }, [testConcepts, searchTerm, error, isLoading]);
-
   return {
-    testTypes: filteredTestTypes,
+    testTypes: testConcepts,
     isLoading: isLoading,
     error: error,
   };
