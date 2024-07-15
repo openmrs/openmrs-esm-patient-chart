b6147a214bdffcac1483e965d2939f864f701760
Senthil Athiban <senthilathiban0@gmail.com>
2024-07-15 18:25:19 +0530

    (test): add e2e for edit and delete vitals

--- e2e/specs/vitals.spec.ts
+++ e2e/specs/vitals.spec.ts
@@ -12,7 +12,7 @@ test.beforeEach(async ({ api }) => {
   visit = await startVisit(api, patient.uuid);
 });
 
-test('Record vital signs', async ({ page }) => {
+test('Add, edit and delete vital signs', async ({ page }) => {
   const vitalsPage = new BiometricsAndVitalsPage(page);
   const headerRow = vitalsPage.vitalsTable().locator('thead > tr');
   const dataRow = vitalsPage.vitalsTable().locator('tbody > tr');
@@ -80,6 +80,79 @@ test('Record vital signs', async ({ page }) => {
     await expect(dataRow).toContainText('65');
     await expect(dataRow).toContainText('16');
     await expect(dataRow).toContainText('98');
+  });
+
+  await test.step('When I click the overflow menu in the table row with the newly added vitals', async () => {
+    await vitalsPage.page
+      .getByRole('button', { name: /options/i })
+      .nth(0)
+      .click();
+  });
+
+  await test.step('And I click the `Edit` button', async () => {
+    await vitalsPage.page.getByRole('menuitem', { name: /edit/i }).click();
+  });
+
+  await test.step('Then I should see the vitals form launch in the workspace in edit mode', async () => {
+    await expect(vitalsPage.page.getByText(/Record vitals and biometrics/i)).toBeVisible();
+  });
+
+  await test.step('And I change the `Temperature`', async () => {
+    await vitalsPage.page.getByRole('spinbutton', { name: /temperature/i }).clear();
+    await vitalsPage.page.getByRole('spinbutton', { name: /temperature/i }).fill('42');
+  });
+
+  await test.step('And I change the change the additional notes', async () => {
+    await vitalsPage.page.getByPlaceholder(/type any additional notes here/i).clear();
+    await vitalsPage.page.getByPlaceholder(/type any additional notes here/i).fill('Vitals test');
+  });
+
+  await test.step('And I click on the `Save and close` button', async () => {
+    await vitalsPage.page.getByRole('button', { name: /save and close/i }).click();
+  });
+
+  await test.step('Then I should see a success notification', async () => {
+    await expect(vitalsPage.page.getByText(/vitals and biometrics updated/i)).toBeVisible();
+  });
+
+  await test.step('And I should see the newly edited vital signs on the page', async () => {
+    await expect(headerRow).toContainText(/temp/i);
+    await expect(headerRow).toContainText(/bp/i);
+    await expect(headerRow).toContainText(/pulse/i);
+    await expect(headerRow).toContainText(/r. rate/i);
+    await expect(headerRow).toContainText(/SPO2/i);
+    await expect(dataRow).toContainText('42');
+    await expect(dataRow).toContainText('120 / 100');
+    await expect(dataRow).toContainText('65');
+    await expect(dataRow).toContainText('16');
+    await expect(dataRow).toContainText('98');
+  });
+
+  await test.step('When I click the overflow menu in the table row with the newly edited vitals', async () => {
+    await vitalsPage.page
+      .getByRole('button', { name: /options/i })
+      .nth(0)
+      .click();
+  });
+
+  await test.step('And I click the `Delete` button', async () => {
+    await vitalsPage.page.getByRole('menuitem', { name: /delete/i }).click();
+  });
+
+  await test.step('And I see the delete modal', async () => {
+    await expect(vitalsPage.page.getByRole('heading', { name: /Delete Vitals and Biometrics/i })).toBeVisible();
+  });
+
+  await test.step('And I click the Delete button in the modal', async () => {
+    await vitalsPage.page.getByRole('button', { name: /danger Delete/i }).click();
+  });
+
+  await test.step('Then I see the success notification', async () => {
+    await expect(vitalsPage.page.getByText(/vitals and biometrics deleted/i)).toBeVisible();
+  });
+
+  await test.step('And the vitals table should be empty', async () => {
+    await expect(vitalsPage.page.getByText(/there are no vital signs to display for this patient/i)).toBeVisible();
   });
 });
 
