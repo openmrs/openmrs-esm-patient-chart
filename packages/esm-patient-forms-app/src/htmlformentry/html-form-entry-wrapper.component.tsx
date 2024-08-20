ea900796716d27d72ca8cbedd29a99316314495f
Mark Goodrich <mgoodrich@pih.org>
2024-08-20 17:16:30 -0400

    (feat) O3-3156: Saving Visit Note should mutate appointment state if appointment status updated

--- packages/esm-patient-forms-app/src/htmlformentry/html-form-entry-wrapper.component.tsx
+++ packages/esm-patient-forms-app/src/htmlformentry/html-form-entry-wrapper.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import styles from './html-form-entry-wrapper.scss';
 
 interface HtmlFormEntryWrapperProps {
@@ -15,20 +15,19 @@ const HtmlFormEntryWrapper: React.FC<HtmlFormEntryWrapperProps> = ({ closeWorksp
     return () => window.removeEventListener('message', callback); // cleanup on unmount
   }, [closeWorkspace]);
 
+  const onLoad = useCallback(() => {
+    const iframe = iframeRef.current;
+    const dashboard = iframeRef.current.contentDocument;
+    dashboard.querySelector('header')?.remove();
+    dashboard.querySelector('.patient-header')?.remove();
+    dashboard.querySelector('#breadcrumbs')?.remove();
+    iframe.style.display = 'block';
+    iframe.height = dashboard.body.scrollHeight + 30 + 'px'; // set iframe height to the height of the form, with some padding
+  }, []);
+
   return (
     <div>
-      <iframe
-        ref={iframeRef}
-        src={src}
-        className={styles.wrapper}
-        onLoad={() => {
-          const dashboard = iframeRef.current.contentDocument;
-          dashboard.querySelector('header')?.remove();
-          dashboard.querySelector('.patient-header')?.remove();
-          dashboard.querySelector('#breadcrumbs')?.remove();
-          iframeRef.current.style.display = 'block';
-        }}
-      ></iframe>
+      <iframe ref={iframeRef} src={src} className={styles.wrapper} onLoad={onLoad}></iframe>
     </div>
   );
 };
