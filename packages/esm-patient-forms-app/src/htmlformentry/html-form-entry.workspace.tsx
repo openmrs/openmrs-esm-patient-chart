a40d7d76742d3ccc7beb8eef69df692af186c8bd
Mark Goodrich <mgoodrich@pih.org>
2024-08-29 10:22:55 -0400

    (feat) O3-3866: Allow customized message/dialog for warning when closing a workspace

--- packages/esm-patient-forms-app/src/htmlformentry/html-form-entry.workspace.tsx
+++ packages/esm-patient-forms-app/src/htmlformentry/html-form-entry.workspace.tsx
@@ -6,6 +6,7 @@ import {
 } from '@openmrs/esm-patient-common-lib';
 import HtmlFormEntryWrapper from './html-form-entry-wrapper.component';
 import { usePatient } from '@openmrs/esm-framework';
+import { useTranslation } from 'react-i18next';
 
 interface HtmlFormEntryComponentProps extends DefaultPatientWorkspaceProps {
   formInfo: FormEntryProps;
@@ -13,16 +14,31 @@ interface HtmlFormEntryComponentProps extends DefaultPatientWorkspaceProps {
 
 const HtmlFormEntry: React.FC<HtmlFormEntryComponentProps> = ({
   patientUuid,
+  title,
   closeWorkspaceWithSavedChanges,
   promptBeforeClosing,
+  setCancelTitle,
+  setCancelMessage,
+  setCancelConfirmText,
   formInfo,
 }) => {
+  const { t } = useTranslation();
   const { patient } = usePatient(patientUuid);
   const { currentVisit } = useVisitOrOfflineVisit(patientUuid);
   const { encounterUuid, visitUuid, htmlForm } = formInfo || {};
 
   // we always want to prompt the user before closing/hiding the workspace because we can't guarantee maintaining the state of the form
   promptBeforeClosing(() => true);
+
+  // custom messaging to display to a user when closing the workspace without using the buttons within the form
+  setCancelTitle(t('warning', 'Warning'));
+  setCancelMessage(
+    t('closeHtmlFormWarning', {
+      defaultValue: 'Please use the Save or Cancel button on the {{title}} form to close the form.',
+      replace: { title },
+    }),
+  );
+  setCancelConfirmText(t('ignoreWarning', 'Ignore Warning'));
 
   // urls for entering a new form and editing an existing form; note that we specify the returnUrl as post-message:close-workspace,
   // which tells HFE-UI to send a message to the parent window to close the workspace when the form is saved or cancelled
