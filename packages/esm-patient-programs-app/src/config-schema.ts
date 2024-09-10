05dc48808baa82d0a0d0e3f9347bfb01379a0c3d
Yassin <yassin791@gmail.com>
2024-09-10 16:03:55 +0200

    (feat) O3-3901 Add support for program states in esm-patient-programs (#1989)
    * (feat) O3-3901 Add support for program states in esm-patient-programs
    
    * (refactor) PR comments
    
    * (fix) Failing unit test
    
    * Minor tweaks
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>

--- packages/esm-patient-programs-app/src/config-schema.ts
+++ packages/esm-patient-programs-app/src/config-schema.ts
@@ -5,8 +5,15 @@ export const configSchema = {
     _type: Type.Boolean,
     _default: false,
   },
+  showProgramStatusField: {
+    _type: Type.Boolean,
+    _description:
+      'Whether to show the Program status field in the Record program enrollment and Edit program enrollment forms. If set to true, the `Program status` field is displayed in the Programs datatable',
+    _default: false,
+  },
 };
 
 export interface ConfigObject {
   hideAddProgramButton: boolean;
+  showProgramStatusField: boolean;
 }
