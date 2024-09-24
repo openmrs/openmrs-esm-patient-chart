bc5cd55fe1b855676257095ff168f959a8160556
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-09-24 12:08:27 +0300

    (fix) extract translations

--- packages/esm-patient-orders-app/src/components/order-stock-details.component.tsx
+++ packages/esm-patient-orders-app/src/components/order-stock-details.component.tsx
@@ -3,12 +3,14 @@ import { useOrderStockInfo } from '../hooks/useOrderStockInfo';
 import { CheckmarkFilled, CloseFilled } from '@carbon/react/icons';
 import styles from './order-stock-details.scss';
 import { SkeletonText } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
 
 interface OrderStockDetailsComponentProps {
   orderItemUuid: string;
 }
 
 const OrderStockDetailsComponent: React.FC<OrderStockDetailsComponentProps> = ({ orderItemUuid }) => {
+  const { t } = useTranslation();
   const { data: stockData, isLoading } = useOrderStockInfo(orderItemUuid);
 
   if (isLoading || !stockData) {
@@ -21,11 +23,11 @@ const OrderStockDetailsComponent: React.FC<OrderStockDetailsComponentProps> = ({
     <div>
       {isInStock ? (
         <div className={styles.itemInStock}>
-          <CheckmarkFilled size={16} /> In Stock
+          <CheckmarkFilled size={16} /> {t('inStock', 'In Stock')}
         </div>
       ) : (
         <div className={styles.itemOutOfStock}>
-          <CloseFilled size={16} /> Out of Stock
+          <CloseFilled size={16} /> {t('outOfStock', 'Out of Stock')}
         </div>
       )}
     </div>
