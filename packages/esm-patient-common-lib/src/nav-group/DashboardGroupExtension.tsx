882b5943064a2d8ef5a9b2fa56918cf48863b6df
Ian <ian.c.bacher@gmail.com>
2024-09-28 13:39:30 -0400

    Remove unrelated changes

--- packages/esm-patient-common-lib/src/nav-group/DashboardGroupExtension.tsx
+++ packages/esm-patient-common-lib/src/nav-group/DashboardGroupExtension.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import { Accordion, AccordionItem } from '@carbon/react';
 import { registerNavGroup } from '..';
@@ -12,14 +11,13 @@ export interface DashboardGroupExtensionProps {
 }
 
 export const DashboardGroupExtension = ({ title, slotName, basePath, isExpanded }: DashboardGroupExtensionProps) => {
-  const { t } = useTranslation();
   useEffect(() => {
     registerNavGroup(slotName);
   }, [slotName]);
 
   return (
     <Accordion>
-      <AccordionItem open={isExpanded ?? true} title={t(title)} style={{ border: 'none' }}>
+      <AccordionItem open={isExpanded ?? true} title={title} style={{ border: 'none' }}>
         <ExtensionSlot name={slotName ?? title} state={{ basePath }} />
       </AccordionItem>
     </Accordion>
