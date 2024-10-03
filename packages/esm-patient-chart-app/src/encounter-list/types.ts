e2ffc0132946f5d6e633625642281c6b6485e5be
CynthiaKamau <cynthiakamau54@gmail.com>
2024-10-03 19:30:27 +0300

    code review

--- packages/esm-patient-chart-app/src/encounter-list/types.ts
+++ packages/esm-patient-chart-app/src/encounter-list/types.ts
@@ -1,14 +1,42 @@
 import { type OpenmrsResource } from '@openmrs/esm-framework';
+import { type ReactElement } from 'react';
 
 export interface Encounter extends OpenmrsResource {
   encounterDatetime: Date;
-  encounterType: { uuid: string };
-  patient: string;
-  location: string;
-  encounterProviders?: Array<{ encounterRole: string; provider: string }>;
-  obs: Array<OpenmrsResource>;
-  form?: string;
+  encounterType: { uuid: string; name: string };
+  patient: {
+    uuid: string;
+    display: string;
+    age: number;
+    birthDate: string;
+  };
+  location: {
+    uuid: string;
+    display: string;
+    name: string;
+  };
+  encounterProviders?: Array<{ encounterRole: string; provider: { uuid: string; name: string } }>;
+  obs: Array<Observation>;
+  form?: {
+    uuid: string;
+  };
   visit?: string;
+}
+
+export interface Observation {
+  uuid: string;
+  concept: { uuid: string; name: string };
+  value:
+    | {
+        uuid: string;
+        name: {
+          name: string;
+        };
+        names?: Array<{ uuid: string; name: string; conceptNameType: string }>;
+      }
+    | string;
+  groupMembers?: Array<Observation>;
+  obsDatetime: string;
 }
 
 export interface ListResponse<T> {
@@ -142,4 +170,18 @@ export interface TabSchema {
   columns: Array<ColumnDefinition>;
   formList: Array<{ name: string; uuid: string; fixedIntent?: string; excludedIntents?: Array<string> }>;
   launchOptions: LaunchOptions;
+}
+
+export type Mode = 'edit' | 'view';
+
+export interface Action {
+  label: string;
+  mode: Mode;
+  form?: { name: string };
+  intent?: string;
+}
+
+export interface TableRow {
+  id: string;
+  actions: Action[] | ReactElement | null;
 }
