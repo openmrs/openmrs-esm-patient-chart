57089e02ccb1dd317272ecb052af2c4a49a976f7
Munyua123 <earlmunyua96@gmail.com>
2024-10-25 09:14:46 +0300

    O3-4026 Add an icon for vitals and biometrics on the left nav

--- packages/esm-patient-common-lib/src/dashboards/DashboardExtension.tsx
+++ packages/esm-patient-common-lib/src/dashboards/DashboardExtension.tsx
@@ -4,6 +4,7 @@ import last from 'lodash-es/last';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { ConfigurableLink } from '@openmrs/esm-framework';
+import { Activity } from '@carbon/react/icons';
 
 export interface DashboardExtensionProps {
   path: string;
@@ -22,13 +23,18 @@ export const DashboardExtension = ({
   const location = useLocation();
   const navLink = useMemo(() => decodeURIComponent(last(location.pathname.split('/'))), [location.pathname]);
 
+  const renderIcon = title === 'Vitals & Biometrics' ? <Activity style={{ marginRight: '8px' }} /> : null;
+
   return (
-    <div key={path}>
+    <div key={path} style={{ display: 'flex', alignItems: 'center' }}>
       <ConfigurableLink
         className={classNames('cds--side-nav__link', { 'active-left-nav-link': path === navLink })}
         to={`${basePath}/${encodeURIComponent(path)}`}
       >
-        {t(title)}
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+          {renderIcon}
+          <span>{t(title)}</span>
+        </span>
       </ConfigurableLink>
     </div>
   );
