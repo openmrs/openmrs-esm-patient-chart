953327cb3597726b08f5b0baa6b0ae6eca906f4a
jwnasambu <wamalwa1844.com>
2024-11-04 17:35:24 +0300

    Update the failing import

--- packages/esm-patient-chart-app/src/patient-chart/patient-chart.component.tsx
+++ packages/esm-patient-chart-app/src/patient-chart/patient-chart.component.tsx
@@ -17,7 +17,7 @@ import Loader from '../loader/loader.component';
 import styles from './patient-chart.scss';
 import VisitHeader from '../visit-header/visit-header.component';
 import SideMenuPanel from '../side-nav/side-menu.component';
-import { getPatientChartStore } from '@openmrs/esm-patient-common-lib';
+import { getPatientChartStore } from '@openmrs/esm-patient-common-lib/src/store/patient-chart-store';
 
 const PatientChart: React.FC = () => {
   const { patientUuid, view: encodedView } = useParams();
