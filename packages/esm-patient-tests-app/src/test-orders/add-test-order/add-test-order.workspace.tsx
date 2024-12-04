9f0f9b11be23925664d136811e589fa3276a4b2e
Vineet Sharma <sharmava05@gmail.com>
2024-12-04 23:59:49 +0530

    Add 'ward-patient' workspace to the order basket related workspaces

--- packages/esm-patient-tests-app/src/test-orders/add-test-order/add-test-order.workspace.tsx
+++ packages/esm-patient-tests-app/src/test-orders/add-test-order/add-test-order.workspace.tsx
@@ -78,6 +78,7 @@ export default function AddLabOrderWorkspace({
     closeWorkspace({
       ignoreChanges: true,
       onWorkspaceClose: () => launchPatientWorkspace('order-basket'),
+      closeWorkspaceGroup: false,
     });
   }, [closeWorkspace]);
 
