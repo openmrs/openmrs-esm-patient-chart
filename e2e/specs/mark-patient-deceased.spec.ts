2da373ae507cbf6b89735e8e042e4c806c308385
Viraj Wathsala Gunasinghe <virajwgunasinghe@gmail.com>
2024-12-16 11:18:22 +0530

    Update mark-patient-deceased.spec.ts

--- e2e/specs/mark-patient-deceased.spec.ts
+++ e2e/specs/mark-patient-deceased.spec.ts
@@ -48,9 +48,8 @@ test('Mark a patient as deceased', async ({ page }) => {
   });
 
    await test.step('Then I should see a “deceased” tag in the patient banner', async () => {
-    // Wait for the "Deceased" tag to be visible before asserting
     await markPatientDeceasedPage.verifyDeceasedTag();
-  });
+  });
 });
 
 test.afterEach(async ({ api }) => {
