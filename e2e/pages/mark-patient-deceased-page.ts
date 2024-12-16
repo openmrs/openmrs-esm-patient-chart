b60beb3667f86e7168c40d0b29818857bd5d56c6
Viraj Wathsala Gunasinghe <virajwgunasinghe@gmail.com>
2024-12-16 11:09:53 +0530

    Update mark-patient-deceased-page.ts

--- e2e/pages/mark-patient-deceased-page.ts
+++ e2e/pages/mark-patient-deceased-page.ts
@@ -32,7 +32,7 @@ export class MarkPatientDeceasedPage {
 
   async verifyDeceasedTag() {
     await this.page.waitForSelector('div:has-text("Deceased")', { state: 'visible' });  // Ensure "Deceased" is visible
-   await expect(this.deceasedTag()).toBeVisible({ timeout: 10000 });  // Waits up to 10 seconds for visibility
+   await expect(this.deceasedTag()).toBeVisible({ timeout: 40000 });  // Waits up to 10 seconds for visibility
 
   }
 }
