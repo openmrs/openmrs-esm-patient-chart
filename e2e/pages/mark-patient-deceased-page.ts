fb2c3185b445601a57eaf5517bba27dd865acbbc
Viraj Wathsala Gunasinghe <virajwgunasinghe@gmail.com>
2024-12-19 13:16:18 +0530

    Update e2e/pages/mark-patient-deceased-page.ts
    Co-authored-by: Daud Kakumirizi <dkakumirizii@gmail.com>

--- e2e/pages/mark-patient-deceased-page.ts
+++ e2e/pages/mark-patient-deceased-page.ts
@@ -3,7 +3,6 @@ import { type Page } from '@playwright/test';
 export class MarkPatientDeceasedPage {
   constructor(readonly page: Page) {}
 
-
   async goTo(patientUuid: string) {
     await this.page.goto('/openmrs/spa/patient/' + patientUuid + '/chart/Patient%20Summary');
   }
