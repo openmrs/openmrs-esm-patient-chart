077a8876a33a7308611eeeb2ae69103ab39a0053
Viraj Wathsala Gunasinghe <virajwgunasinghe@gmail.com>
2024-12-19 07:36:32 +0530

    Update mark-patient-deceased-page.ts

--- e2e/pages/mark-patient-deceased-page.ts
+++ e2e/pages/mark-patient-deceased-page.ts
@@ -2,8 +2,9 @@ import { type Page } from '@playwright/test';
 
 export class MarkPatientDeceasedPage {
   constructor(private readonly page: Page) {}
-
-  getPatientChartPath(patientUuid: string): string {
-    return `/openmrs/spa/patient/${patientUuid}/chart/Patient%20Summary`;
+  
+  async goTo(patientUuid: string) {
+    const patientChartPath = `/openmrs/spa/patient/${patientUuid}/chart/Patient%20Summary`;
+    await this.page.goto(patientChartPath);
   }
 }
