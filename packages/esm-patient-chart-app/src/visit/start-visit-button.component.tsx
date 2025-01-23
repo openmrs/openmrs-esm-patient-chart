2b0cec5b01986d8a0e046b68f54dcc7c0076062b
Dennis Kigen <kigen.work@gmail.com>
2025-01-23 13:24:54 +0300

    This commit makes some additional improvements:
    - Add a `StartVisitButtonProps` interface for annotating the `StartVisitButton` component.
    - Add a test for the `StartVisitButton` component.
    - Reorder imports.
    - Show a snackbar when an error occurs while launching the visit form workspace.

--- packages/esm-patient-chart-app/src/visit/start-visit-button.component.tsx
+++ packages/esm-patient-chart-app/src/visit/start-visit-button.component.tsx
@@ -1,24 +1,37 @@
-import { Button } from '@carbon/react';
-import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import React, { useCallback } from 'react';
+import { Button } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
+import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
+import { showSnackbar } from '@openmrs/esm-framework';
 
-const StartVisitButton = ({ patientUuid }) => {
+interface StartVisitButtonProps {
+  patientUuid: string;
+}
+
+const StartVisitButton = ({ patientUuid }: StartVisitButtonProps) => {
   const { t } = useTranslation();
+  const startVisitWorkspaceForm = 'start-visit-workspace-form';
 
   const handleStartVisit = useCallback(() => {
     try {
-      launchPatientWorkspace('start-visit-workspace-form', {
+      launchPatientWorkspace(startVisitWorkspaceForm, {
         patientUuid,
         openedFrom: 'patient-chart-start-visit',
       });
     } catch (error) {
-      console.error('Error launching Start Visit workspace:', error);
+      console.error('Error launching visit form workspace:', error);
+
+      showSnackbar({
+        isLowContrast: false,
+        kind: 'error',
+        title: t('errorStartingVisit', 'Error starting visit'),
+        subtitle: error.message ?? t('errorStartingVisitDescription', 'An error occurred while starting the visit'),
+      });
     }
-  }, [patientUuid]);
+  }, [patientUuid, t]);
 
   return (
-    <Button kind="primary" onClick={handleStartVisit} aria-label={t('startVisit', 'Start visit')}>
+    <Button aria-label={t('startVisit', 'Start visit')} kind="primary" onClick={handleStartVisit}>
       {t('startVisit', 'Start visit')}
     </Button>
   );
