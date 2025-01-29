9aebcb18472659ecb16ba7257bf63fbdbd132866
Earl Munyua <142984533+Munyua123@users.noreply.github.com>
2025-01-29 22:24:14 +0300

    (feat) O3-2801 - Add icons to patient chart left nav (#2121)

--- packages/esm-patient-chart-app/src/dashboard.meta.ts
+++ packages/esm-patient-chart-app/src/dashboard.meta.ts
@@ -2,10 +2,12 @@ export const summaryDashboardMeta = {
   slot: 'patient-chart-summary-dashboard-slot',
   path: 'Patient Summary',
   title: 'Patient Summary',
+  icon: 'omrs-icon-report',
 };
 
 export const encountersDashboardMeta = {
   slot: 'patient-chart-encounters-dashboard-slot',
   path: 'Visits',
   title: 'Visits',
+  icon: 'omrs-icon-calendar-heat-map',
 };
