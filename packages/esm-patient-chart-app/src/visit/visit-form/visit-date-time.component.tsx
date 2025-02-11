270d867a780fb6078d7cd5e27aadb631ddd4f1b7
Chi Bong Ho <cbho@pih.org>
2025-02-11 16:52:36 -0500

    (feat) O3-4443 Add ability to create a visit that ended in the past

--- packages/esm-patient-chart-app/src/visit/visit-form/visit-date-time.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visit-form/visit-date-time.component.tsx
@@ -1,123 +1,207 @@
-import React from 'react';
-import classNames from 'classnames';
-import dayjs from 'dayjs';
 import { DatePicker, DatePickerInput, SelectItem, TimePicker, TimePickerSelect } from '@carbon/react';
-import { Controller, useFormContext } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
 import { ResponsiveWrapper } from '@openmrs/esm-framework';
 import { type amPm } from '@openmrs/esm-patient-common-lib';
-import { type VisitFormData } from './visit-form.resource';
+import classNames from 'classnames';
+import dayjs from 'dayjs';
+import React from 'react';
+import { type Control, Controller, type FieldPath, useFormContext, useWatch } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { convertToDate, type VisitFormData } from './visit-form.resource';
 import styles from './visit-form.scss';
 
-interface VisitDateTimeFieldProps {
-  visitDatetimeLabel: string;
-  dateFieldName: 'visitStartDate' | 'visitStopDate';
-  timeFieldName: 'visitStartTime' | 'visitStopTime';
-  timeFormatFieldName: 'visitStartTimeFormat' | 'visitStopTimeFormat';
-  /** minDate: Milliseconds since Jan 1 1970. */
-  minDate?: number;
-  /** maxDate: Milliseconds since Jan 1 1970. */
-  maxDate?: number;
+interface VisitDateTimeSectionProps {
+  control: Control<VisitFormData, any>;
+  firstEncounterDateTime: number;
+  lastEncounterDateTime: number;
 }
 
-const VisitDateTimeField: React.FC<VisitDateTimeFieldProps> = ({
-  visitDatetimeLabel,
-  dateFieldName,
-  timeFieldName,
-  timeFormatFieldName,
-  minDate,
-  maxDate,
+/**
+ * The component conditionally renders the Visit start and end
+ * date / time fields based on the visit status (new / ongoing / past)
+ */
+const VisitDateTimeSection: React.FC<VisitDateTimeSectionProps> = ({
+  control,
+  firstEncounterDateTime,
+  lastEncounterDateTime,
 }) => {
   const { t } = useTranslation();
-  const {
+  const [
+    visitStatus,
+    visitStartDate,
+    visitStartTime,
+    visitStartTimeFormat,
+    visitStopDate,
+    visitStopTime,
+    visitStopTimeFormat,
+  ] = useWatch({
     control,
-    formState: { errors },
-  } = useFormContext<VisitFormData>();
+    name: [
+      'visitStatus',
+      'visitStartDate',
+      'visitStartTime',
+      'visitStartTimeFormat',
+      'visitStopDate',
+      'visitStopTime',
+      'visitStopTimeFormat',
+    ],
+  });
 
-  // Since we have the separate date and time fields, the final validation needs to be done at the form
-  // submission, hence just using the min date with hours/ minutes/ seconds set to 0 and max date set to
-  // last second of the day. We want to just compare dates and not time.
-  const minDateObj = minDate ? dayjs(new Date(minDate).setHours(0, 0, 0, 0)).format('DD/MM/YYYY') : null;
-  const maxDateObj = maxDate ? dayjs(new Date(maxDate).setHours(23, 59, 59, 59)).format('DD/MM/YYYY') : null;
+  const hasStartTime = ['ongoing', 'past'].includes(visitStatus);
+  const hasStopTime = 'past' == visitStatus;
+  const selectedVisitStartDateTime = convertToDate(visitStartDate, visitStartTime, visitStartTimeFormat);
+  const selectedVisitStopDateTime = convertToDate(visitStopDate, visitStopTime, visitStopTimeFormat);
+
+  if (visitStatus == 'new') {
+    return <></>;
+  }
 
   return (
     <section>
-      <h1 className={styles.sectionTitle}>{visitDatetimeLabel}</h1>
-      <div className={classNames(styles.dateTimeSection, styles.sectionField)}>
-        <Controller
-          name={dateFieldName}
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <ResponsiveWrapper>
-              <DatePicker
-                className={styles.datePicker}
-                dateFormat="d/m/Y"
-                datePickerType="single"
-                id={dateFieldName}
-                maxDate={maxDateObj}
-                minDate={minDateObj}
-                onChange={([date]) => onChange(date)}
-                value={value ? dayjs(value).format('DD/MM/YYYY') : null}
-              >
-                <DatePickerInput
-                  id={`${dateFieldName}Input`}
-                  invalid={Boolean(errors[dateFieldName])}
-                  invalidText={errors[dateFieldName]?.message}
-                  labelText={t('date', 'Date')}
-                  placeholder="dd/mm/yyyy"
-                  style={{ width: '100%' }}
-                />
-              </DatePicker>
-            </ResponsiveWrapper>
-          )}
-        />
-        <ResponsiveWrapper>
-          <Controller
-            name={timeFieldName}
-            control={control}
-            render={({ field: { onBlur, onChange, value } }) => (
-              <div className={styles.timePickerContainer}>
-                <TimePicker
-                  className={styles.timePicker}
-                  id={timeFieldName}
-                  invalid={Boolean(errors[timeFieldName])}
-                  invalidText={errors[timeFieldName]?.message}
-                  labelText={t('time', 'Time')}
-                  onBlur={onBlur}
-                  onChange={(event) => onChange(event.target.value as amPm)}
-                  pattern="^(0[1-9]|1[0-2]):([0-5][0-9])$"
-                  value={value}
-                >
-                  <Controller
-                    name={timeFormatFieldName}
-                    control={control}
-                    render={({ field: { onChange, value } }) => (
-                      <TimePickerSelect
-                        aria-label={t('timeFormat ', 'Time Format')}
-                        className={classNames({
-                          [styles.timePickerSelectError]: errors[timeFormatFieldName],
-                        })}
-                        id={`${timeFormatFieldName}Input`}
-                        onChange={(event) => onChange(event.target.value as amPm)}
-                        value={value}
-                      >
-                        <SelectItem value="" text="" />
-                        <SelectItem value="AM" text="AM" />
-                        <SelectItem value="PM" text="PM" />
-                      </TimePickerSelect>
-                    )}
-                  />
-                </TimePicker>
-                {errors[timeFormatFieldName] && (
-                  <div className={styles.timerPickerError}>{errors[timeFormatFieldName]?.message}</div>
-                )}
-              </div>
-            )}
-          />
-        </ResponsiveWrapper>
+      <div className={styles.sectionTitle}>
+        {visitStatus == 'ongoing'
+          ? t('visitStartDate', 'Visit start date')
+          : t('visitStartAndEndDate', 'Visit start and end date')}
       </div>
+      {hasStartTime && (
+        <VisitDateTimeField
+          dateField={{ name: 'visitStartDate', label: t('startDate', 'Start date') }}
+          timeField={{ name: 'visitStartTime', label: t('startTime', 'Start time') }}
+          timeFormatField={{ name: 'visitStartTimeFormat', label: t('startTimeFormat', 'Start time format') }}
+          maxDate={Math.min(firstEncounterDateTime, selectedVisitStopDateTime?.getTime(), Date.now())}
+        />
+      )}
+      {hasStopTime && (
+        <VisitDateTimeField
+          dateField={{ name: 'visitStopDate', label: t('endDate', 'End date') }}
+          timeField={{ name: 'visitStopTime', label: t('endTime', 'End time') }}
+          timeFormatField={{ name: 'visitStopTimeFormat', label: t('endTimeFormat', 'End time format') }}
+          minDate={Math.max(lastEncounterDateTime, selectedVisitStartDateTime?.getTime())}
+          maxDate={Date.now()}
+        />
+      )}
     </section>
   );
 };
 
-export default VisitDateTimeField;
+interface DateTimeFieldProps {
+  dateField: Field;
+  timeField: Field;
+  timeFormatField: Field;
+  minDate?: dayjs.ConfigType;
+  maxDate?: dayjs.ConfigType;
+  disabled?: boolean;
+}
+
+interface Field {
+  name: FieldPath<VisitFormData>;
+  label: string;
+}
+
+/**
+ * This components renders a DatePicker, TimePicker and AM / PM dropdown
+ * used to input a Date.
+ * It is used by the visit form for the start and end time inputs.
+ */
+const VisitDateTimeField: React.FC<DateTimeFieldProps> = ({
+  dateField,
+  timeField,
+  timeFormatField,
+  minDate,
+  maxDate,
+  disabled,
+}) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<VisitFormData>();
+  const { t } = useTranslation();
+
+  // Since we have the separate date and time fields, the full validation is done by zod.
+  // We are just using minDateObj and maxDateObj to restrict the bounds of the DatePicker.
+  const minDateObj = minDate ? dayjs(minDate).startOf('day') : null;
+  const maxDateObj = maxDate ? dayjs(maxDate).endOf('day') : null;
+
+  return (
+    <div className={classNames(styles.dateTimeSection, styles.sectionField)}>
+      <Controller
+        name={dateField.name}
+        control={control}
+        render={({ field: { onChange, value, onBlur } }) => {
+          return (
+            <ResponsiveWrapper>
+              <DatePicker
+                dateFormat="d/m/Y"
+                datePickerType="single"
+                id={dateField.name}
+                maxDate={maxDateObj?.valueOf()}
+                minDate={minDateObj?.valueOf()}
+                onChange={([date]: [date: Date]) => {
+                  onChange(dayjs(date).format('YYYY-MM-DD'));
+                }}
+                value={dayjs(value as string).valueOf()}
+                onBlur={onBlur}
+              >
+                <DatePickerInput
+                  id={`${dateField.name}Input`}
+                  invalid={Boolean(errors[dateField.name])}
+                  invalidText={errors[dateField.name]?.message}
+                  labelText={dateField.label}
+                  placeholder="dd/mm/yyyy"
+                  style={{ width: '100%' }}
+                  disabled={disabled}
+                />
+              </DatePicker>
+            </ResponsiveWrapper>
+          );
+        }}
+      />
+      <ResponsiveWrapper>
+        <Controller
+          name={timeField.name}
+          control={control}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <div className={styles.timePickerContainer}>
+              <TimePicker
+                className={styles.timePicker}
+                id={timeField.name}
+                invalid={Boolean(errors[timeField.name])}
+                invalidText={errors[timeField.name]?.message}
+                labelText={timeField.label}
+                onBlur={onBlur}
+                onChange={(event) => onChange(event.target.value as amPm)}
+                pattern="^(0[1-9]|1[0-2]):([0-5][0-9])$"
+                value={value}
+                disabled={disabled}
+              >
+                <Controller
+                  name={timeFormatField.name}
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <TimePickerSelect
+                      aria-label={timeFormatField.label}
+                      className={classNames({
+                        [styles.timePickerSelectError]: errors[timeFormatField.name],
+                      })}
+                      id={`${timeFormatField.name}Input`}
+                      onChange={(event) => onChange(event.target.value as amPm)}
+                      value={value}
+                      disabled={disabled}
+                    >
+                      <SelectItem value="AM" text="AM" />
+                      <SelectItem value="PM" text="PM" />
+                    </TimePickerSelect>
+                  )}
+                />
+              </TimePicker>
+              {errors[timeFormatField.name] && (
+                <div className={styles.timerPickerError}>{errors[timeFormatField.name]?.message}</div>
+              )}
+            </div>
+          )}
+        />
+      </ResponsiveWrapper>
+    </div>
+  );
+};
+
+export default VisitDateTimeSection;
