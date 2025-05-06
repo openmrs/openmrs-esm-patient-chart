0d4cf60e87537c704001cf3a47d791a4e582202a
Amos Machora <amosmachora13@gmail.com>
2025-05-06 17:55:52 +0300

    fix: allow for passing of the form control via props and improve date time picker responsiveness

--- packages/esm-patient-chart-app/src/visit/visits-widget/visit-context/retrospective-data-date-time-picker/retrospective-date-time-picker.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visits-widget/visit-context/retrospective-data-date-time-picker/retrospective-date-time-picker.component.tsx
@@ -1,49 +1,58 @@
 import { SelectItem, TimePickerSelect, TimePicker, Checkbox } from '@carbon/react';
 import { OpenmrsDatePicker, ResponsiveWrapper, useFeatureFlag, useVisit } from '@openmrs/esm-framework';
 import React, { useEffect, useState } from 'react';
-import { Controller, useForm, useFormContext } from 'react-hook-form';
+import { type Control, Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import styles from './restrospective-date-time-picker.scss';
 import { useSystemVisitSetting } from '@openmrs/esm-patient-common-lib';
 
 type TFormValues = {
-  'retrospective-date': string;
+  'retrospective-date': Date;
   'retrospective-time': string;
   'retrospective-time-format': string;
 };
 
+type IRetrospectiveDateTimePickerProps = {
+  patientUuid: string;
+  control: Control<TFormValues>;
+  maxDate?: Date;
+  minDate?: Date;
+  onChange?: (data: TFormValues) => void;
+};
+
 const RetrospectiveDateTimePicker = ({
   patientUuid,
+  control: propControl,
+  maxDate,
+  minDate,
   onChange,
-}: {
-  patientUuid: string;
-  onChange?: (data: TFormValues) => void;
-}) => {
+}: IRetrospectiveDateTimePickerProps) => {
   const { t } = useTranslation();
-  const formContext = useFormContext<TFormValues>();
   const { systemVisitEnabled } = useSystemVisitSetting();
   const isRdeEnabled = useFeatureFlag('rde');
 
   const showDatePicker = systemVisitEnabled && isRdeEnabled;
 
-  const { currentVisit, isLoading } = useVisit(patientUuid);
+  const { currentVisit } = useVisit(patientUuid);
   const isActiveVisit = !Boolean(currentVisit && currentVisit.stopDatetime);
 
   const [manuallyEnableDateTimePicker, setManuallyEnableDateTimePicker] = useState<boolean | false>(false);
 
-  // this form is for when the component is not rendered inside a form context
+  // disable inputs if isActiveVisit and user has not manually enabled the picker.
+  const disableInputs = isActiveVisit && !manuallyEnableDateTimePicker;
+
+  // this form is for when the caller does not pass a control prop
   // when this happens we assume the caller passed an onChange function which we
   // call to supply the data
   const form = useForm<TFormValues>();
   const unManagedFormValues = form.getValues();
 
-  const control = formContext ? formContext.control : form.control;
-
-  // disable inputs if isActiveVisit and user has not manually enabled the picker.
-  const disableInputs = isActiveVisit && !manuallyEnableDateTimePicker;
+  const control = propControl || form.control;
 
   useEffect(() => {
-    onChange?.(unManagedFormValues);
+    if (onChange) {
+      onChange(unManagedFormValues);
+    }
   }, [onChange, unManagedFormValues]);
 
   if (showDatePicker === false) {
@@ -53,68 +62,75 @@ const RetrospectiveDateTimePicker = ({
   return (
     <section className={styles.wrapper}>
       <h4 className={styles.heading}>Encounter time</h4>
+      {isActiveVisit && (
+        <Checkbox
+          checked={manuallyEnableDateTimePicker}
+          className={styles.checkbox}
+          id={'enable-date-time-picker'}
+          labelText={t('enable', 'Enable')}
+          onChange={(_, { checked, id }) => {
+            setManuallyEnableDateTimePicker(checked);
+          }}
+        />
+      )}
       <div className={styles.pickerWrapper}>
         <Controller
           name={'retrospective-date'}
           control={control}
           render={({ field, fieldState }) => (
-            <OpenmrsDatePicker
-              {...field}
-              id={'retrospective-date-picker-input'}
-              labelText={t('date', 'Date')}
-              invalid={Boolean(fieldState?.error?.message)}
-              invalidText={fieldState?.error?.message}
-              isDisabled={disableInputs}
-              maxDate={new Date()}
-            />
+            <ResponsiveWrapper>
+              <OpenmrsDatePicker
+                {...field}
+                id={'retrospective-date-picker-input'}
+                labelText={t('date', 'Date')}
+                invalid={Boolean(fieldState?.error?.message)}
+                invalidText={fieldState?.error?.message}
+                isDisabled={disableInputs}
+                maxDate={maxDate}
+                minDate={minDate}
+                className={styles.datePicker}
+              />
+            </ResponsiveWrapper>
           )}
         />
-        <Controller
-          name={'retrospective-time'}
-          control={control}
-          render={({ field: { onBlur, onChange, value } }) => (
-            <div>
-              <TimePicker
-                id={'retrospective-time-picker-input'}
-                labelText={t('time', 'Time')}
-                onBlur={onBlur}
-                onChange={(event) => onChange(event.target.value)}
-                pattern="^(0[1-9]|1[0-2]):([0-5][0-9])$"
-                value={value}
-                disabled={disableInputs}
-              >
-                <Controller
-                  name={'retrospective-time-format'}
-                  control={control}
-                  render={({ field: { onChange, value } }) => (
-                    <TimePickerSelect
-                      aria-label={t('timeFormat ', 'Time Format')}
-                      id={`am-pm-input`}
-                      onChange={(event) => onChange(event.target.value)}
-                      value={value}
-                      disabled={disableInputs}
-                    >
-                      <SelectItem value="" text="" />
-                      <SelectItem value="AM" text="AM" />
-                      <SelectItem value="PM" text="PM" />
-                    </TimePickerSelect>
-                  )}
-                />
-              </TimePicker>
-            </div>
-          )}
-        />
-        {isActiveVisit && (
-          <Checkbox
-            checked={manuallyEnableDateTimePicker}
-            className={styles.checkbox}
-            id={'enable-date-time-picker'}
-            labelText={'Enable'}
-            onChange={(_, { checked, id }) => {
-              setManuallyEnableDateTimePicker(checked);
-            }}
+        <ResponsiveWrapper>
+          <Controller
+            name={'retrospective-time'}
+            control={control}
+            render={({ field: { onBlur, onChange, value } }) => (
+              <div className={styles.timePickerWrapper}>
+                <TimePicker
+                  id={'retrospective-time-picker-input'}
+                  labelText={t('time', 'Time')}
+                  onBlur={onBlur}
+                  onChange={(event) => onChange(event.target.value)}
+                  pattern="^(0[1-9]|1[0-2]):([0-5][0-9])$"
+                  value={value}
+                  disabled={disableInputs}
+                  className={styles.timePicker}
+                >
+                  <Controller
+                    name={'retrospective-time-format'}
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
+                      <TimePickerSelect
+                        aria-label={t('timeFormat ', 'Time Format')}
+                        id={`am-pm-input`}
+                        onChange={(event) => onChange(event.target.value)}
+                        value={value}
+                        disabled={disableInputs}
+                      >
+                        <SelectItem value="" text="" />
+                        <SelectItem value="AM" text="AM" />
+                        <SelectItem value="PM" text="PM" />
+                      </TimePickerSelect>
+                    )}
+                  />
+                </TimePicker>
+              </div>
+            )}
           />
-        )}
+        </ResponsiveWrapper>
       </div>
     </section>
   );
