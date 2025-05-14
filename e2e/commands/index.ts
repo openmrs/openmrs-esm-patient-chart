3b45d35ef368af74cd569e29d8f176021f5c1a9d
gitcliff <46714226+gitcliff@users.noreply.github.com>
2025-05-14 20:40:02 +0300

    (test) O3-4687: Add Automated e2e test for cancelling a drug order (#2434)

--- e2e/commands/index.ts
+++ e2e/commands/index.ts
@@ -1,2 +1,5 @@
+export * from './drug-order-operations';
+export * from './encounter-operations';
 export * from './patient-operations';
+export * from './provider-operations';
 export * from './visit-operations';
