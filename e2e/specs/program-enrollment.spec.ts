bee57b4f1e7fac4d14177ab69a0da91b1c55e0cb
its-kios09 <legacyitsolution@gmail.com>
2025-05-15 19:05:32 +0300

    (refactor) reverted the tests program-enrollment.spec.ts

--- e2e/specs/program-enrollment.spec.ts
+++ e2e/specs/program-enrollment.spec.ts
@@ -68,13 +68,8 @@ test('Add and edit a program enrollment', async ({ page, patient }) => {
   });
 
   await test.step('When I click on the `Edit` button of the created program', async () => {
-    await expect(dataRow).toBeVisible();
-
-    await dataRow.hover();
-
-    await programsPage.overflowButton().click({ timeout: 10000 });
-
-    await page.getByRole('menuitem', { name: /edit/i }).click({ timeout: 10000 });
+    await programsPage.overflowButton().click();
+    await programsPage.editProgramButton().click();
   });
 
   await test.step('Then I should see the program launch in the workspace in edit mode`', async () => {
