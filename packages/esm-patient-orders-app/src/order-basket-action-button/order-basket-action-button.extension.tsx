e181e8cafe492b65f6f76259d5c76b8ce51808f0
jwnasambu <wamalwa1844.com>
2025-05-22 23:36:35 +0300

    fix/03-4454: Clicking on the Order Basket should not do anything when the order basket is open

--- packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
+++ packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
@@ -1,21 +1,31 @@
 import React, { type ComponentProps } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ActionMenuButton, ShoppingCartIcon } from '@openmrs/esm-framework';
+import { ActionMenuButton, ShoppingCartIcon, useWorkspaces } from '@openmrs/esm-framework';
 import { useLaunchWorkspaceRequiringVisit, useOrderBasket } from '@openmrs/esm-patient-common-lib';
 
 const OrderBasketActionButton: React.FC = () => {
   const { t } = useTranslation();
   const { orders } = useOrderBasket();
   const launchOrderBasket = useLaunchWorkspaceRequiringVisit('order-basket');
+  const { currentWorkspace } = useWorkspaces();
+
+  const isOrderBasketActive = currentWorkspace?.workspace === 'order-basket';
+
+  const handleClick = () => {
+    if (!isOrderBasketActive) {
+      launchOrderBasket();
+    }
+  };
 
   return (
     <ActionMenuButton
       getIcon={(props: ComponentProps<typeof ShoppingCartIcon>) => <ShoppingCartIcon {...props} />}
       label={t('orderBasket', 'Order basket')}
       iconDescription={t('medications', 'Medications')}
-      handler={launchOrderBasket}
-      type={'order'}
-      tagContent={orders?.length > 0 ? orders?.length : null}
+      handler={handleClick}
+      type="order"
+      tagContent={orders?.length > 0 ? orders.length : null}
+      disabled={isOrderBasketActive}
     />
   );
 };
