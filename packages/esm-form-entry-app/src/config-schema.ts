56b7f11c5e61dd3e3bffe84686981d2e22b1199f
Harsh Thakkar <iharshthkkr@gmail.com>
2025-06-02 22:47:45 +0530

    (fix) Improve config schema for esm-form-entry-app

--- packages/esm-form-entry-app/src/config-schema.ts
+++ packages/esm-form-entry-app/src/config-schema.ts
@@ -1,34 +1,34 @@
-import { validator } from '@openmrs/esm-framework';
+import { Type, validator } from '@openmrs/esm-framework';
 
 export const configSchema = {
   /** @deprecated use customDataSources instead */
   dataSources: {
     monthlySchedule: {
-      _type: 'Boolean',
+      _type: Type.Boolean,
       _default: false,
       _description:
         'Whether to use monthly scheduled appointment data source in form-entry engine. Requires `appointmentsResourceUrl`.',
     },
   },
   customDataSources: {
-    _type: 'Array',
+    _type: Type.Array,
     _default: [],
     _elements: {
       name: {
-        _type: 'String',
+        _type: Type.String,
         _default: 'customDataSource',
         _description: 'The name of the data source. This is how the data source is referenced inside the form.',
         _validators: [validator((val) => val !== '', 'Must have a value')],
       },
       moduleName: {
-        _type: 'String',
-        _default: '',
+        _type: Type.String,
+        _default: null,
         _description:
           'The specifier for the module, e.g., "@myOrg/myGreatDataSource". This module should be defined in your import map and will be loaded for all forms.',
         _validators: [validator((val) => val !== '', 'Must have a value')],
       },
       moduleExport: {
-        _type: 'String',
+        _type: Type.String,
         _default: 'default',
         _description:
           'The property of the module to be exposed as the data source. This is the name of the exported JS object. For example, setting this to "ageFactory" will use the attribute named "ageFactory". The default value, "default", will point to the default export, if your JS module has one.',
@@ -36,13 +36,13 @@ export const configSchema = {
     },
   },
   appointmentsResourceUrl: {
-    _type: 'String',
+    _type: Type.String,
     _default: '/etl-latest/etl/get-monthly-schedule',
     _description:
       'Custom URL to load resources required for appointment monthly schedule feature (under `dataSources`).',
   },
   customEncounterDatetime: {
-    _type: 'Boolean',
+    _type: Type.Boolean,
     _default: false,
     _description:
       'Weather to default the encounterDate to visitStartDatetime if the visitStartDatetime is before current Date.',
