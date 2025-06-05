16631563ccee057f99c10448bf7bb0deaeb62148
CynthiaKamau <cynthiakamau54@gmail.com>
2025-06-05 15:16:50 +0300

    O3-4746 Implementation of pagination in esm-patient-forms-app

--- packages/esm-patient-forms-app/src/forms/forms-dashboard.component.tsx
+++ packages/esm-patient-forms-app/src/forms/forms-dashboard.component.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, { useMemo } from 'react';
+=======
+import React, { useCallback, useMemo, useState } from 'react';
+>>>>>>> 5ad15105 (O3-4746 Implementation of pagination in esm-patient-forms-app)
 import { useTranslation } from 'react-i18next';
 import { Tile } from '@carbon/react';
 import { ResponsiveWrapper, useConfig, useConnectivity, type Visit } from '@openmrs/esm-framework';
@@ -18,11 +22,57 @@ const FormsDashboard: React.FC<FormsDashbaordProps> = ({ handleFormOpen, patient
   const { t } = useTranslation();
   const config = useConfig<FormEntryConfigSchema>();
   const isOnline = useConnectivity();
+  const [pageSize, setPageSize] = useState(50);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
   const {
     data: forms,
     error,
     mutateForms,
-  } = useForms(patient.id, visitContext?.uuid, undefined, undefined, !isOnline, config.orderBy);
+    totalCount,
+  } = useForms(
+    patientUuid,
+    visitContext?.uuid,
+    undefined,
+    undefined,
+    !isOnline,
+    config.orderBy,
+    searchTerm,
+    pageSize,
+    currentPage,
+  );
+
+  const htmlFormEntryForms = useMemo(() => {
+    return mapFormsToHtmlFormEntryForms(allForms, config.htmlFormEntryForms);
+  }, [config.htmlFormEntryForms, allForms]);
+
+  const handleFormOpen = useCallback(
+    (form: Form, encounterUuid: string) => {
+      launchFormEntryOrHtmlForms(
+        htmlFormEntryForms,
+        patientUuid,
+        form,
+        visitContext?.uuid,
+        encounterUuid,
+        visitContext?.visitType.uuid,
+        visitContext?.startDatetime,
+        visitContext?.stopDatetime,
+        mutateForms,
+        clinicalFormsWorkspaceName,
+        formEntryWorkspaceName,
+        htmlFormEntryWorkspaceName,
+      );
+    },
+    [
+      visitContext,
+      htmlFormEntryForms,
+      mutateForms,
+      patientUuid,
+      clinicalFormsWorkspaceName,
+      formEntryWorkspaceName,
+      htmlFormEntryWorkspaceName,
+    ],
+  );
 
   const sections = useMemo(() => {
     return config.formSections?.map((formSection) => ({
@@ -56,7 +106,12 @@ const FormsDashboard: React.FC<FormsDashbaordProps> = ({ handleFormOpen, patient
               sectionName={section.name}
               forms={section.availableForms}
               error={error}
+              pageSize={pageSize}
               handleFormOpen={handleFormOpen}
+              searchTerm={searchTerm}
+              onPageChange={setCurrentPage}
+              onPageSizeChange={setPageSize}
+              onSearchTermChange={setSearchTerm}
             />
           );
         })
