dd846512188c6fddd10775080dab63c5faf53c2c
Amos Machora <amosmachora13@gmail.com>
2025-06-25 05:28:09 +0300

    feat: use rde date for new rde orders

--- packages/esm-patient-orders-app/src/order-basket/order-basket.workspace.tsx
+++ packages/esm-patient-orders-app/src/order-basket/order-basket.workspace.tsx
@@ -42,6 +42,7 @@ const OrderBasket: React.FC<DefaultPatientWorkspaceProps> = ({
   const {
     visitRequired,
     isLoading: isLoadingEncounterUuid,
+    encounterUuid,
     error: errorFetchingEncounterUuid,
     mutate: mutateEncounterUuid,
   } = useOrderEncounter(patientUuid);
@@ -96,25 +97,49 @@ const OrderBasket: React.FC<DefaultPatientWorkspaceProps> = ({
   const handleSave = useCallback(async () => {
     const abortController = new AbortController();
     setCreatingEncounterError('');
+    let orderEncounterUuid = encounterUuid;
     setIsSavingOrders(true);
-    // If there's no encounter present, create an encounter along with the orders.
-    await postOrdersOnNewEncounter(
-      patientUuid,
-      config?.orderEncounterType,
-      visitRequired ? currentVisit : null,
-      session?.sessionLocation?.uuid,
-      abortController,
-      rdeDate,
-    );
-    mutateEncounterUuid();
-    mutateVisit();
-    clearOrders();
-    await mutateOrders();
-    closeWorkspaceWithSavedChanges();
-    showOrderSuccessToast(t, orders);
+
+    // If there's no encounter present or we are adding retrospective data, create an encounter along with the orders.
+    if (!orderEncounterUuid || rdeDate) {
+      try {
+        await postOrdersOnNewEncounter(
+          patientUuid,
+          config?.orderEncounterType,
+          visitRequired ? currentVisit : null,
+          session?.sessionLocation?.uuid,
+          abortController,
+          rdeDate,
+        );
+        mutateEncounterUuid();
+        mutateVisit();
+        clearOrders();
+        await mutateOrders();
+        closeWorkspaceWithSavedChanges();
+        showOrderSuccessToast(t, orders);
+      } catch (e) {
+        console.error(e);
+        setCreatingEncounterError(
+          e.responseBody?.error?.message ||
+            t('tryReopeningTheWorkspaceAgain', 'Please try launching the workspace again'),
+        );
+      }
+    } else {
+      const erroredItems = await postOrders(patientUuid, orderEncounterUuid, abortController);
+      clearOrders({ exceptThoseMatching: (item) => erroredItems.map((e) => e.display).includes(item.display) });
+      mutateVisit();
+      await mutateOrders();
+      if (erroredItems.length == 0) {
+        closeWorkspaceWithSavedChanges();
+        showOrderSuccessToast(t, orders);
+      } else {
+        setOrdersWithErrors(erroredItems);
+      }
+    }
     setIsSavingOrders(false);
     return () => abortController.abort();
   }, [
+    encounterUuid,
     patientUuid,
     config?.orderEncounterType,
     visitRequired,
@@ -138,10 +163,7 @@ const OrderBasket: React.FC<DefaultPatientWorkspaceProps> = ({
     <>
       <div className={styles.container}>
         <ExtensionSlot name="visit-context-header-slot" state={{ patientUuid }} />
-        <ExtensionSlot
-          name="restrospective-date-time-picker-slot"
-          state={{ patientUuid, onChange: handleRdeDateTimeChange }}
-        />
+
         <div className={styles.orderBasketContainer}>
           <ExtensionSlot
             className={classNames(styles.orderBasketSlot, {
@@ -161,6 +183,10 @@ const OrderBasket: React.FC<DefaultPatientWorkspaceProps> = ({
                 />
               </div>
             ))}
+          <ExtensionSlot
+            name="restrospective-date-time-picker-slot"
+            state={{ patientUuid, onChange: handleRdeDateTimeChange }}
+          />
         </div>
 
         <div>
