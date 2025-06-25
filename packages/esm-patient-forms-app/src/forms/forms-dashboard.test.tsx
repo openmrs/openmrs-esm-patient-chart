090fd2a92eef85820db33b23fa59b9dc98f1372f
CynthiaKamau <cynthiakamau54@gmail.com>
2025-06-25 15:52:03 +0300

    code review

--- packages/esm-patient-forms-app/src/forms/forms-dashboard.test.tsx
+++ packages/esm-patient-forms-app/src/forms/forms-dashboard.test.tsx
@@ -4,6 +4,7 @@ import { getDefaultsFromConfigSchema, useConfig } from '@openmrs/esm-framework';
 import { configSchema, type FormEntryConfigSchema } from '../config-schema';
 import FormsDashboard from './forms-dashboard.component';
 import { mockPatient } from 'tools';
+import { FormsProvider } from '../hooks/use-forms-context';
 
 const mockUseConfig = jest.mocked(useConfig<FormEntryConfigSchema>);
 
@@ -17,8 +18,20 @@ jest.mock('../hooks/use-forms', () => ({
 }));
 
 mockUseConfig.mockReturnValue({ ...getDefaultsFromConfigSchema(configSchema), htmlFormEntryForms: [] });
+jest.mock('@openmrs/esm-patient-common-lib', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-patient-common-lib');
+
+  return {
+    ...originalModule,
+    useVisitOrOfflineVisit: jest.fn(),
+  };
+});
 
 describe('FormsDashboard', () => {
+  beforeEach(() => {
+    mockUseConfig.mockReturnValue({ ...getDefaultsFromConfigSchema(configSchema), htmlFormEntryForms: [] });
+  });
+
   test('renders an empty state if there are no forms persisted on the server', async () => {
     render(<FormsDashboard handleFormOpen={jest.fn()} patient={mockPatient} visitContext={null} />);
 
