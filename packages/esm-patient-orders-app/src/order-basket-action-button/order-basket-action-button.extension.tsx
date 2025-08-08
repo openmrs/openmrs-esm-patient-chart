e332d3cc79196d205b394b8bf35d11166d0a24a6
EDSON <edsonwasswa54@gmail.com>
2025-08-08 15:07:59 +0300

    clicking-on-the-order-basket-should-not-do-anything-when-the-order-basket-is-open

--- packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
+++ packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
@@ -1,19 +1,33 @@
 import React, { type ComponentProps } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ActionMenuButton, ShoppingCartIcon } from '@openmrs/esm-framework';
+import { ActionMenuButton, ShoppingCartIcon, useWorkspaces, launchWorkspace } from '@openmrs/esm-framework';
 import { useLaunchWorkspaceRequiringVisit, useOrderBasket } from '@openmrs/esm-patient-common-lib';
 
 const OrderBasketActionButton: React.FC = () => {
   const { t } = useTranslation();
   const { orders } = useOrderBasket();
   const launchOrderBasket = useLaunchWorkspaceRequiringVisit('order-basket');
+  const { workspaces } = useWorkspaces();
+  const orderBasketWorkspaces = workspaces.filter((w) => w.name === 'order-basket');
+  const isOrderBasketActive = orderBasketWorkspaces.length > 0;
+  const existingOrderBasket = orderBasketWorkspaces[0];
 
+  const handleClick = () => {
+    if (isOrderBasketActive) {
+      // If order basket is already open, do absolutely nothing
+      // This prevents the workspace from being reset to its initial state
+      //return;
+    } else {
+      // If not open, launch it normally
+      launchOrderBasket();
+    }
+  };
   return (
     <ActionMenuButton
       getIcon={(props: ComponentProps<typeof ShoppingCartIcon>) => <ShoppingCartIcon {...props} />}
       label={t('orderBasket', 'Order basket')}
       iconDescription={t('medications', 'Medications')}
-      handler={launchOrderBasket}
+      handler={handleClick}
       type={'order'}
       tagContent={orders?.length > 0 ? orders?.length : null}
     />
