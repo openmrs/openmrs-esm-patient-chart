9a99f2503a56c3bc21fcc47279be884c45e7b982
Chi Bong Ho <chibongho@gmail.com>
2025-08-14 16:57:45 -0400

    add e2e test for visit context

--- e2e/specs/start-and-end-visit.spec.ts
+++ e2e/specs/start-and-end-visit.spec.ts
@@ -3,12 +3,6 @@ import { test } from '../core';
 import { ChartPage } from '../pages';
 
 test('Start and end a new visit', async ({ page, patient, api }) => {
-  const chartPage = new ChartPage(page);
-
-  await test.step('When I visit the chart summary page', async () => {
-    await chartPage.goTo(patient.uuid);
-  });
-
   await test.step('Ensure no active visits for the patient', async () => {
     const res = await api.get(`visit?patient=${patient.uuid}&active=true`);
     const data = await res.json();
@@ -17,6 +11,12 @@ test('Start and end a new visit', async ({ page, patient, api }) => {
     for (const visit of visits) {
       await api.post(`visit/${visit.uuid}`, { data: { voided: true } });
     }
+  });
+
+  const chartPage = new ChartPage(page);
+
+  await test.step('When I visit the chart summary page', async () => {
+    await chartPage.goTo(patient.uuid);
   });
 
   await test.step('And I click on the `Add visit` button in the actions overflow menu', async () => {
@@ -102,5 +102,111 @@ test('Start and end a new visit', async ({ page, patient, api }) => {
 
   await test.step('Then I should see a success notification', async () => {
     await expect(chartPage.page.getByText(/ended current visit successfully/i)).toBeVisible();
+  });
+});
+
+test('visit context after starting / ending visit', async ({ page, patient, api }) => {
+  await test.step('Ensure no active visits for the patient', async () => {
+    const res = await api.get(`visit?patient=${patient.uuid}&active=true`);
+    const data = await res.json();
+    const { results: visits = [] } = data;
+
+    for (const visit of visits) {
+      await api.post(`visit/${visit.uuid}`, { data: { voided: true } });
+    }
+  });
+
+  const chartPage = new ChartPage(page);
+
+  await test.step('When I visit the chart summary page', async () => {
+    await chartPage.goTo(patient.uuid);
+  });
+
+  await test.step('When I click the `Visit note` button on the siderail with no visit', async () => {
+    await page.getByRole('button', { name: /note/i }).click();
+  });
+
+  await test.step('Then I should see a modal to prompt for starting a visit', async () => {
+    await expect(chartPage.page.getByText(/no active visit/i)).toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /cancel/i })).toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /start new visit/i })).toBeVisible();
+  });
+
+  await test.step('When cancel the start visit prompt modal', async () => {
+    await chartPage.page.getByRole('button', { name: /cancel/i }).click();
+  });
+
+  await test.step('Then the prompt modal should close', async () => {
+    await expect(chartPage.page.getByText(/no active visit/i)).not.toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /cancel/i })).not.toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /start new visit/i })).not.toBeVisible();
+  });
+
+  await test.step('When I click the `Visit note` button on the siderail again with no visit', async () => {
+    await page.getByRole('button', { name: /note/i }).click();
+  });
+
+  await test.step("And I click 'start new visit'in the prompt modal ", async () => {
+    await chartPage.page.getByRole('button', { name: /start new visit/i }).click();
+  });
+
+  await test.step('Then I should see the `Start Visit` form launch in the workspace', async () => {
+    await expect(chartPage.page.getByRole('tab', { name: /new/i })).toBeVisible();
+    await expect(chartPage.page.getByRole('tab', { name: /ongoing/i })).toBeVisible();
+    await expect(chartPage.page.getByRole('tab', { name: /in the past/i })).toBeVisible();
+  });
+  await test.step('When I select visit status: new', async () => {
+    await chartPage.page.getByRole('tab', { name: /new/i }).click();
+  });
+
+  await test.step('And I select the visit type: `OPD Visit`', async () => {
+    await chartPage.page.getByText(/opd visit/i).click();
+  });
+
+  await test.step('And I click on the `Start Visit` button', async () => {
+    await chartPage.page
+      .locator('form')
+      .getByRole('button', { name: /start visit/i })
+      .click();
+  });
+
+  await test.step('Then I should see a success notification', async () => {
+    await expect(chartPage.page.getByText(/opd visit started successfully/i)).toBeVisible();
+  });
+
+  await test.step('And I should see the Active Visit tag on the patient header', async () => {
+    await expect(chartPage.page.getByLabel(/active visit/i)).toBeVisible();
+  });
+
+  await test.step('When I click the `Visit note` button on the siderail', async () => {
+    await page.getByRole('button', { name: /note/i }).click();
+  });
+
+  await test.step('Then I should see the visit note form launch in the workspace', async () => {
+    await expect(page.getByText('Visit Note', { exact: true })).toBeVisible();
+  });
+
+  await test.step('When I close the workspace', async () => {
+    await page.getByRole('button', { name: /discard/i }).click();
+  });
+
+  await test.step('And when I end the active visit', async () => {
+    await chartPage.page.getByRole('button', { name: /actions/i }).click();
+    await chartPage.page.getByRole('menuitem', { name: /end active visit/i }).click();
+    await chartPage.page.getByRole('button', { name: /end visit/i }).click();
+  });
+
+  await test.step('Then I should not see the Active Visit tag on the patient header', async () => {
+    await expect(chartPage.page.getByLabel(/active visit/i)).not.toBeVisible();
+  });
+
+  await test.step('When I click the `Visit note` button on the siderail', async () => {
+    await page.getByRole('button', { name: /note/i }).click();
+  });
+
+  await test.step('Then I should see a modal to prompt for starting a visit', async () => {
+    await expect(chartPage.page.getByText(/no active visit/i)).toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /cancel/i })).toBeVisible();
+    await expect(chartPage.page.getByRole('button', { name: /start new visit/i })).toBeVisible();
   });
 });
