46b3e1f3d999212b1a36db2ecef759a50abc3a0c
EDSON <edsonwasswa54@gmail.com>
2025-08-25 16:44:43 +0300

    wrap funtion into  useCallback()

--- packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
+++ packages/esm-patient-orders-app/src/order-basket-action-button/order-basket-action-button.extension.tsx
@@ -1,4 +1,4 @@
-import React, { type ComponentProps, useMemo } from 'react';
+import React, { type ComponentProps, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ActionMenuButton, ShoppingCartIcon, useWorkspaces } from '@openmrs/esm-framework';
 import { useLaunchWorkspaceRequiringVisit, useOrderBasket } from '@openmrs/esm-patient-common-lib';
@@ -10,17 +10,16 @@ const OrderBasketActionButton: React.FC = () => {
   const { orders } = useOrderBasket();
   const launchOrderBasket = useLaunchWorkspaceRequiringVisit('order-basket');
   const { workspaces } = useWorkspaces();
-  const isFormWorkspaceActive = useMemo(() => {
-    return workspaces.some((workspace) => formWorkspaceNames.includes(workspace.name));
-  }, [workspaces]);
 
-  const handleOrderbasketClick = () => {
+  const handleOrderbasketClick = useCallback(() => {
+    const isFormWorkspaceActive = workspaces.some((workspace) => formWorkspaceNames.includes(workspace.name));
     if (isFormWorkspaceActive) {
-      //do nothing
-      return;
+      return; //do nothing
     }
+
     launchOrderBasket();
-  };
+  }, [workspaces, launchOrderBasket]);
+
   return (
     <ActionMenuButton
       getIcon={(props: ComponentProps<typeof ShoppingCartIcon>) => <ShoppingCartIcon {...props} />}
