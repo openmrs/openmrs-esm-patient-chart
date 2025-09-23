e56a63efa3adb20a9bf0d4ac126dd0e4dd22c3e7
Dennis Kigen <kigen.work@gmail.com>
2025-09-23 21:51:16 +0300

    Fixup

--- packages/esm-patient-procedures-app/src/hooks/useProcedures.ts
+++ packages/esm-patient-procedures-app/src/hooks/useProcedures.ts
@@ -1,34 +1,32 @@
-import {
-  type FetchResponse,
-  OpenmrsResource,
-  openmrsFetch,
-  restBaseUrl,
-  useConfig,
-  type ConfigObject,
-} from '@openmrs/esm-framework';
 import useSWRImmutable from 'swr/immutable';
+import { type FetchResponse, openmrsFetch, restBaseUrl, useConfig, type ConfigObject } from '@openmrs/esm-framework';
 import { type Results } from './types';
 
 interface EncounterResponse {
   results: Array<Results>;
 }
 
-export function useProcedures(patientUuid: string, encounterTypeUuid: string) {
+export function useProcedures(patientUuid: string) {
   const config = useConfig<ConfigObject>();
 
-  const url = `${restBaseUrl}/encounter?patient=${patientUuid}&encounterType=${encounterTypeUuid}&v=custom:(uuid,display,encounterDatetime,obs:(uuid,concept:(uuid,display,conceptClass:(uuid,display)),display,groupMembers:(uuid,concept:(uuid,display),value:(uuid,display),display),value,obsDatetime))`;
+  const url = `${restBaseUrl}/encounter?patient=${patientUuid}&encounterType=${config.procedureEncounterType}&v=custom:(uuid,display,encounterDatetime,obs:(uuid,concept:(uuid,display,conceptClass:(uuid,display)),display,groupMembers:(uuid,concept:(uuid,display),value:(uuid,display),display),value,obsDatetime))`;
 
-  const { data, error, isLoading } = useSWRImmutable<FetchResponse<EncounterResponse>, Error>(url, openmrsFetch);
+  const { data, error, isLoading, mutate } = useSWRImmutable<FetchResponse<EncounterResponse>, Error>(
+    url,
+    openmrsFetch,
+  );
 
   const procedures =
     data?.data.results?.map((encounter) => ({
-      id: encounter.obs.find((ob) => ob.uuid).uuid,
+      id: encounter.obs.find((ob) => ob.uuid)?.uuid,
       procedure: encounter.obs.find((ob) => ob.concept.uuid === config?.nameOfProcedurePerformedUuid)?.value?.display,
       date: encounter.obs.find((ob) => ob.concept.uuid === config?.dayProcedurePerformedUuid)?.value,
     })) ?? [];
+
   return {
     procedures,
     isLoading,
     error,
+    mutate,
   };
 }
