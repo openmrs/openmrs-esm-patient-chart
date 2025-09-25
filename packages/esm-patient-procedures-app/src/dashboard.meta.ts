32deb489add6d1d674d54b6664dc027ccd8c84c8
Dennis Kigen <kigen.work@gmail.com>
2025-09-25 23:41:14 +0300

    Update left nav dashboard link icon and async imports

--- packages/esm-patient-procedures-app/src/dashboard.meta.ts
+++ packages/esm-patient-procedures-app/src/dashboard.meta.ts
@@ -1,6 +1,8 @@
-export const dashboardMeta = {
+import { type DashboardLinkConfig } from '@openmrs/esm-patient-common-lib';
+
+export const dashboardMeta: DashboardLinkConfig & { slot: string } = {
   slot: 'patient-chart-procedures-dashboard-slot',
   path: 'Procedures',
   title: 'Procedures',
-  icon: 'omrs-pict-procedure-order',
+  icon: 'omrs-icon-procedure-order',
 };
--- packages/esm-patient-procedures-app/src/index.ts
+++ packages/esm-patient-procedures-app/src/index.ts
@@ -1,8 +1,7 @@
-import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
+import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmrs/esm-framework';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { configSchema } from './config-schema';
 import { dashboardMeta } from './dashboard.meta';
-import proceduresDetailedSummaryComponent from './components/procedures-history/procedures-history.component';
 
 const moduleName = '@openmrs/esm-patient-procedures-app';
 
@@ -17,8 +16,12 @@ export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 }
 
-export const proceduresDetailedSummary = getSyncLifecycle(proceduresDetailedSummaryComponent, options);
+export const proceduresDetailedSummary = getAsyncLifecycle(
+  () => import('./components/procedures-history/procedures-history.component'),
+  options,
+);
 
+// t('procedures', 'Procedures')
 export const proceduresDashboardLink = getSyncLifecycle(
   createDashboardLink({
     ...dashboardMeta,
