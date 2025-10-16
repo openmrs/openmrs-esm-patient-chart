8431b7c6d8c329b92fe4244a9c7d0cd7853ebda4
Chi Bong Ho <cbho@pih.org>
2025-10-16 09:58:04 -0400

    (fix) O3-4389 fix encoutner type display string in visit timeline

--- packages/esm-patient-chart-app/src/clinical-views/hooks/useEncountersByVisit.ts
+++ packages/esm-patient-chart-app/src/clinical-views/hooks/useEncountersByVisit.ts
@@ -2,7 +2,7 @@ import { type Encounter, restBaseUrl, useOpenmrsFetchAll } from '@openmrs/esm-fr
 
 export function useEncountersByVisit(patientUuid: string, visitUuid: string) {
   const customRepresentation =
-    'custom:(uuid,encounterType:(name),encounterProviders:(provider:(person:(display))),encounterDatetime,visit:(uuid))';
+    'custom:(uuid,encounterType:(uuid,display),encounterProviders:(provider:(person:(display))),encounterDatetime,visit:(uuid))';
   const url = `${restBaseUrl}/encounter?patient=${patientUuid}&order=desc&visit=${visitUuid}&v=${customRepresentation}`;
   const { data: encounters, ...rest } = useOpenmrsFetchAll<Encounter>(url);
 
