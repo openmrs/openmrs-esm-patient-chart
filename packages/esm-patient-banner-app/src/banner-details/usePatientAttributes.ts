fc9ba83e9ed1c169303364d3150b68dde3007321
BastardCC <ainamandarine@gmail.com>
2025-11-20 09:19:08 +0300

    Add personal attributes in response

--- packages/esm-patient-banner-app/src/banner-details/usePatientAttributes.ts
+++ packages/esm-patient-banner-app/src/banner-details/usePatientAttributes.ts
@@ -5,7 +5,7 @@ import { useConfig } from '@openmrs/esm-react-utils';
 import { type Patient } from './types';
 
 const customRepresentation =
-  'custom:(uuid,display,identifiers:(identifier,uuid,preferred,location:(uuid,name),identifierType:(uuid,name,format,formatDescription,validator)),person:(uuid,display,gender,birthdate,dead,age,deathDate,birthdateEstimated,causeOfDeath,preferredName:(uuid,preferred,givenName,middleName,familyName),attributes,preferredAddress:(uuid,preferred,address1,address2,cityVillage,longitude,stateProvince,latitude,country,postalCode,countyDistrict,address3,address4,address5,address6,address7)))';
+  'custom:(uuid,display,identifiers:(identifier,uuid,preferred,location:(uuid,name),identifierType:(uuid,name,format,formatDescription,validator)),person:(uuid,display,gender,birthdate,dead,age,deathDate,birthdateEstimated,causeOfDeath,preferredName:(uuid,preferred,givenName,middleName,familyName),attributes:(uuid,display,value,attributeType:(uuid,display,name)),preferredAddress:(uuid,preferred,address1,address2,cityVillage,longitude,stateProvince,latitude,country,postalCode,countyDistrict,address3,address4,address5,address6,address7)))';
 
 /**
  *  React hook that takes patientUuid and return Patient Attributes {@link Attribute}
