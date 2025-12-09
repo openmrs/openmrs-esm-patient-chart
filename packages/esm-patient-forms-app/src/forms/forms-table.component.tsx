be498c5d5115a5b07ad38f8d745b2e811f2ce615
CynthiaKamau <cynthiakamau54@gmail.com>
2025-12-09 10:53:04 +0300

    Code review

--- packages/esm-patient-forms-app/src/forms/forms-table.component.tsx
+++ packages/esm-patient-forms-app/src/forms/forms-table.component.tsx
@@ -53,75 +53,78 @@ const FormsTable = ({
   const pageSizes = [50];
 
   return (
-    <DataTable rows={tableRows} headers={tableHeaders} size={isTablet ? 'lg' : 'sm'} useZebraStyles>
-      {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
-        <>
-          <TableContainer className={styles.tableContainer}>
-            <div className={styles.toolbarWrapper}>
-              <TableToolbar className={styles.tableToolbar}>
-                <TableToolbarContent>
-                  <TableToolbarSearch
-                    className={styles.search}
-                    expanded
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleSearch(event.target.value)}
-                    placeholder={t('searchThisList', 'Search this list')}
-                    size="sm"
-                  />
-                </TableToolbarContent>
-              </TableToolbar>
-            </div>
-            {rows.length > 0 && (
-              <Table aria-label="forms" {...getTableProps()} className={styles.table}>
-                <TableHead>
-                  <TableRow>
-                    {headers.map((header) => (
-                      <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row, i) => (
-                    <TableRow {...getRowProps({ row })}>
-                      <TableCell key={row.cells[0].id}>
-                        <Link
-                          style={{ cursor: 'pointer' }}
-                          onClick={() => {
-                            handleFormOpen(tableRows[i].form, null);
-                          }}
-                          role="presentation"
-                          className={styles.formName}
-                        >
-                          {tableRows[i]?.formName}
-                        </Link>
-                      </TableCell>
-                      <TableCell className={styles.editCell}>
-                        <span>{row.cells[1].value ?? t('never', 'Never')}</span>
-                      </TableCell>
+    <>
+      <DataTable rows={tableRows} headers={tableHeaders} size={isTablet ? 'lg' : 'sm'} useZebraStyles>
+        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+          <>
+            <TableContainer className={styles.tableContainer}>
+              <div className={styles.toolbarWrapper}>
+                <TableToolbar className={styles.tableToolbar}>
+                  <TableToolbarContent>
+                    <TableToolbarSearch
+                      className={styles.search}
+                      expanded
+                      onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleSearch(event.target.value)}
+                      placeholder={t('searchThisList', 'Search this list')}
+                      size="sm"
+                    />
+                  </TableToolbarContent>
+                </TableToolbar>
+              </div>
+              {rows.length > 0 && (
+                <Table aria-label="forms" {...getTableProps()} className={styles.table}>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map((header) => (
+                        <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
+                      ))}
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            )}
-          </TableContainer>
-          <Pagination
-            forwardText={t('nextPage', 'Next page')}
-            backwardText={t('previousPage', 'Previous page')}
-            page={currentPage}
-            pageSize={pageSize}
-            size="sm"
-            pageSizes={pageSizes}
-            totalItems={totalForms}
-            onChange={({ page, pageSize: newPageSize }) => {
-              if (newPageSize !== pageSize) {
-                setPageSize(newPageSize);
-              }
-              setCurrentPage(page);
-              goTo(page);
-            }}
-          />
-        </>
-      )}
-    </DataTable>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row, i) => (
+                      <TableRow {...getRowProps({ row })}>
+                        <TableCell key={row.cells[0].id}>
+                          <Link
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => {
+                              handleFormOpen(tableRows[i].form, null);
+                            }}
+                            role="presentation"
+                            className={styles.formName}
+                          >
+                            {tableRows[i]?.formName}
+                          </Link>
+                        </TableCell>
+                        <TableCell className={styles.editCell}>
+                          <span>{row.cells[1].value ?? t('never', 'Never')}</span>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
+            </TableContainer>
+          </>
+        )}
+      </DataTable>
+      <Pagination
+        forwardText={t('nextPage', 'Next page')}
+        backwardText={t('previousPage', 'Previous page')}
+        page={currentPage}
+        pageSize={pageSize}
+        size={isTablet ? 'lg' : 'sm'}
+        itemsPerPageText={t('noOfForms', 'No. of forms')}
+        pageSizes={pageSizes}
+        totalItems={totalForms}
+        onChange={({ page, pageSize: newPageSize }) => {
+          if (newPageSize !== pageSize) {
+            setPageSize(newPageSize);
+          }
+          setCurrentPage(page);
+          goTo(page);
+        }}
+      />
+    </>
   );
 };
 
