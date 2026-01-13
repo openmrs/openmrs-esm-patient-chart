2888411076a8f46da6ff321122828e6c006106de
dilankavishka <dkgamage01@gmail.com>
2026-01-13 20:08:10 +0530

    Made a change

--- packages/esm-patient-growth-chart-app/src/config-schema.ts
+++ packages/esm-patient-growth-chart-app/src/config-schema.ts
@@ -1,28 +1,5 @@
 import { Type, validators } from '@openmrs/esm-framework';
 
-export const configSchema = {
-  // Add your configuration schema here
-  // Example:
-  // logo: {
-  //   src: {
-  //     _type: Type.String,
-  //     _default: '',
-  //     _description: 'The path or URL to the logo image',
-  //     _validators: [validators.isUrl],
-  //   },
-  //   alt: {
-  //     _type: Type.String,
-  //     _default: 'Logo',
-  //     _description: 'The alternative text for the logo image',
-  //   },
-  // },
-};
+export const configSchema = {};
 
-export type ConfigSchema = {
-  // TypeScript type derived from schema
-  // Example:
-  // logo: {
-  //   src: string;
-  //   alt: string;
-  // };
-};
+export type ConfigSchema = {};
