573e62031c6f61e1ce5d68b8f61aeb235b1e7017
Ian <ian.c.bacher@gmail.com>
2026-03-12 14:15:57 -0400

    Suggestion from code review

--- packages/esm-patient-attachments-app/rspack.config.js
+++ packages/esm-patient-attachments-app/rspack.config.js
@@ -1,1 +1,13 @@
-module.exports = require('openmrs/default-rspack-config');
+const config = require('openmrs/default-rspack-config');
+
+config.cssRuleConfig.exclude = /react-html5-camera-photo/;
+
+module.exports = (env, argv) => {
+  const rspackConfig = config(env, argv);
+  rspackConfig.module.rules.push({
+    test: /\.css$/,
+    include: /react-html5-camera-photo/,
+    use: ['style-loader', 'css-loader'],
+  });
+  return rspackConfig;
+};
