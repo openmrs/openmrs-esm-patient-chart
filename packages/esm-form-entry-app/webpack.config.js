9812b2b998bb3b1c9efc34e1c575000c5a61672c
Dennis Kigen <kigen.work@gmail.com>
2026-05-21 15:30:19 +0300

    chore: resolve Angular 20 form entry merge prep

--- packages/esm-form-entry-app/webpack.config.js
+++ packages/esm-form-entry-app/webpack.config.js
@@ -125,8 +125,6 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss'],
     alias: {
       '@openmrs/esm-framework': '@openmrs/esm-framework/src/internal',
-      '@angular/cdk/portal$': resolve(__dirname, 'src/compat/angular-cdk-portal.ts'),
-      '@openmrs/ngx-file-uploader$': resolve(__dirname, 'src/compat/ngx-file-uploader.ts'),
     },
   },
 };
