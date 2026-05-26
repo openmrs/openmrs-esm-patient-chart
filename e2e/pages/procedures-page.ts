3b3acf58ee0235301761dd1326734663aca6a67a
dilankavishka <dkgamage01@gmail.com>
2026-05-26 20:06:14 +0530

    Add e2e for procedure app

---
+++ e2e/pages/procedures-page.ts
@@ -0,0 +1,11 @@
+import { type Page } from '@playwright/test';
+
+export class ProceduresPage {
+  constructor(readonly page: Page) {}
+
+  readonly proceduresTable = () => this.page.getByRole('table');
+
+  async goTo(uuid: string) {
+    await this.page.goto(`/openmrs/spa/patient/${uuid}/chart/Procedures`);
+  }
+}
